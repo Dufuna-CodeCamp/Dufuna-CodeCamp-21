22058bb2788b23654fcf00a71a8b25e841251af0
ireti203 <opteaoged@gmail.com>
2021-06-07 17:13:20 +0100

    made changes

--- tests/add_navigation_links/navigation_tasks.js
+++ tests/add_navigation_links/navigation_tasks.js
@@ -1,7 +1,7 @@
 var file_path = process.env.FILE_PATH
 var webdriver = require('selenium-webdriver');
 var assert = require('chai').assert;
-var { Builder, By, until } = require("selenium-webdriver");
+var { Builder, By } = require("selenium-webdriver");
 
 describe("Testing Navigation on HtmlForms", function() {
     this.timeout(3 * 1000 * 60);
@@ -9,12 +9,11 @@ describe("Testing Navigation on HtmlForms", function() {
 
     before(function setupWebdriver(done) {
         var file_url = `file://${file_path}`;
-
         driver.get(file_url).then(done)
     })
-
-        after(function() {
-            driver.quit()
+    
+    after(function() {
+        driver.quit()
         })
 
         it('test case: assert that there are 2 links on this page', async () => {
