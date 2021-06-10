c677d811a99fb1d35f5d2d42cb98dcd52903693f
ireti203 <opteaoged@gmail.com>
2021-06-10 13:07:02 +0100

    resolve comments

--- tests/landing_page/landing_page.js
+++ tests/landing_page/landing_page.js
@@ -37,43 +37,49 @@ describe("Testing Scholars Project", function () {
             assert.include(text, "FoodBag", `Ensure "FoodBag" text is present`);
         });
 
-        it("test case: check service-title", async () => {
+       
+        it("test case: check Login", async () => {
             const element = await driver.findElement(By.css("body"))
             const text = await element.getText();
-            assert.include(text, "Anything food, we have you covered.", "Ensure Quantity label is present");
+            assert.include(text, "Login", "Ensure the `Login` link is present on the header");
         })
 
-        it("test case: check service-title", async () => {
+        it("test case: check Cart", async () => {
             const element = await driver.findElement(By.css("body"))
             const text = await element.getText();
-            assert.include(text, "Where are you?", "Ensure Quantity label is present");
+            assert.include(text, "Cart", "Ensure the `Cart` link is present on the header");
+        })
+        it("test case: check Sign up", async () => {
+            const element = await driver.findElement(By.css("body"))
+            const text = await element.getText();
+            assert.include(text, "Ensure the `Cart` link is present on the header");
         })
 
-        it("test case: quantity field is a text input field", async () => {
+        it("test case: check service-descrption1", async () => {
             const element = await driver.findElement(By.css("body"))
             const text = await element.getText();
             assert.include(text, "Pick an interest", "Ensure Quantity label is present");
         })
 
-        it("test case: check service-title", async () => {
+        it("test case: check service-description2", async () => {
             const element = await driver.findElement(By.css("body"))
             const text = await element.getText();
             assert.include(text, "How we serve you", "Ensure you have the appropriate text for the service");
         })
 
-        it("test case: check service title", async () => {
+        it("test case: check service title-1", async () => {
             const element = await driver.findElement(By.css("body"))
             const text = await element.getText();
             assert.include(text, "Tell us where you are", "Ensure you have the appropriate text for the service");
         })
 
-        it("test case: check service-title", async () => {
+        it("test case: check service-title-2", async () => {
             const element = await driver.findElement(By.css("body"))
             const text = await element.getText();
             assert.include(text, "Tell us what you want", "Ensure you have the appropriate text for the service");
         })
 
-        it("test case: check service-title", async () => {
+        it("test case: check service-title-3", async () => {
             const element = await driver.findElement(By.css("body"))
             const text = await element.getText();
             assert.include(text, "We'll come running", "Ensure you have the appropriate text for the service");
@@ -85,13 +91,13 @@ describe("Testing Scholars Project", function () {
             assert.include(text, "Contact us", "Ensure you have the appropriate text as seen on the mock-up");
         })
 
-        it("test case: check service-title", async () => {
+        it("test case: check terms & conditions on footer", async () => {
             const element = await driver.findElement(By.css("footer"))
             const text = await element.getText();
             assert.include(text, "Terms & Conditions . Privacy Policy", "Ensure the texts on the footer is properly formated");
         })
 
-        it("test case: check service-title", async () => {
+        it("test case: check copyright on footer", async () => {
             const element = await driver.findElement(By.css("footer"))
             const text = await element.getText();
             assert.include(text, "© Copyright 2021 FoodBag is a registered trademark", "Ensure you have the appropriate text as seen on the mock-up");
@@ -108,9 +114,14 @@ describe("Testing Scholars Project", function () {
             assert.equal(element, "Enter your delivery address", `Ensure you have the placeholder as seen in the mock-up`);
         });
 
-        it('test case: assert image amount is 2', async () => {
+        it('test case: assert placeholder', async () => {
+            const element = await driver.findElement(By.css('input')).getAttribute('label');
+            assert.equal(element, "Where are you?", `Ensure you have the input field as the appropriate label as seen in the mock-up`);
+        });
+
+        it('test case: assert image amount is 10', async () => {
             const images = await driver.findElements(By.css('img'));
             const size = images.length
-            assert.equal(size, 7, `Ensure all the images are present in the page`);
+            assert.equal(size, 10, `Ensure all the images are present on the page`);
         });
 })
