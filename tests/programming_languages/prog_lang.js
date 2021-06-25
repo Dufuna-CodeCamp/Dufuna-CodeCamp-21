389051ccf7ed286dda301adee6d4ba22495ac3b4
ireti203 <opteaoged@gmail.com>
2021-06-25 15:49:38 +0100

    programming link test

---
+++ tests/programming_languages/prog_lang.js
@@ -0,0 +1,24 @@
+var file_path = process.env.FILE_PATH
+var webdriver = require('selenium-webdriver');
+var assert = require('chai').assert;
+var { Builder, By } = require("selenium-webdriver");
+
+describe("Testing Navigation on HtmlForms", function() {
+    this.timeout(3 * 1000 * 60);
+    var driver = new webdriver.Builder().forBrowser('chrome').build()
+
+    before(function setupWebdriver(done) {
+        var file_url = `file://${file_path}`;
+        driver.get(file_url).then(done)
+    })
+    
+    after(function() {
+        driver.quit()
+    })
+
+    it('test case: assert that the url to the document is present', async () => {
+        const body = await driver.findElements(By.css('body'));
+        const size = body.getText();
+        assert.equal(size, "https://docs.google.com/spreadsheets", `Ensure you habe the link to the google doc containinf the results from your research`);
+    });
+ })
