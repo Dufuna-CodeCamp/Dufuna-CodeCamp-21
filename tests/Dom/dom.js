0f96ab0f9f4bc1a4f6cf2fa1248767704fac3998
ireti203 <opteaoged@gmail.com>
2021-07-06 16:31:40 +0100

    test script for Dom task

---
+++ tests/Dom/dom.js
@@ -0,0 +1,46 @@
+var file_path = process.env.FILE_PATH;
+var webdriver = require('selenium-webdriver');
+var Select = require('selenium-webdriver');
+var assert = require('chai').assert;
+var { Builder, By } = require("selenium-webdriver");
+
+
+describe("Testing Scholars Project", function () {
+    this.timeout(3 * 1000 * 60);
+    var driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .build();
+
+    before(function setupWebdriver(done) {
+        var file_url = `file://${file_path}`;
+        driver.get(file_url).then(done);
+    });
+
+    after(function () {
+        driver.quit();
+    });
+
+
+    it('test case: check dropdown', async () => {
+        const element = await driver.findElement(By.css("select")).isDisplayed();
+        assert(element == true)
+    });
+
+    it('test case: check options in dropdown', async () => {
+        driver.findElement(By.css("select")).click();
+        const option = await driver.findElement(By.css("option")).isDisplayed();
+        assert(option == true)
+    });
+
+    it('test case: check options in dropdown', async () => {
+        driver.findElement(By.css("select")).click();
+        const options = await driver.findElements(By.css("option"));
+        options.forEach(validateImage);
+        async function validateImage(item, arr) {
+            arr[3] = item.click();
+            const images = await driver.findElements(By.css("img"));
+            const validate = images.length
+            assert.equal(validate, 10, `Ensure the images change when you click on any option`)
+        }
+    });
+});
