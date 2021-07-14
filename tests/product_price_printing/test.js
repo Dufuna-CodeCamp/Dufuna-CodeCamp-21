a2b02673390b3fef626f35828c9920bb105e3aee
ireti203 <opteaoged@gmail.com>
2021-07-14 15:00:12 +0100

    fixed javascript test

--- tests/product_price_printing/test.js
+++ tests/product_price_printing/test.js
@@ -1,9 +1,13 @@
 var file_path = process.env.FILE_PATH
+var assert = require('chai').assert;
+const {By} = require("selenium-webdriver");
 const webdriver = require("selenium-webdriver");
 
 describe("Printing product price", function () {
     this.timeout(3 * 1000 * 60)
-    var driver = new webdriver.Builder().forBrowser('chrome').build()
+    var driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .build();
 
     before(function setupWebdriver(done) {
         var file_url = `file://${file_path}`;
@@ -14,8 +18,16 @@ describe("Printing product price", function () {
          driver.quit()
     })
 
-    it("prints product price", () => {
-        var productList = [{ productName: "a", imageUrl: "b", productPrice: "4" }, { productName: "a", imageUrl: "b", productPrice: "4" }, { productName: "a", imageUrl: "b", productPrice: "4" }]
-        console.log(productList)
+    it("test case: check product details", async () => {
+        const element = await driver.findElement(By.css("body"))
+        const text = await element.getText();
+        assert.include(text, "is sold for", "Ensure the `Cart` link is present on the header");
+    })
+
+    it("test case: check Sign up", async () => {
+        const element = await driver.findElement(By.css("body"))
+        const text = await element.getText();
+        const count = text.split("is sold for").length-1;
+        assert.equal(count, 3, "Ensure the `Sign up` link is present on the header");
     })
 })
