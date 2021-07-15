d4cadd2638500bcd7b8ff14b9673ed390526daff
ireti203 <opteaoged@gmail.com>
2021-07-15 11:42:09 +0100

    resolve comments

--- tests/conditionals_js/conditionals_test.js
+++ tests/conditionals_js/conditionals_test.js
@@ -1,40 +1,39 @@
-var file_path = process.env.FILE_PATH
+const file_path = process.env.FILE_PATH
 const webdriver = require('selenium-webdriver');
 const assert = require('chai').assert;
-var { Builder, By } = require("selenium-webdriver");
 
 describe("Testing Product Projects", function () {
     this.timeout(3 * 1000 * 60);
-    var driver = new webdriver.Builder()
+    const driver = new webdriver.Builder()
         .forBrowser('chrome')
         .build();
 
-        before(function setupWebdriver(done) {
-            var file_url = `file://${file_path}`;
-            driver.get(file_url).then(done);
-        });
+    before(function setupWebdriver(done) {
+        const file_url = `file://${file_path}`;
+        driver.get(file_url).then(done);
+    });
 
-        after(function() {
-            driver.quit();
-        });
+    after(function () {
+        driver.quit();
+    });
 
     it('test case: Prints out the value of array', async () => {
         const productList = driver.wait(webdriver.until.elementLocated(webdriver.By.css("body")));
         const text = await productList.getText();
-        let x= 123 -7;
+        const x = 123 - 7;
         for (let i = 1; i <= x; i++) {
-          if (i % 3 === 0 && i % 5 === 0)  {      
-        assert.include(text, 'Software Developer' );
-          }
-          else if (i % 3 === 0) {
-            assert.include(text, 'Software');
-        }
-        else if (i % 5 === 0) {
-          assert.include(text, 'Developer');
-      }
-      else {
-        assert.include(text, i)
-    }
+            if (i % 3 === 0 && i % 5 === 0) {
+                assert.include(text, 'Software Developer');
+            }
+            else if (i % 3 === 0) {
+                assert.include(text, 'Software');
+            }
+            else if (i % 5 === 0) {
+                assert.include(text, 'Developer');
+            }
+            else {
+                assert.include(text, i)
+            }
         }
     });
 });
