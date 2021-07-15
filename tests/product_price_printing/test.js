51c491dfe39d50f416e4285b8443a8cd248be02f
ireti203 <opteaoged@gmail.com>
2021-07-15 11:54:29 +0100

    resolve comment

--- tests/product_price_printing/test.js
+++ tests/product_price_printing/test.js
@@ -1,21 +1,21 @@
-var file_path = process.env.FILE_PATH
-var assert = require('chai').assert;
-const {By} = require("selenium-webdriver");
+const file_path = process.env.FILE_PATH
+const assert = require('chai').assert;
+const { By } = require("selenium-webdriver");
 const webdriver = require("selenium-webdriver");
 
 describe("Printing product price", function () {
     this.timeout(3 * 1000 * 60)
-    var driver = new webdriver.Builder()
+    const driver = new webdriver.Builder()
         .forBrowser('chrome')
         .build();
 
     before(function setupWebdriver(done) {
-        var file_url = `file://${file_path}`;
+        const file_url = `file://${file_path}`;
         driver.get(file_url).then(done)
     })
 
     after(function () {
-         driver.quit()
+        driver.quit()
     })
 
     it("test case: check product details", async () => {
@@ -27,7 +27,7 @@ describe("Printing product price", function () {
     it("test case: check Sign up", async () => {
         const element = await driver.findElement(By.css("body"))
         const text = await element.getText();
-        const count = text.split("is sold for").length-1;
+        const count = text.split("is sold for").length - 1;
         assert.equal(count, 3, "Ensure the `Sign up` link is present on the header");
     })
 })
