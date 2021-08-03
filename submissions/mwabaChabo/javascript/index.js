cf1a60176ec14ef8f522846129432e75516e7ba5
mchabo <60313051+mchabo@users.noreply.github.com>
2021-08-03 13:59:12 +0200

    added a strict operator

--- submissions/mwabaChabo/javascript/index.js
+++ submissions/mwabaChabo/javascript/index.js
@@ -5,11 +5,11 @@ var z= 123;
 var f= (z - y);
 while (x < f) {
     x += 1;
-    if ((x%3==0) && (x%5==0)) {
+    if ((x%3===0) && (x%5===0)) {
     document.write("software developer");
-} else if (x%3==0) {
+} else if (x%3===0) {
     document.write("software");
-} else if (x%5==0){
+} else if (x%5===0){
     document.write("developer");
 }
 }
