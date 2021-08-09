ba65aecdb98ec31c4327a04626cfcef04c191b1f
mchabo <60313051+mchabo@users.noreply.github.com>
2021-08-09 12:00:31 +0200

    made changes

--- submissions/mwabaChabo/javascript/index.js
+++ submissions/mwabaChabo/javascript/index.js
@@ -3,12 +3,12 @@ var positiveDifference=123 -7;
 
 for (var x=1; x<=positiveDifference; x++){
     if((x%3===0)&& (x%5===0)){
-        document.write("Software developer")
+        document.write("Software Developer")
     }
     else if(x%3===0){
         document.write("Software")
     }
-    else if (x%5===0){
+    else if ( x%5===0){
         document.write("Developer")
     }
 }
