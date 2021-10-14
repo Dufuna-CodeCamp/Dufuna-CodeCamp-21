0655cdb08f43a38f9784316bed3c93283e61c24a
Babatunde <tundeoladejo@outlook.com>
2021-10-14 23:42:50 +0100

    dsa-task

---
+++ tests/product_algorithm/product_algorithm.js
@@ -0,0 +1,25 @@
+const file_path = process.env.FILE_PATH;
+const { products, product_price} = require(file_path);
+const expect = require("chai").expect;
+
+describe("DSA Algorithm 1", function () {
+    it("check length of products", function () {
+        expect(products.length).to.equal(20);
+    });
+
+    it("check length of product price", function () {
+        expect(product_price.length).to.equal(20);
+    });
+
+    it("check products data type", function () {
+        for (i = 0; i < products.length; i++) {
+            expect(products[i]).to.be.a("string");
+        }
+    });
+
+    it("check product price data type", function () {
+        for (i = 0; i < product_price.length; i++) {
+            expect(product_price[i]).to.be.a("number");
+        }
+    });
+});
