ad28defe2531f7ede4969dca01e9f1c33684dfdd
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-10-26 18:02:03 +0100

    changes made

--- submissions/chidubemOnuoha/JavaScript/js_product_price.js
+++ submissions/chidubemOnuoha/JavaScript/js_product_price.js
@@ -1,29 +1,24 @@
-const productList = [
-  
-    {        
-      productName: "<h1>Rice flavour </h1>",
-     imageURL: '<img src="../images/flavour.svg">',
-      productPrice: "#900"
-    },
-    {        
-      productName: "<h1>chiken flavour</h1>",
-      imageURL: '<img src="../images/chicken.svg">',
-      productPrice: "#800"
-    },
-
-    {        
-        productName: "<h1>chiken flavour</h1>",
-        imageURL: '<img src="../images/chicken.svg">',
-        productPrice: "#800"
-
-      },
-   
-]
-
-const total =  productList.forEach(function (product ){
-    document.write(`${product.productName}  ${product.imageURL}  is sold for:  ${product.productPrice}`);
-});
+var productList = new Array({
+  productName : "product1",
+  imageUrl : "http://localhost:5000/p1",
+  productPrice : 1000
+},{
+  productName : "product2",
+  imageUrl : "http://localhost:5000/p2",
+  productPrice : 2000
+},{
+  productName : "product3",
+  imageUrl : "http://localhost:5000/p3",
+  productPrice : 3000
+})
 
 
+var value;
 
+function printPrice() {
+for(let i=0; i<=productList.length; i++){
 
+  document.write(productList[i]['productName'] + " with " + productList[i]['imageUrl'] + " is sold for " + productList[i]['productPrice'] + " ")
+}
+}
+printPrice();
