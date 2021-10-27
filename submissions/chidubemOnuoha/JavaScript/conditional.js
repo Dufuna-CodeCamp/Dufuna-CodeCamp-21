123786211d00a5ba878a3e52aa37a27fc96fb12e
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-10-27 22:32:19 +0100

    adding conditional code

--- submissions/chidubemOnuoha/JavaScript/conditional.js
+++ submissions/chidubemOnuoha/JavaScript/conditional.js
@@ -1,3 +1,21 @@
-var conditional = 'my conditional'
+function multiplyFunc() {
+    let innitialCount = 1;
+    let mainCount = 7;
+    let endCount = 123;
 
-console.log(conditional);
+     let posiDeff = endCount - mainCount
+    
+    for(let counter = innitialCount ; counter <= posiDeff ;counter++) {
+        if(counter === 3) {
+          document.write('<br> Software instead of the number')
+        } else if (counter === 5) {
+            document.wwrite(' <br> Developr instead of the number</h1>')
+        } else if (counter === 3 && counter === 5) {
+             document.write('<br> Software Developer </h1>')
+        } else{
+            document.write(' <br> <br>Deverloper')
+        }
+    }
+}
+
+multiplyFunc()
