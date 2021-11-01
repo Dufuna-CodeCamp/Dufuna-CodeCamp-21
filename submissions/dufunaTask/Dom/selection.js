80f08863d03acf3089564710890fafd365d8cff9
ireti203 <opteaoged@gmail.com>
2021-11-01 08:29:37 +0100

    update

--- submissions/dufunaTask/Dom/selection.js
+++
@@ -1,7 +0,0 @@
-var elem = document.getElementById("select_service");
-elem.onchange = function(){
-    var hiddenDiv = document.getElementById("active");
-    var showDiv = document.getElementById('inactive');
-    hiddenDiv.style.display = (this.value == "") ? "none":"flex";
-    showDiv.style.display = (this.value == "") ? "flex":"none";
-};
