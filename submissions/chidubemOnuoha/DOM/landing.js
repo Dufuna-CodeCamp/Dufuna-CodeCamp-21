acc32b88c5a788801eb0557c1e3cfbf2f7624407
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-11-01 18:42:43 +0100

    logo file added

--- submissions/chidubemOnuoha/DOM/landing.js
+++ submissions/chidubemOnuoha/DOM/landing.js
@@ -1,43 +1,67 @@
-var userSelection =  document.getElementById("select-state");
+var userSelection = document.getElementById("select-state");
 var errorBlock = document.querySelector(".error-block");
 var submitForm = document.getElementById('submitForm')
 var submitForm1 = document.getElementById('submitForm1')
 var submitForm2 = document.getElementById('submitForm2')
 var submitForm13 = document.getElementById('submitForm3')
 
-function Validate(){
-    if(userSelection.value == "") {
-      errorBlock.innerHTML = "Please select your prefer location!"
+function Validate() {
+    if (userSelection.value == "") {
+        errorBlock.innerHTML = "Please select your prefer location!"
 
-      return false
-    } else{
+        return false
+    } else {
         errorBlock.innerHTML = "";
         return true;
     }
 
-    
+
 }
 
-function SubmitInput(){
+function SubmitInput() {
     return Validate();
-    
+
 }
 
-submitForm.addEventListener('click' ,() => {
-    location.href = "./vendor.html";    
+submitForm.addEventListener('click', () => {
+    if (userSelection.value == "") {
+        errorBlock.innerHTML = "please select your prefer location"
+        return false;
+    } else {
+        location.href = "./vendor.html";
+        return true;
+    }
 })
 
 
-submitForm1.addEventListener('click' ,() => {
-    location.href = "./vendor.html";    
+submitForm1.addEventListener('click', () => {
+    if (userSelection.value == "") {
+        errorBlock.innerHTML = "please select your prefer location"
+        return false;
+    } else {
+        location.href = "./vendor.html";
+        return true;
+    }
 })
 
-submitForm2.addEventListener('click' ,() => {
-    location.href = "./vendor.html";    
+submitForm2.addEventListener('click', () => {
+    if (userSelection.value == "") {
+        errorBlock.innerHTML = "please select your prefer location"
+        return false;
+    } else {
+        location.href = "./vendor.html";
+        return true;
+    }
 })
 
-submitForm3.addEventListener('click' ,() => {
-    location.href = "./vendor.html";    
+submitForm3.addEventListener('click', () => {
+    if (userSelection.value == "") {
+        errorBlock.innerHTML = "please select your prefer location"
+        return false;
+    } else {
+        location.href = "./vendor.html";
+        return true;
+    }
 })
 
 
