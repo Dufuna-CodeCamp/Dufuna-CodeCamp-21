5e53831b88a9f161fe2116a43e44e1df12de7ae8
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-11-05 15:22:45 +0100

    dom2 code

--- submissions/chidubemOnuoha/DOM/vendor.js
+++ submissions/chidubemOnuoha/DOM/vendor.js
@@ -1,34 +1,40 @@
 const showCart = document.querySelector('.add-to-cart');
 const firstButton = document.querySelector('#add-info');
+const toggleButton = document.querySelector('#add-info');
 const firstButton2 = document.querySelector('#add-info2');
 const firstButton3 = document.querySelector('#add-info3');
 const firstButton4 = document.querySelector('#add-info4');
-const remveInfo = document.querySelector('#remove-info')
-const itemOne = document.querySelector('.item-one')
+const secondButton = document.querySelector('.second-one')
+const secondButton1 = document.querySelector('.second-one1')
+const secondButton2 = document.querySelector('.second-one2')
+const secondButton3 = document.querySelector('.second-one3')
 
 
 
-function addToCart (e)  {
-    e.preventDefault(); 
-        showCart.innerHTML = '<div style="background-color: red; width: 17px; height:17px; border-radius: 50px"; margin-top: 30px; ></div>'
-        
-}
 
-function removeCart(e) {
-    e.preventDefault();
-    showCart.innerHTML = ''
-    
-    
+function addToCart(id) {
+  showCart.innerHTML = '<div style="background-color: red; width: 17px; height:17px; border-radius: 50px"; margin-top: 30px; ></div>'
 }
 
 
-firstButton.addEventListener('click', addToCart)
-firstButton2.addEventListener('click', addToCart)
-firstButton3.addEventListener('click', addToCart)
-firstButton4.addEventListener('click', addToCart)
+function toggleCart(e) {
+  e.preventDefault()
+   
+   addToCart();
+     
+}
 
 
-removInfo.addEventListener('click', removeCart)
+firstButton.addEventListener('click', toggleCart)
+firstButton2.addEventListener('click', toggleCart)
+firstButton3.addEventListener('click', toggleCart)
+firstButton4.addEventListener('click', toggleCart)
+secondButton.addEventListener('click', toggleCart)
+secondButton1.addEventListener('click', toggleCart)
+secondButton2.addEventListener('click', toggleCart)
+secondButton3.addEventListener('click', toggleCart)
+
+
 
 
 
