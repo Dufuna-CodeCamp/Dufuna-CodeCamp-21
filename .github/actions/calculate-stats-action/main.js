2ccd11293e24cdbc653fe8bb7d67eedd29cd43fc
Babatunde <tundeoladejo@outlook.com>
2021-11-08 13:18:16 +0100

    react github review quick fix

--- .github/actions/calculate-stats-action/main.js
+++ .github/actions/calculate-stats-action/main.js
@@ -12,6 +12,18 @@ try {
     core.setOutput("stats-output", status);
 
 } catch(error) {
-    core.setFailed(error.message);
-    console.log(`Error parsing JSON string: ${error}`);
+    try {
+        const fileContent = core.getInput("log-file-content");
+        const content = JSON.parse(fileContent);
+        const testPasses = content.numPassedTests;
+        const testFails = content.numFailedTests; 
+
+        const status = testFails > 0 ? "FAIL" : "PASS";
+        console.log(`status is ${status}`);
+        core.setOutput("stats-output", status);
+
+    } catch(error) {
+        core.setFailed(error.message);
+        console.log(`Error parsing JSON string: ${error}`);
+    }
 }
