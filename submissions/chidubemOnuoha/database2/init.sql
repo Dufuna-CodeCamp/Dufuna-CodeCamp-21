6f6e4c5734a416dee33b24035d89d0d136fa9deb
Onuoha Collins <onuohacollinschidubem@gmail.com>
2021-12-04 22:11:35 +0100

    adding varchar

--- submissions/chidubemOnuoha/database2/init.sql
+++ submissions/chidubemOnuoha/database2/init.sql
@@ -7,9 +7,9 @@ USE transport_company;
 
 CREATE TABLE  passengers (
     id NOT NULL AUTO_INCREMENT PRIMARY key,
-    full_name VARCHAR NOT NULL,
+    full_name VARCHAR(100) NOT NULL,
     passengers_id INT NOT NULL age VARCHAR,
-    sex VARCHAR NOT NULL, 
+    sex VARCHAR(10) NOT NULL, 
     created_at DATETIME,
     
 );
@@ -17,12 +17,12 @@ CREATE TABLE  passengers (
 --creating a passengers detail table
  CREATE TABLE passengers_details (
      id INT NOT NULL AUTO_INCREMENT  FOREIGN KEY (passengers_id),
-     passenger_class VARCHAR NOT NULL,
-     ticket_number VARCHAR NOT NULL,
-     trip_fare VARCHAR NOT NULL,
-     assigned_cabin VARCHAR NOT NULL,
-     number_passengers VARCHAR NULL,
-     number_sibling  VARCHAR NULL,
+     passenger_class VARCHAR(100) NOT NULL,
+     ticket_number VARCHAR(50) NOT NULL,
+     trip_fare VARCHAR(50) NOT NULL,
+     assigned_cabin VARCHAR(20) NOT NULL,
+     number_passengers VARCHAR(20) NULL,
+     number_sibling  VARCHAR(50) NULL,
      created_at DATETIME,
      passenger_id INT NOT NULL,
      
@@ -33,9 +33,9 @@ CREATE TABLE  passengers (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY (id) FOREIGN KEY (passengers_id),
     passenger_id INT NOT NULL AUTO_INCREMENT,
     accident_id INT NOT NULL
-    ticket_number VARCHAR NOT NULL,
-    assigned_cabin VARCHAR NOT NULL,
-    passenger_class VARCHAR  NOT NULL,
+    ticket_number VARCHAR(50) NOT NULL,
+    assigned_cabin VARCHAR(100) NOT NULL,
+    passenger_class VARCHAR(100)  NOT NULL,
     created_at DATETIME
     FOREIGN key(passengers_id)
     
