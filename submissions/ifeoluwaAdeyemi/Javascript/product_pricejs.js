fc2bf3d3a14b1fa58d748b8a96b76c6f09d2e7d2
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2021-12-09 23:34:32 +0100

    i have made new corrections

--- submissions/ifeoluwaAdeyemi/Javascript/product_pricejs.js
+++ submissions/ifeoluwaAdeyemi/Javascript/product_pricejs.js
@@ -2,25 +2,29 @@
 let productList = [
     {
         productName: "Hand Bag",
-        imageUrl: "https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFnfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fbag&psig=AOvVaw0XonbfHtWxhbNWwQWgUpbQ&ust=1638996808686000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDM1ofJ0vQCFQAAAAAdAAAAABAD/",
         productPrice: "$800"
     },
 
     {
         productName: "Back Pack",
-        imageUrl: "https://media.istockphoto.com/photos/red-backpack-standing-on-white-background-picture-id184797262?k=20&m=184797262&s=612x612&w=0&h=o3jcLwH3JMq2aa7ouf4r8v87DApz5qnYQNEecGv7nKA=",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fschool-bag&psig=AOvVaw0XonbfHtWxhbNWwQWgUpbQ&ust=1638996808686000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDM1ofJ0vQCFQAAAAAdAAAAABAY/",
         productPrice: "$600"
     },
 
     {
         productName: "Tote Bag",
-        imageUrl: "https://images.unsplash.com/photo-1544816155-12df9643f363?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJhZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
+        imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fschool-bag&psig=AOvVaw0XonbfHtWxhbNWwQWgUpbQ&ust=1638996808686000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDM1ofJ0vQCFQAAAAAdAAAAABAY/",
         productPrice: "$550"
     }
 ];
 
-productList.forEach(function(item){
-    document.write(`${item.productName}${item.imageUrl} sells for ${item.productPrice}`);
-  });
+productList.forEach((product) => {
+    document.write(
+        `<p>${product.productName} with ${product.imageUrl} is sold for ${product.productPrice}</p>`
+    );
+});
+
+console.log(productList);
 
 
