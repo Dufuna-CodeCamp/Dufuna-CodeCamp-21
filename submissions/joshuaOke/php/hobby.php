efec09f005d75cb25228116d29551106955573b7
Yeshua7dm <yeshua7dm@gmail.com>
2021-12-10 21:52:55 +0100

    [DCSFUS-94] Hobby Arrays

--- submissions/joshuaOke/php/hobby.php
+++ submissions/joshuaOke/php/hobby.php
@@ -3,7 +3,13 @@
   $names = ['Bola', 'Bayo', 'Bourne','Jason','John','Johanna','King','Kingsley'];
   $hobbies = ['cooking', 'sweeping', 'singing', 'dancing', 'running', 'football', 'books', 'keyboards'];
 
-  for ($i=0; $i < count($names); $i++) { 
-    echo "My name is $names[$i].";
-    echo "I love $hobbies[$i].<br>";
+  // for ($i=0; $i < count($names); $i++) { 
+  //   echo "My name is $names[$i].";
+  //   echo "I love $hobbies[$i].<br>";
+  // }
+
+  foreach ($names as $name) {
+    foreach ($hobbies as $hobby) {
+      echo "My name is $name. I love $hobby. \n";
+    }
   }
