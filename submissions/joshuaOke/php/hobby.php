40c50b5b2615ce14d2a38f97519af6854d38ae04
Yeshua7dm <yeshua7dm@gmail.com>
2021-12-11 09:50:03 +0100

    [DCSFUS-88] PHP Unit Converter

--- submissions/joshuaOke/php/hobby.php
+++ submissions/joshuaOke/php/hobby.php
@@ -1,14 +1,7 @@
 <?php
-  $names = ['Bola', 'Bayo', 'Bourne','Jason','John','Johanna','King','Kingsley'];
-  $hobbies = ['cooking', 'sweeping', 'singing', 'dancing', 'running', 'football', 'books', 'keyboards'];
+$names = array('Bola', 'Bayo', 'Bourne','Jason','John','Johanna','King','Kingsley');
+  $hobbies = array('cooking', 'sweeping', 'singing', 'dancing', 'running', 'football', 'books', 'keyboards');
 
-  // for ($i=0; $i < count($names); $i++) { 
-  //   echo "My name is $names[$i].";
-  //   echo "I love $hobbies[$i].<br>";
-  // }
-
-  foreach ($names as $name) {
-    foreach ($hobbies as $hobby) {
-      echo "My name is $name. I love $hobby. \n";
-    }
+  for ($i=0; $i < count($names); $i++) { 
+    echo "My name is $names[$i]. I love $hobbies[$i].<br>";
   }
