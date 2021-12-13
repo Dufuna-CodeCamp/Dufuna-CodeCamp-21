e6a250794dc40a92d575f3b87c51040926eac5f6
FabunmiTolulopeEniola <70230223+ToluEnny24@users.noreply.github.com>
2021-12-13 16:15:27 +0100

    Revert "[DGPFET-2] Develop a product Page using HTML"

--- tests/product_page_using_html/html_task.js
+++ tests/product_page_using_html/html_task.js
@@ -1,43 +1,45 @@
 var file_path = process.env.FILE_PATH;
-var webdriver = require("selenium-webdriver");
-var assert = require("chai").assert;
+var webdriver = require('selenium-webdriver');
+var assert = require('chai').assert;
 var { Builder, By } = require("selenium-webdriver");
 
 describe("Testing Scholars Project", function () {
-  this.timeout(3 * 1000 * 60);
-  const driver = new webdriver.Builder().forBrowser("chrome").build();
+    this.timeout(3 * 1000 * 60);
+    var driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .build();
 
-  before(function setupWebdriver(done) {
-    var file_url = `file://${file_path}`;
-    driver.get(file_url).then(done);
-  });
+    before(function setupWebdriver(done) {
+        var file_url = `file://${file_path}`;
+        driver.get(file_url).then(done);
+    });
 
-  after(function () {
-    driver.quit();
-  });
+    after(function() {
+        driver.quit();
+    });
 
-  it("test case: check if Price is present", async () => {
-    const element = await driver.findElement(By.css("body"));
-    const text = await element.getText();
-    const price = text.toLowerCase();
-    assert.include(price, "price", `Ensure Price is present`);
-  });
+    it("test case: check if Price is present", async () => {
+        const element = await driver.findElement(By.css("body"));
+        const text = await element.getText();
+        const price = await text.toLowerCase();
+        assert.include(price, "price", `Ensure Price is present`);
+    });
 
-  it("test case: check if Description is present", async () => {
-    const element = await driver.findElement(By.css("body"));
-    const text = await element.getText();
-    const description = text.toLowerCase();
-    assert.include(description, "description", `Ensure DESCRIPTION is present`);
-  });
+    it("test case: check if Description is present", async () => {
+        const element = await driver.findElement(By.css("body"));
+        const text = await element.getText();
+        const description = await text.toLowerCase();
+        assert.include(description, "description", `Ensure DESCRIPTION is present`);
+    });
 
-  it("test case: assert image amount is 2", async () => {
-    const images = await driver.findElements(By.css("img"));
-    const size = images.length;
-    assert.equal(size, 2, `Amount of images on the page not equal to 2`);
-  });
+    it('test case: assert image amount is 2', async () => {
+        const images = await driver.findElements(By.css('img'));
+        const size = images.length
+        assert.equal(size, 2, `Amount of images on the page not equal to 2`);
+    });
 
-  it("test case: ordered list available", async function () {
-    const list = (await driver).findElements(webdriver.By.id("ol"));
-    assert.notStrictEqual(list.length, 0, "No ordered list on this page");
-  });
+    it('test case: ordered list available', async function () {
+        const list = (await driver).findElements(webdriver.By.id('ol'));
+        assert.notStrictEqual(list.length, 0, 'No ordered list on this page');
+    });
 });
