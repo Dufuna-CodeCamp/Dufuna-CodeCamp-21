1e3c5e697d6f4ae07af599b56fdd40d11f25c1d2
mauricebuchi <mauricejahwizwid@gmail.com>
2022-01-11 13:38:30 +0100

    first test

---
+++ submissions/mauriceOkeke/Dom/vendorPage.js
@@ -0,0 +1,30 @@
+let ctaBtn = document.getElementsByTagName("button");
+let dot = document.querySelector(".dot");
+
+for (let btn of ctaBtn) {
+  btn.addEventListener("click", function () {
+    if (btn.textContent == "Add to Cart") {
+      btn.style.background = "gray";
+      btn.style.color = "rgba(255, 255, 255, 1)";
+      btn.style.border = "gray";
+      btn.innerHTML =
+        '<img src = "../images/remove.svg" class = "remove" style=" width:20px; padding-right:2px" />Remove';
+      btn.style.display = "flex";
+      btn.style.alignItems = "center";
+    } else if (btn.innerText == "Remove") {
+      btn.style.background = "#fff";
+      btn.style.color = "#f00";
+      btn.style.border = "1px solid red";
+      btn.innerHTML = "Add to Cart";
+      dot.innerText--;
+    }
+
+    let cartDotCount = document.querySelectorAll(".remove").length;
+    if (cartDotCount > 0) {
+      dot.style.display = "flex";
+    } else {
+      dot.style.display = "none";
+    }
+    dot.innerText = cartDotCount;
+  });
+}
