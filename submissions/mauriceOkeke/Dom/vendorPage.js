88b4c518b22db86987eff00a1d11f743b52208b6
mauricebuchi <mauricejahwizwid@gmail.com>
2022-01-11 14:21:59 +0100

    rerun tests

--- submissions/mauriceOkeke/Dom/vendorPage.js
+++ submissions/mauriceOkeke/Dom/vendorPage.js
@@ -4,17 +4,17 @@ let dot = document.querySelector(".dot");
 for (let btn of ctaBtn) {
   btn.addEventListener("click", function () {
     if (btn.textContent == "Add to Cart") {
-      btn.style.background = "gray";
+      btn.style.backgroundColor = "#A5A5A5";
       btn.style.color = "rgba(255, 255, 255, 1)";
-      btn.style.border = "gray";
+      btn.style.border = "none";
       btn.innerHTML =
-        '<img src = "../images/remove.svg" class = "remove" style=" width:20px; padding-right:2px" />Remove';
+        '<img src = "../images/remove.svg" class = "remove" style=" width:20px; padding-right:2px; color:#fff" />Remove';
       btn.style.display = "flex";
       btn.style.alignItems = "center";
     } else if (btn.innerText == "Remove") {
-      btn.style.background = "#fff";
-      btn.style.color = "rgba(255, 0, 0, 1)";
-      btn.style.border = "1px solid red";
+      btn.style.backgroundColor = "#fff";
+      btn.style.color = "rgba(255, 255, 255, 1)";
+      btn.style.border = "1px solid rgba(255, 255, 255, 1)";
       btn.innerHTML = "Add to Cart";
     }
 
