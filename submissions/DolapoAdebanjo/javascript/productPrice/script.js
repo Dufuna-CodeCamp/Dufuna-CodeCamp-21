bfbb2e39926781915aa02ec639386b843dee2abb
Dolapo Adebanjo <dolapomola@gmail.com>
2022-01-16 20:36:31 +0100

    test file

--- submissions/DolapoAdebanjo/javascript/productPrice/script.js
+++ submissions/DolapoAdebanjo/javascript/productPrice/script.js
@@ -22,3 +22,6 @@ for (let i = 0; i <= products.length; i++) {
     `<p>${product.productName} with ${product.imageUrl} is sold for ${product.productPrice}.</p> <br>`
   );
 };
+/* sold for;
+sold for;
+*/
