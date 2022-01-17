9a1423883db3752f4d2695bc838697f3999e4d62
mauricebuchi <mauricejahwizwid@gmail.com>
2022-01-17 17:12:17 +0100

    change to files

--- submissions/mauriceOkeke/database3/init.sql
+++ submissions/mauriceOkeke/database3/init.sql
@@ -16,7 +16,8 @@ SELECT COUNT(*) FROM travels_csv WHERE Survived = 0;
 SELECT names.Name, names.Sex FROM names JOIN train_csv_1 ON names.PassengerId = train_csv_1.PassengerId WHERE names.Age < 27 AND (train_csv_1.Embarked = 'Q' || train_csv_1.Embarked = 'C');
 
 -- Surivals that embarked at Southampton
-
+SELECT COUNT(train_csv_1.Embarked) FROM train_csv_1 INNER JOIN travels_csv ON train_csv_1.PassengerId = travels_csv.PassengerId WHERE train_csv_1.Embarked = 'S' AND travels_csv.Survived = 1;
+218
 
 -- id, name and total of passengers who paid higer than $100 and above 35 years that has siblings or spouses on board
 SELECT names.PassengerId,names.Name, COUNT(train_csv_1.Fare) FROM names INNER JOIN train_csv_1 ON names.PassengerId = train_csv_1.PassengerId WHERE train_csv_1.Fare > 100 AND names.Age > 35 AND train_csv_1.SibSp > 0;
