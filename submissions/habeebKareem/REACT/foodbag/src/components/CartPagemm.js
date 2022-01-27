2764d5cb2e669b92423dc779f3d4405fef0816dc
olaide-hok <olaidekareem43@yahoo.com>
2022-01-27 04:40:56 +0100

    create react app

---
+++ submissions/habeebKareem/REACT/foodbag/src/components/CartPagemm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CartPage() {
+    return (
+        <div>
+            <h2>Cart</h2>
+            <span>0</span>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <button>Checkout</button>
+            
+        </div>
+    )
+}
+
+export default CartPage;
