fbb01a74d885af7dcb9cd9d3a97936e4f54b6487
Dolapo Adebanjo <dolapomola@gmail.com>
2022-02-10 17:52:04 +0100

    vendor assignment and test

--- submissions/DolapoAdebanjo/DOM/vendor.js
+++ submissions/DolapoAdebanjo/DOM/vendor.js
@@ -4,19 +4,22 @@ let notifyCount = 0;
 
 for (let button of clickAnchor) {
   console.log(button);
-  const clickAdd = button.firstElementChild;
-  const clickRemove = button.lastElementChild;
 
   button.addEventListener("click", function () {
     if (button.innerText === "Add to Cart") {
-      clickAdd.style.display = `none`;
-      clickRemove.style.display = `inline-block`;
+      button.style.backgroundColor = "#A5A5A5";
+      button.style.color = "rgba(255, 255, 255, 1)";
+      button.style.border = "none";
+      button.innerHTML =
+        '<img src = "../assets/remove.svg" class = "remove"/>Remove';
       notify.style.display = `inline-block`;
       notifyCount++;
       notify.innerText = notifyCount;
     } else if (button.innerText === "Remove") {
-      clickAdd.style.display = `inline-block`;
-      clickRemove.style.display = `none`;
+      button.style.backgroundColor = "#fff";
+      button.style.color = "#ad4c4c";
+      button.style.border = "1px solid #ad4c4c";
+      button.innerText = "Add to Cart";
       notifyCount--;
       notify.innerText = notifyCount;
     }
