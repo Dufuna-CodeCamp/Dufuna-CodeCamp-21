4e7dfd96ad86e613496cce37cdf66f8b723e354c
Popoola Mariam <popoolamariam05@gmail.com>
2022-02-11 15:59:57 -0700

    my landing page

---
+++ tests/casperjs.cmd
@@ -0,0 +1,17 @@
+@ECHO off
+SETLOCAL
+CALL :find_dp0
+
+IF EXIST "%dp0%\python.exe" (
+  SET "_prog=%dp0%\python.exe"
+) ELSE (
+  SET "_prog=python"
+  SET PATHEXT=%PATHEXT:;.JS;=;%
+)
+
+"%_prog%"  "%dp0%\node_modules\casperjs\bin\casperjs" %*
+ENDLOCAL
+EXIT /b %errorlevel%
+:find_dp0
+SET dp0=%~dp0
+EXIT /b
