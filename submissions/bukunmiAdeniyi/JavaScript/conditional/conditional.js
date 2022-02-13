a89dcba7bd102278facbe5e7df13fe656114bf89
bukiz-cloud <adeniyibukunmiadetunji@gmail.com>
2022-02-13 14:34:55 -0800

    adding conditonal to the file

---
+++ submissions/bukunmiAdeniyi/JavaScript/conditional/conditional.js
@@ -0,0 +1,23 @@
+const positiveDifference = 123 - 7;
+
+
+
+
+for ( let count = 1; count <= positiveDifference; count++){
+
+if( count % 3 ===0 && count % 5 ===0){
+    document.write("Software Developer")
+}
+else if (count % 3 ===0){
+    document.write("Software")
+}
+else if( count % 5 ===0){
+    document.write("Developer")
+}
+
+else{
+    document.write(count);
+}
+
+
+}
