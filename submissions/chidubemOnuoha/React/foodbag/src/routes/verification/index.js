ee2e3bc7b70bd6866437a8f540d8dea54ad6c807
mary699 <oseafianamarychinwe@gmail.com>
2022-02-17 13:48:39 +0100

    review

---
+++ submissions/chidubemOnuoha/React/foodbag/src/routes/verification/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Verification = () => {
+    return (
+        <div>
+            <h1>Verification page</h1>
+        </div>
+    )
+}
+
+export default Verification;
