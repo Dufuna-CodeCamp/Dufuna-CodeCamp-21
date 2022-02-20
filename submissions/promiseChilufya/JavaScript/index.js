8623ca7e1f7f3d4cdd721e7676897b4cfa253eda
Promise316 <chilufyajohn01@gmail.com>
2022-02-20 15:06:20 +0200

    add files

--- submissions/promiseChilufya/JavaScript/index.js
+++ submissions/promiseChilufya/JavaScript/index.js
@@ -1,13 +1,13 @@
-function printMultiplesOf3and5() {
-    for(let value = 1; value <= 117; value++){
-        if(value % 3 === 0 && value % 5 === 0){
+var positiveDifference = 123 - 7;
+
+    for (let i = 1; i <= positiveDiff; i++){
+        if (i % 3 === 0 && i % 5 === 0){
             document.write("Software Developer");
-            }else if(value % 3 === 0) {
-                document.write("Software");
-            }else if(value % 5 === 0){
-                document.write("Developer");
-                } else {
-                    document.write(value);
+        } else if (i % 3 === 0){
+            document.write("Software");
+        } else if (i % 5 === 0){
+            document.write("Developer");
+        } else {
+            document.write("i");
         }
     }
-}
