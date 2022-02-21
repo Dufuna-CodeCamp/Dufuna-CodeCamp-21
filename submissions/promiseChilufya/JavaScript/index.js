9d8242617fb47d70df72c6f1638b2b835685ed60
FabunmiTolulopeEniola <70230223+ToluEnny24@users.noreply.github.com>
2022-02-21 09:47:16 +0100

    Update index.js
    positiveDiff is not the same as positiveDifference

--- submissions/promiseChilufya/JavaScript/index.js
+++ submissions/promiseChilufya/JavaScript/index.js
@@ -1,6 +1,6 @@
 var positiveDifference = 123 - 7;
 
-    for (let i = 1; i <= positiveDiff; i++){
+    for (let i = 1; i <= positiveDifference; i++){
         if (i % 3 === 0 && i % 5 === 0){
             document.write("Software Developer");
         } else if (i % 3 === 0){
