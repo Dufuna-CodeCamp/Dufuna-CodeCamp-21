7c78650fd9380abbbede25b9ed6f662212a7d44d
MahMaejah <chipohameja@gmail.com>
2022-02-25 17:07:56 +0200

    First vendor dom commit

--- submissions/chipoHameja/Dom/js/index.js
+++ submissions/chipoHameja/Dom/js/index.js
@@ -1,42 +1,49 @@
-const selectedLocation = document.querySelector("#location"),
+document.addEventListener('DOMContentLoaded', () => {
+    const selectedLocation = document.querySelector("#location"),
       interestCardImages = document.querySelectorAll(".interest-image"),
       interestCards = document.querySelectorAll(".interest-card"),
       enabledImages = ["fast-food.svg", "Vegetables _ Drinks.svg", "Drinks _ Cocktails.svg", "resturant.svg"],
-      disabledImages = ["fast-food_disabled.svg", "Vegetables _ Drinks_disabled.svg", "Drinks _ Cocktails_disabled.svg", "resturant_disabled.svg"];;
+      disabledImages = ["fast-food_disabled.svg", "Vegetables _ Drinks_disabled.svg", "Drinks _ Cocktails_disabled.svg", "resturant_disabled.svg"];
+      let saved = [];
+    selectedLocation.addEventListener("change", (e) => {
+        if (e.target.value === "") {
+            let i = 0;
+            interestCardImages.forEach(function (image) {
+                image.src = "../css/assets/" + disabledImages[i];
+                i++;
+            });
+            interestCards.forEach(function (interestCard) {
+                interestCard.addEventListener("mouseover", function () {
+                    interestCard.style.cursor = "default";
+                });
+            });
+            interestCards.forEach((card) => {
+                const add = card.addEventListener('click', () => {
+                    return null;
+                }, false);
+                add;
+                card.removeEventListener('click', add, true);
+            });
+        }else {
+            let i = 0;
 
-selectedLocation.addEventListener("change", (e) => {
-    if (e.target.value === "") {
-        let i = 0;
-
-        interestCardImages.forEach(function (image) {
-            image.src = "../css/assets/" + disabledImages[i];
-            i++;
-        })
-
-        interestCards.forEach(function (interestCard) {
-            interestCard.addEventListener("mouseover", function () {
-                interestCard.style.cursor = "default"
+            interestCardImages.forEach(function (image) {
+                // console.log(image);
+                image.src = "../css/assets/" + enabledImages[i];
+                i++;
+                console.dir(image)
             })
-        })
-    }
 
-    else {
-        let i = 0;
+            interestCards.forEach(function (interestCard) {
+                interestCard.addEventListener("mouseover", function () {
+                    interestCard.style.cursor = "pointer";
+                })
 
-        interestCardImages.forEach(function (image) {
-            image.src = "../css/assets/" + enabledImages[i];
-            i++;
-            console.dir(image)
-        })
-
-        interestCards.forEach(function (interestCard) {
-            interestCard.addEventListener("mouseover", function () {
-                interestCard.style.cursor = "pointer";
+                interestCard.addEventListener("click", function () {
+                    window.open("vendor.html", "_self");
+                }, true);
             })
-
-            interestCard.addEventListener("click", function () {
-                window.open("vendor.html", "_self");
-            })
-        })
-    }
+            saved[0] = interestCards.innerHTML;
+        }
+    });
 })
