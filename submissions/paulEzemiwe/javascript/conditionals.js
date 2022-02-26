96085cc812785c0191fb9d5c1cfe516d0d211ef8
pezemiwe <pezemiwe@gmail.com>
2022-02-26 16:30:13 +0100

    formatted code

--- submissions/paulEzemiwe/javascript/conditionals.js
+++ submissions/paulEzemiwe/javascript/conditionals.js
@@ -1,13 +1,13 @@
-let diff  = 123 - 7;
+let diff = 123 - 7;
 
 for (let i = 1; i <= diff; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        document.write(`Software Developer <br />`);
-    } else if (i % 5 === 0) {
-        document.write(`Developer <br />`);
-    } else if (i % 3 === 0) {
-        document.write(`Software <br />`);
-    } else {
-        document.write(`${i} <br />`);
-    }
+  if (i % 3 === 0 && i % 5 === 0) {
+    document.write(`Software Developer <br />`);
+  } else if (i % 5 === 0) {
+    document.write(`Developer <br />`);
+  } else if (i % 3 === 0) {
+    document.write(`Software <br />`);
+  } else {
+    document.write(`${i} <br />`);
+  }
 }
