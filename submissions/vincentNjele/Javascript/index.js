6e446ffdee520d9769502759772939a2fe60f1bc
Vincent <njelevincent0@gmail.com>
2022-03-07 12:39:44 +0200

    I have added the JS folder with indenx.html and index.js files displaying the properties of objects

---
+++ submissions/vincentNjele/Javascript/index.js
@@ -0,0 +1,36 @@
+var productList = [
+    
+    phone = {
+        "productName" : "Tecno",
+        "imageURL" : "Desktop/Dufuna/vincentNjele/Javascript/Tecno",
+        "productPrice" : "K1,645",
+
+    },
+
+    computer = {
+        "productName" : "Lenovo ThinkPad",
+        "imageURL" : "Desktop/Dufuna/vincentNjele/Javascript/Thinkpad",
+        "productPrice" : "K8,645",
+
+    },
+
+    headsets = {
+        "productName" : "AKG",
+        "imageURL" : "Desktop/Dufuna/vincentNjele/Javascript/akg",
+        "productPrice" : "K200",
+
+    }
+
+]
+//document.write(productList[0].productName)
+let para = document.getElementsByTagName("h1");
+for(var i =0; i<productList.length; i++) {
+   
+    
+  document.write("<strong>"+productList[i]['productName']+"</strong> " +" with  " +"<strong>"+productList[i]["imageURL"] +"</strong>"+ " is sold for "+"<strong>"+productList[i]["productPrice"]+"</strong>"+"<br>"+"<br>");
+
+
+}
+
+
+
