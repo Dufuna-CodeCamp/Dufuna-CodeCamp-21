097da3848eff3f61b5217911bcce9bc8139524b2
Vincent <njelevincent0@gmail.com>
2022-03-15 17:41:51 +0200

    I have made the required changes

--- submissions/vincentNjele/Database/init.sql
+++ submissions/vincentNjele/Database/init.sql
@@ -1,11 +1,11 @@
-create database food_bag;
+CREATE database food_bag;
 
 use food_bag;
 /* food_table comes here */
 
-create table food_table (
-id int not null auto_increment,
-food_type varchar(255) not null,
+CREATE TABLE foods_table (
+id int NOT null auto_increment,
+food_type varchar(255) NOT null,
 primary key(id)
 )
 
@@ -13,43 +13,44 @@ primary key(id)
 /* vendors_table comes here */
 
 
-create table vendors_table (
-id int not null auto_increment,
-admin_id int not null,
-food_id int not null,
-name varchar(255) not null,
-amount double not null,
+CREATE TABLE vendors_table (
+id int NOT null auto_increment,
+admin_id int NOT null,
+food_id int NOT null,
+name varchar(255) NOT null,
+amount double NOT null,
 primary key(id), foreign key(food_id) references food_table(id)
 )
 
 /* admins_table comes here */
 
-create table admins_table (
-id int not null auto_increment,
-name varchar(255) not null,
-type int not null,
-email_address varchar(255) not null,
-phone_number varchar(255) not null,
-password varchar(255) not null,
+CREATE TABLE admins_table (
+id int NOT null auto_increment,
+name varchar(255) NOT null,
+type int NOT null,
+email_address varchar(255) NOT null,
+phone_number int NOT null,
+password varchar(255) NOT null,
 primary key(id)
 )
 
 /* customers_table comes here */
 
-create table customers_table (
-id int not null auto_increment,
-name varchar(255) not null,
-phone_number varchar(255) not null,
-address varchar(255) not null,
+
+CREATE TABLE customers_table (
+id int NOT null auto_increment,
+name varchar(255) NOT null,
+phone_number int NOT null,
+address varchar(255) NOT null,
 payment_method varchar(255) null,
 primary key(id)
 )
 
 /* orders_table comes here */
 
-create table orders_table (
-id int not null auto_increment,
-vendor_id int not null,
+CREATE TABLE orders_table (
+id int NOT null auto_increment,
+vendor_id int NOT null,
 customer_id int  null,
 primary key(id), foreign key(vendor_id) references vendors_table(id), foreign key(customer_id) references customers_table(id)
 )
