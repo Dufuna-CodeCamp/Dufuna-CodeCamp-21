cf902038bfff7abaa8ef6d1a4c67bf7cd720b882
Mwansa Squared <mwansasquared9@gmail.com>
2022-03-29 17:06:57 +0200

    used conditonals to print out the required output

--- submissions/mwansaMwansa/Javascript/conditionals.js
+++ submissions/mwansaMwansa/Javascript/conditionals.js
@@ -1,11 +1,11 @@
 function printMultiples() {
     for(let value = 1; value <= 116; value++) {
-        if (value % 3 === 0) {
+        if (value % 3 === 0 && value % 5 === 0) {
+            document.write("Software Developer" +"<br >");
+        } else if (value % 3 === 0) {
             document.write("Software"+"<br >");
         } else if (value % 5 === 0) {
             document.write("Developer" +"<br >");
-        } else if (value % 3 === 0 && value % 5 === 0) {
-            document.write("Software Developer" +"<br >");
         } else {
             document.write(value +"<br >");
         }
