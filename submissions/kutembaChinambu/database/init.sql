779cd901a5f6d855f2c830210ab6e38497e3a478
Kutemba Chinambu <kutembachinambu@rocketmail.com>
2022-03-30 20:34:24 +0200

    adding the foodbag database tables

--- submissions/kutembaChinambu/database/init.sql
+++ submissions/kutembaChinambu/database/init.sql
@@ -3,10 +3,7 @@
 CREATE TABLE food 
 (
 id INT NOT NULL AUTO_INCREMENT,
-fast_food VARCHAR(100) NOT NULL,
-vegetables_and_fruits VARCHAR(100) NOT NULL,
-drinks_and_cocktails VARCHAR(100) NOT NULL,
-resturants VARCHAR(100) NOT NULL,
+category ENUM ('fast_food','vegetables_and_fruits','drinks_and_cocktails','resturants') NOT NULL,
 PRIMARY KEY (id)
 );
 
@@ -15,10 +12,10 @@ PRIMARY KEY (id)
 CREATE TABLE admin 
 (
 id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(100) NOT NULL,
+name VARCHAR(250) NOT NULL,
 type ENUM ('0','1') NOT NULL,
 email_address VARCHAR(200) NOT NULL,
-phone_number CHAR(13) NOT NULL,
+phone_number VARCHAR(13) NOT NULL,
 password VARCHAR(100) NOT NULL,
 PRIMARY KEY (id)
 );
@@ -30,9 +27,8 @@ CREATE TABLE vendor_food
 id INT NOT NULL AUTO_INCREMENT,
 food_id INT NOT NULL,
 admin_id INT NOT NULL,
-name VARCHAR(100) NOT NULL,
-email_address VARCHAR(200) NOT NULL,
-amount DECIMAL(30,20) NOT NULL,
+name VARCHAR(250) NOT NULL,
+amount DECIMAL(30,3) NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (food_id) REFERENCES food (id),
 FOREIGN KEY (admin_id) REFERENCES admin (id)
@@ -44,7 +40,7 @@ CREATE TABLE customer
 (
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(100) NOT NULL,
-phone_number CHAR(13) NOT NULL,
+phone_number VARCHAR(13) NOT NULL,
 address VARCHAR(200) NOT NULL,
 payment_method ENUM('card','Mobile Money','Cash'),
 PRIMARY KEY (id)
