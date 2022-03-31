f482322b180e86a2543368b175a61334d70df64e
MahMaejah <chipohameja@gmail.com>
2022-03-31 15:45:22 +0200

    React second commit

--- submissions/chipoHameja/REACT/react-app/src/components/NavBar.js
+++ submissions/chipoHameja/REACT/react-app/src/components/NavBar.js
@@ -6,9 +6,9 @@ function NavBar() {
             <a href='#'><div className='first-part'>Food</div><div className='second-part'>Bag</div></a>
         </div>
         <ul id="nav-links">
-            <li id="nav-cart"><a href='#'>Cart</a></li>
-            <li id="login"><a href='#'>Login</a></li>
-            <li id="sign-up"><a href='#'>Sign up</a></li>
+            <li id="nav-cart"><a href='/'>Cart</a></li>
+            <li id="login"><a href='/'>Login</a></li>
+            <li id="sign-up"><a href='/'>Sign up</a></li>
         </ul>
     </nav>
 }
