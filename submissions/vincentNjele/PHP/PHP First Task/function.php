dee51e790ba58bfe8117b4de2e9bee280cd90138
Vincent <njelevincent0@gmail.com>
2022-04-04 11:19:12 +0200

    I have created the first PHP task where I have created the function to convert different figures

---
+++ submissions/vincentNjele/PHP/PHP First Task/function.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     
+    <link rel="stylesheet" href="styles.css">
+    </head>
+
+    <body>
+       <form class="form" action="convert.php" method="POST">
+
+           Number: <br/>
+          <input type="text" name="number" placeholder="Enter number to convert">
+          <br/>
+         <button class="btn" type="submit" >Convert</button>
+
+       </form>
+    </body>
+</html>
