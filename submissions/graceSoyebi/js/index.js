2203610d7fbc6ae42bdfe428bb923c8ea19a8eb1
Grace O <75256483+gra2101@users.noreply.github.com>
2022-04-04 20:43:41 +0100

    practice js array and object

---
+++ submissions/graceSoyebi/js/index.js
@@ -0,0 +1,23 @@
+ var productList = [
+    {
+        productName: "Wig",
+        imageUrl:"imageurl-1",
+        productPrice: 5000,
+    },
+
+    {
+        productName: "Denim",
+        imageUrl:"imageurl-2",
+        productPrice: 7000,
+    },
+
+    {
+        productName: "Gold-pendant",
+        imageUrl:"imageurl-3",
+        productPrice: 3500,
+    }
+]
+
+document.write(productList[0].productName, " with ", productList[0].imageUrl, " is sold for ", productList[0].productPrice, ". ")
+document.write(productList[1].productName, " with ", productList[1].imageUrl, " is sold for ", productList[1].productPrice, ". ")  
+document.write(productList[2].productName, " with ", productList[2].imageUrl, " is sold for ", productList[2].productPrice, ". ")
