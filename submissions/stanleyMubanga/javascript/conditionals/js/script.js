9ef16bcbe2e093bbbe8656c808f756cd91e72084
Stanley Mubanga <estabannabum@gmail.com>
2022-04-07 19:59:45 -0400

    created conditionals.js

---
+++ submissions/stanleyMubanga/javascript/conditionals/js/script.js
@@ -0,0 +1,21 @@
+// potential difference isn sum
+
+const sum = 123 - 7;
+let statement = "";
+
+for (i = 0; i <= sum; i++){
+    if ( i % 3 == 0){
+        statement += "Software <br>";
+    }
+    else if(i % 5 ==0){
+        statement += "Developer <br>";
+    }
+    else if (i % 3 == 0 && i % 5 == 0){
+        statement += "Software Developer <br>";
+    }
+    else {
+        statement += i + "<br>";
+    }
+}
+
+document.write(statement);
