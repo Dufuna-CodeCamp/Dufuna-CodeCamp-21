672ab4a7be4f0c23600f919b316a4185a7a749e8
Dolapo Adebanjo <dolapomola@gmail.com>
2022-04-12 16:15:44 +0100

    database and test

--- submissions/DolapoAdebanjo/DATABASE/init.sql
+++ submissions/DolapoAdebanjo/DATABASE/init.sql
@@ -7,7 +7,11 @@ USE FOODBAG;
 CREATE TABLE food (
 id INT NOT NULL AUTO_INCREMENT,
 -- type = the four different categories of food on the homepage.
-`type` VARCHAR(250) NOT NULL,
+type INT NOT NULL,
+fast_food_category VARCHAR(250) NOT NULL,
+veg_fruits_category VARCHAR(250) NOT NULL,
+drinks_cocktails_category VARCHAR(250) NOT NULL,
+restaurants_category VARCHAR(250) NOT NULL,
 PRIMARY KEY(id)
 );
 
@@ -16,7 +20,7 @@ CREATE TABLE vendor_food (
 id INT NOT NULL AUTO_INCREMENT,
 admin_id INT NOT NULL,
 food_id INT NOT NULL,
-`name` VARCHAR(250) NOT NULL,
+name VARCHAR(250) NOT NULL,
 amount FLOAT(10, 2) NOT NULL,
 PRIMARY KEY(id),
 FOREIGN KEY(admin_id) REFERENCES admins (id),
@@ -26,18 +30,18 @@ FOREIGN KEY (food_id) REFERENCES food (id)
 -- 3) CREATING THE admins table showing the admins in charge of foodbag platform and vendors.
 CREATE TABLE admins (
 id INT NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(250) NOT NULL,
-`type` TINYINT(2) UNSIGNED NOT NULL,
+name VARCHAR(250) NOT NULL,
+type INT NOT NULL,
 email_address VARCHAR(250) NOT NULL,
 phone_number TINYINT(50) NOT NULL,
-`password` VARCHAR(250) NOT NULL,
+password VARCHAR(250) NOT NULL,
 PRIMARY KEY(id)
 );
 
 -- 4) CREATING THE customers table.
 CREATE TABLE customers (
 id INT NOT NULL AUTO_INCREMENT,
-`name` VARCHAR(250) NOT NULL,
+name VARCHAR(250) NOT NULL,
 phone_number TINYINT(50) NOT NULL,
 address VARCHAR(250) NOT NULL,
 payment_method ENUM('card', 'bank transfer', 'cash'),
