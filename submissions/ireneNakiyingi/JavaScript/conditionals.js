bc0a9346b22cb98f2c2eab7bea4dfe6b8f28ab9d
Irene Nakiyingi <iryntracy@gmail.com>
2022-04-12 18:33:17 +0300

    software developer conditionals

--- submissions/ireneNakiyingi/JavaScript/conditionals.js
+++ submissions/ireneNakiyingi/JavaScript/conditionals.js
@@ -2,16 +2,16 @@ var figure = []
 var diff = (123-7);
 for (var num =1; num<=diff ;num++ ) {
     if (num%3 ===0 && num%5===0){
-        console.log (num + 'software developer');
+        console.log (num + 'Software developer');
         document.write('Software Developer' + '<br>')
         
     }
     else if (num%3 === 0 ){
-        console.log( num + 'software');
+        console.log( num + 'Software');
         document.write('Software' + '<br>')
     }
     else if (num%5 === 0){
-        console.log (num + 'developer');
+        console.log (num + 'Developer');
         document.write('Developer' + '<br>')
     }   
 }
