952813c6e62e702e8160ef724e34813ec2a3fe5a
ChijiokeOrabueze <95033997+ChijiokeOrabueze@users.noreply.github.com>
2022-04-16 07:10:08 -0700

    ran test and made corrections

--- submissions/chijiokeOrabueze/database2/database2.sql
+++ submissions/chijiokeOrabueze/database2/database2.sql
@@ -5,19 +5,19 @@ CREATE DATABASE travel_sample;
 USE travel_sample;
 
 
--- creating users table
-CREATE TABLE users (
+-- creating passengers table
+CREATE TABLE passengers (
 	id INT NOT NULL AUTO_INCREMENT,
     full_name VARCHAR(100) NOT NULL,
     sex VARCHAR(10) NOT NULL,
-    age INT,
+    age VARCHAR(20),
     PRIMARY KEY (id)
 );
 
--- creating trip_passengers table
-CREATE TABLE trip_passengers (
+-- creating passengers_details table
+CREATE TABLE passengers_details (
 	id INT NOT NULL AUTO_INCREMENT,
-    passenger_id INT NOT NULL,
+    passengers_id INT NOT NULL,
     passenger_class INT NOT NULL,
     ticket_number VARCHAR(50) NOT NULL,
     trip_fare DECIMAL(10,5) NOT NULL,
@@ -26,16 +26,16 @@ CREATE TABLE trip_passengers (
     num_sibilings_spouses INT NOT NULL,
     embark_point VARCHAR(10) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (passenger_id) REFERENCES users (id)
+    FOREIGN KEY (passengers_id) REFERENCES passengers (id)
 );
 
--- creating accidents table
-CREATE TABLE accidents (
+-- creating accident_cases table
+CREATE TABLE accident_cases (
 	id INT NOT NULL AUTO_INCREMENT,
-    passenger_id INT NOT NULL,
+    passengers_id INT NOT NULL,
     status INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (passenger_id) REFERENCES trip_passengers (id)
+    FOREIGN KEY (passengers_id) REFERENCES passengers_details (id)
 );
 
 
