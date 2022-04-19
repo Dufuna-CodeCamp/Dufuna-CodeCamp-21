3e2861790b03cd5974677af666d74b9ee7929666
Stanley Mubanga <estabannabum@gmail.com>
2022-04-19 12:24:29 -0400

    created dom for landing page

---
+++ submissions/stanleyMubanga/DOM/landingPage.js
@@ -0,0 +1,31 @@
+const   city = document.getElementById('location');
+        interestCards = document.querySelectorAll('.interest-card');
+        interestImg = document.querySelector('.interest-image');
+        interestHeaders = document.querySelectorAll(".interests-header");
+        fastFood = document.getElementById("fastFood");
+        vegetables = document.getElementById("vegetables");
+        drinks = document.getElementById("drinks");
+        resturant = document.getElementById("resturant");
+
+    city.addEventListener('input', (event) => {
+
+        if(event.target.value !== "") {
+            fastFood.src =  "../assets/fast-food.svg";
+            vegetables.src = "../assets/Vegetables & Drinks.svg";
+            drinks.src = "../assets/Drinks & Cocktails.svg";
+            resturant.src = "../assets/resturant.svg";
+
+            interestHeaders.forEach((interestHeader => {
+                interestHeader.style.color = "#AD4C4C";
+            }))
+            interestCards.forEach((interestCard => {
+                interestCard.classList.add("activeCard");
+            }))
+            for (let l = 0; l < interestCards.length; l++) {
+                interestCards[l].addEventListener('click', (redirect) => {
+                    window.location = "vendorPage.html"
+                })
+                
+            }
+        }
+    })
