6215f01857eea4d8d5ac562b08e34789c839bb30
John Promise Chilufya <51807673+Promise316@users.noreply.github.com>
2022-04-22 16:12:42 +0200

    Add files via upload

--- submissions/promiseChilufya/src/App.js
+++ submissions/promiseChilufya/src/App.js
@@ -1,16 +1,36 @@
-import Holder from './components/Holder';
-import Navbar from './components/Navbar/Navbar';
-import ManageContext from './Context/main'
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Holder from "./components/Holder";
+import { VerificationHolder } from "./components/VerificationHolder";
+import ManageContext from "./Context/main";
+
 
 
 function App() {
+
   return (
+
     <div>
-        <ManageContext>
-          <Navbar />
-    < Holder />
-      </ManageContext>
-    </div>
+    <ManageContext >
+     
+  
+    </ManageContext>
+
+  <Router>
+
+  <Routes>
+ 
+  <Route path="/"  element = { <Holder />} />
+  
+  <Route path="VerificationHolder"  element = { <VerificationHolder />} />
+
+  </Routes>
+     
+
+  </Router>
+  
+  </div>
+       
+      
   );
 }
 
---
+++ submissions/promiseChilufya/src/components/VerificationHolder.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import { VerificationButton } from './VerificationButton';
+import VerificationNavbar from './VerificationNavbar';
+import { VerificationInput } from './VerificationInput';
+import { VerificationSelect } from './VerificationSelect';
+import VerificationDetails from './VerificationDetails';
+import './styles.css'
+
+
+
+export const VerificationHolder = () => {
+
+    const SelectOptions = [
+        {
+            title: "Card",
+            value: "Card"
+        },
+        {
+            title: "Card",
+            value: "Visa Card"
+        },
+        {
+            title: "Master Card",
+            value: "Master Card"
+        }
+    ]
+
+  return (
+    <div className="contents">
+
+    <VerificationNavbar />
+    <VerificationDetails />
+
+    <VerificationInput name = "Phone Number" lableId = "label-id" inputId= 'input-id' placeholder = "+234"  />
+     <hr id='line'/>
+    <VerificationSelect labelId = "label-id" name = "Payment Method" selectId = "form-group" options  =  
+    {SelectOptions.map((index)=>(
+
+        <option key={index.title}>{index.value}</option>
+    
+        ))}>
+    </VerificationSelect>
+
+    <VerificationInput name = "Card Number" lableId = "label-id" inputId= 'input-id' 
+     placeholder = "4647 0003 9058 1302"  />
+
+     <VerificationInput name = "Card Name" lableId = "label-id" inputId= 'input-id' 
+     placeholder = "Fabunmi Tolulope Eniola"  />
+
+     <div className="comb">
+     
+     <>
+     <VerificationInput name = "Expiry Date" lableId = "label-id" inputId= 'input-id' 
+     placeholder = "10/24"  > </VerificationInput>
+    
+        <>
+        <VerificationInput name = "CVC" lableId = "label-id" inputId= 'input-id' 
+           placeholder = "656"  > </VerificationInput>
+        </>
+     </>
+        </div>
+        <br/>
+     
+        <VerificationButton  buttonClass="button-class" name= "Pay" />
+    </div>
+  )
+}
