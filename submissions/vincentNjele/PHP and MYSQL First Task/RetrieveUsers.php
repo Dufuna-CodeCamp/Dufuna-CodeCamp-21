529b09a2e08ed5675346d8d082f8bb375e4f2aa2
Vincent <njelevincent0@gmail.com>
2022-04-25 17:30:28 +0200

    I have created the first PHP and Database task

---
+++ submissions/vincentNjele/PHP and MYSQL First Task/RetrieveUsers.php
@@ -0,0 +1,47 @@
+<?php
+
+include_once("Connection.php");
+
+class Retrieve {
+
+     private $con;
+
+    function __construct()
+    {
+
+        $obj = new Connection();
+        $this->con = $obj->Dbcon;
+        
+    }
+
+
+    function RetrieveUsers() {
+
+
+        try {
+
+
+            $select = "SELECT * FROM Customer";
+
+            $result = $this->con->query($select);
+    
+            if($result->rowCount() > 0) {
+    
+                return $result;
+    
+            } else {
+    
+                echo "There are no records in the database";
+            }
+    
+
+        } catch(PDOException $e) {
+
+            die("ERROR: Could not execute $select." .$e->getMessage());
+        }
+
+       
+    }
+}
+
+
