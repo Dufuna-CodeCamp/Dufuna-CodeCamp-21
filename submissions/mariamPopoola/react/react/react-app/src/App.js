07c87065bc79977206efc5cde0558ddaa44c50c5
mariam60 <76251994+mariam60@users.noreply.github.com>
2022-04-26 08:39:50 -0600

    cart page

--- submissions/mariamPopoola/react/react/react-app/src/App.js
+++ submissions/mariamPopoola/react/react/react-app/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:submissions/mariamPopoola/react/react/react-app/src/App.js
 import './App.css';
 import Verification from '../src/components/verificationpage/verification';
 
@@ -12,4 +13,20 @@ function App() {
 export default App;
 
   
+=======
+import './App.css';
+import CartPage from '../src/components/cart/cartpage';
+
+function App() {
+  return (
+    <div>
+       <CartPage/>
+    </div>
+  );
+}
+
+export default App;
+
+  
+>>>>>>> 3302d5dc (cart page):react/react-app/src/App.js
      
