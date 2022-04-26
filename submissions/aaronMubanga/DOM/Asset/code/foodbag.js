e02bd8989206633ced70dc8bc8b7c0e4cca1b488
Mubanga Aaron <93948423+AaronAsborn@users.noreply.github.com>
2022-04-26 09:26:30 +0200

    [HIRFUS-166] Landing Page Validation

---
+++ submissions/aaronMubanga/DOM/Asset/code/foodbag.js
@@ -0,0 +1,34 @@
+const   town = document.getElementById('local'),
+        interests = document.querySelector('.interests'),
+        interestHeader = document.querySelectorAll('.interetHeader'),
+        interest = document.getElementsByClassName('yourInteret'),
+        cards = document.querySelectorAll('.card')
+        fastFood = document.getElementById('fastFood'),
+        vDrinks = document.getElementById('V&Drinks'),
+        drinksCocktail = document.getElementById('drinks&Cocktail'),
+        resturant = document.getElementById('resturant')
+;
+
+town.addEventListener('input', (e)=>{
+
+    if(e.target.value !== " "){
+        fastFood.src = "Asset/images/fast-food.svg";
+        vDrinks.src = "Asset/images/Vegetables_&_Drinks.svg";
+        drinksCocktail.src = "Asset/images/Drinks_&_Cocktails.svg";
+        resturant.src = "Asset/images/resturant_disabled.svg";
+
+        interestHeader.forEach((interetHeader => {
+            interetHeader.style.color = "#AD4C4C";
+        })) 
+
+        for (var i = 0; i < cards.length; i++) {
+            cards[i].addEventListener("click", ()=>{
+                window.location = "venders.html"
+            })
+        }
+
+    }
+
+})
+
+console.dir("interestHeader");
