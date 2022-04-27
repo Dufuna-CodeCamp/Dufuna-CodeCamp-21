323abe5b910aafd482551355e96f1c4fb1a09e3c
ChijiokeOrabueze <95033997+ChijiokeOrabueze@users.noreply.github.com>
2022-04-27 14:34:26 -0700

    React task 1

---
+++ submissions/chijiokeOrabueze/REACT/react-foodbag-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import CartPage from './components/CartPage';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <CartPage />
+    </div>
+  );
+}
+
+export default App;
+
