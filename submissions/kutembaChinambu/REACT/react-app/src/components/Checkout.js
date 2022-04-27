f5cbde3dd5795219417e1afa3bfdad4660ee16ba
Kutemba Chinambu <kutembachinambu@rocketmail.com>
2022-04-27 19:12:51 +0200

    added Cart page

--- submissions/kutembaChinambu/REACT/react-app/src/components/Checkout.js
+++ submissions/kutembaChinambu/REACT/react-app/src/components/Checkout.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 
-handleClick = () => {
-    window.open("http://twitter.com/saigowthamr");
-
-}
 
 const Checkout = (props) => {
-    const history = useHistory();
+   
   
-    const openVerification = () => {
-        history.push("/Verification")
 
     return (
         <div id="container">
-            <button id="button" onClick={openVerification}>Checkout</button>
+            <button id="button" >Checkout</button>
         </div>
     )
 }
