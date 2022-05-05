032e081d5c3766f185b336139cb204b9dc85fc96
Mirriam Sikalume <mirriammlaliki640@gmail.com>
2022-05-05 11:36:49 +0200

    I have created my first task on the Databases

---
+++ submissions/mirriamSikalume/database/foodbag.sql
@@ -0,0 +1,45 @@
+CREATE database food_bag;
+use food_bag;
+
+CREATE TABLE food_table (
+	id INT NOT NULL auto_increment,
+	food_type VARCHAR(255) NOT NULL,
+	primary key(id)
+);
+
+CREATE TABLE vendors_table (
+	id INT NOT NULL auto_increment,
+	admin_id INT NOT NULL,
+	food_id INT NOT NULL,
+	name VARCHAR (255) NOT NULL,
+	amount INT NOT NULL,
+	primary key(id), foreign key(food_id) references food_table(id) 
+);
+
+CREATE TABLE admins_table (
+	id INT NOT NULL auto_increment,
+	name VARCHAR (255) NOT NULL,
+	type INT NOT NULL,
+	email_address VARCHAR (255) NOT NULL,
+	phone_number INT NOT NULL,
+	password VARCHAR (255) NOT NULL,
+	primary key(id)
+);
+
+CREATE TABLE customers_table (
+	id INT NOT NULL auto_increment,
+	name VARCHAR (255) NOT NULL,
+	phone_number INT NOT NULL,
+	address VARCHAR (255) NOT NULL,
+	payment_method VARCHAR (255) NOT NULL,
+	primary key(id)
+);
+
+CREATE TABLE orders_table (
+	id INT NOT NULL auto_increment,
+	vendor_id INT NULL,
+	customer_id INT NOT NULL,
+	primary key(id),
+	foreign key(vendor_id) references vendors_table(id), 
+	foreign key(customer_id) references customers_table(id)
+)
