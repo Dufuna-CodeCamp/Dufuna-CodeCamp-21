803f2fa19fc471105ddf935e07d6e3ccf3ee89b9
Popoola Mariam <popoolamariam05@gmail.com>
2022-05-05 11:10:11 -1000

    cart

--- submissions/mariamPopoola/react/react/react-app/src/App.js
+++ submissions/mariamPopoola/react/react/react-app/src/App.js
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 <<<<<<< HEAD:submissions/mariamPopoola/react/react/react-app/src/App.js
+=======
+>>>>>>> 30a7b3ac (cart)
 import './App.css';
 import Verification from '../src/components/verificationpage/verification';
 
 function App() {
   return (
     <div>
+<<<<<<< HEAD
        <Verification/>
+=======
+       <Cartpage/>
+>>>>>>> 30a7b3ac (cart)
     </div>
   );
 }
@@ -16,6 +23,7 @@ export default App;
 export default App;
 
   
+<<<<<<< HEAD
 =======
 import './App.css';
 import CartPage from '../src/components/cart/cartpage';
@@ -32,5 +40,11 @@ export default App;
 
   
 >>>>>>> 3302d5dc (cart page):react/react-app/src/App.js
+<<<<<<< HEAD
      
 >>>>>>> 07c87065 (cart page)
+=======
+=======
+>>>>>>> 30a7b3ac (cart)
+     
+>>>>>>> d24e5bb3 (cart)
