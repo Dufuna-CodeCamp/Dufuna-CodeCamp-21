3f74c858f71f0b4f74b54a330821a26417b60b2c
Kutemba Chinambu <kutembachinambu@rocketmail.com>
2022-05-05 10:22:09 +0200

    fixing code factor errors

--- submissions/kutembaChinambu/REACT/react-app/src/index.js
+++ submissions/kutembaChinambu/REACT/react-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
