30a7b3ac28305dfb644d4d552c5f21439c4410c3
Popoola Mariam <popoolamariam05@gmail.com>
2022-05-05 11:10:11 -1000

    cart

---
+++ submissions/mariamPopoola/react/react/react-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Verification from '../src/components/verificationpage/verification';
+
+function App() {
+  return (
+    <div>
+       <Cartpage/>
+    </div>
+  );
+}
+
+export default App;
+
+  
+     
