9600dc8478b8048afe45b7b190efb508f197d205
Stanley Mubanga <estabannabum@gmail.com>
2022-05-05 15:51:36 +0200

    created queries.sql

--- submissions/stanleyMubanga/database3/queries.sql
+++ submissions/stanleyMubanga/database3/queries.sql
@@ -20,7 +20,7 @@ WHERE passengers_details.embark_point='S' AND accident_cases.survived=1;
 -- Get the id, name and the total number of passengers who paid a fare greater than $100 and above the age of 35 had siblings or spouses on board
 SELECT passengers.passengers_id, passengers.full_name FROM passengers
 INNER JOIN passengers_details ON passengers.passengers_id = passengers_details.passenger_id
-WHERE passengers_details.trip_fare > 100 AND passengers.age > 35 AND passengers_details.siblings_spouses > 0;
+WHERE passengers_details.trip_fare > 100 AND passengers_details.age > 35 AND passengers_details.siblings_spouses > 0;
 
 SELECT COUNT(*) FROM passengers INNER JOIN passengers_details ON passengers.passengers_id = passengers_details.passenger_id
 WHERE passengers_details.trip_fare > 100.00 AND passengers.age > 35 AND passengers_details.siblings_spouses > 0;
