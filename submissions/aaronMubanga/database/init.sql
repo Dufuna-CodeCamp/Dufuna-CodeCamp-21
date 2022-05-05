9c05b2f8c5c11717568394c33314cc3401b8bc5a
Mubanga Aaron <93948423+AaronAsborn@users.noreply.github.com>
2022-05-05 15:33:45 +0200

    changes made to PR

--- submissions/aaronMubanga/database/init.sql
+++ submissions/aaronMubanga/database/init.sql
@@ -19,19 +19,6 @@ CREATE TABLE food_bag (
     PRIMARY KEY (id)
 );
 
--- Addition of values to Food Bag
-
-INSERT INTO food_bag (type)
-	VALUE	('Fast Food'),
-			('Vegetables & Fruits'),
-            ('Drinks & Crocktails'),
-            ('Resturants')
-;
-
--- Checking table Results
-
-SELECT * FROM food_bag;
-
 -- (3) Table Admins
 
 CREATE TABLE admins (
@@ -39,14 +26,10 @@ CREATE TABLE admins (
     name VARCHAR(250) NOT NULL,
     type ENUM('0', '1') NOT NULL,
     email_address VARCHAR(250) NOT NULL,
-    phone_number INT NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
     password VARCHAR(250) NOT NULL,
     PRIMARY KEY (id)
 );
-
--- Checking table Results
-
-SELECT * FROM admins;
 
 -- (2) Table Vendor Food
 
@@ -55,15 +38,11 @@ CREATE TABLE vendor_food (
     admin_id INT NOT NULL,
     food_id INT NOT NULL,
     name VARCHAR(250) NOT NULL,
-    amount DECIMAL(4,2) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (admin_id) REFERENCES admins (id),
     FOREIGN KEY (food_id) REFERENCES food_bag (id)
 );
-
--- Checking table Results
-
-SELECT * FROM vendor_food;
 
 -- (4) Table Customer's Account
 
@@ -71,17 +50,12 @@ CREATE TABLE customers_account (
 	id INT NOT NULL AUTO_INCREMENT,
     admin_id INT NOT NULL,
     name VARCHAR(250) NOT NULL,
-    phone_number INT NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
     address VARCHAR(250) NOT NULL,
     payment_method VARCHAR(250),
-    payment_verification VARCHAR(250),
     PRIMARY KEY (id),
     FOREIGN KEY (admin_id) REFERENCES admins (id)
 );
-
--- Checking table Results
-
-SELECT * FROM customers_account;
 
 -- (5) Table Orders 
 
@@ -94,11 +68,3 @@ CREATE TABLE orders (
     FOREIGN KEY (vendor_id) REFERENCES vendor_food (id),
     FOREIGN KEY (admin_id) REFERENCES admins (id)
 );
-
--- Checking table Results
-
-SELECT * FROM orders;
-
--- Checking Database results
-
-SHOW TABLES;
