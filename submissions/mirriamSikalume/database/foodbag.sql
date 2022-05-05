d0dceaa96bb1f5091384bd5444371ef86f2b0d9f
Mirriam Sikalume <mirriammlaliki640@gmail.com>
2022-05-05 14:40:55 +0200

    I have modified the first task on database

--- submissions/mirriamSikalume/database/foodbag.sql
+++ submissions/mirriamSikalume/database/foodbag.sql
@@ -12,16 +12,16 @@ CREATE TABLE vendors_table (
 	admin_id INT NOT NULL,
 	food_id INT NOT NULL,
 	name VARCHAR (255) NOT NULL,
-	amount INT NOT NULL,
+	amount DECIMAL NOT NULL,
 	primary key(id), foreign key(food_id) references food_table(id) 
 );
 
 CREATE TABLE admins_table (
 	id INT NOT NULL auto_increment,
 	name VARCHAR (255) NOT NULL,
-	type INT NOT NULL,
+	type ENUM ('0','1') NOT NULL,
 	email_address VARCHAR (255) NOT NULL,
-	phone_number INT NOT NULL,
+	phone_number VARCHAR (50) NOT NULL,
 	password VARCHAR (255) NOT NULL,
 	primary key(id)
 );
@@ -29,9 +29,9 @@ CREATE TABLE admins_table (
 CREATE TABLE customers_table (
 	id INT NOT NULL auto_increment,
 	name VARCHAR (255) NOT NULL,
-	phone_number INT NOT NULL,
+	phone_number VARCHAR (50) NOT NULL,
 	address VARCHAR (255) NOT NULL,
-	payment_method VARCHAR (255) NOT NULL,
+	payment_method VARCHAR (255) NULL,
 	primary key(id)
 );
 
