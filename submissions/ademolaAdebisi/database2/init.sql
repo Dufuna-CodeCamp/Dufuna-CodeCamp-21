c19ef49df408a27a6387a97696edf9ef9e51f012
Stanley Mubanga <estabannabum@gmail.com>
2022-05-07 22:50:58 +0200

    editted database2

--- submissions/ademolaAdebisi/database2/init.sql
+++ submissions/ademolaAdebisi/database2/init.sql
@@ -1,43 +1,38 @@
--- create a database travel_records
-CREATE DATABASE REFERENCES;
--- select the database travel_records
-USE REFERENCES;
--- create table structure for passengers
+-- CREATE THE DATABASE FOR THE COMPANY
+CREATE DATABASE transportation_company;
+
+-- CHOOSE TO USE THE DATABASE CREATED ABOVE
+USE transportation_company;
+
+-- CREATE TABLE FOR PASSENGERS
 CREATE TABLE passengers (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT,
+    full_name VARCHAR(250) NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    age VARCHAR(3),
+    PRIMARY KEY (id)
+);
+
+-- CREATE TABLE FOR TRIPS 
+CREATE TABLE trips (
+	id INT NOT NULL AUTO_INCREMENT,
     passengers_id INT NOT NULL,
-    full_name VARCHAR(100) NOT NULL,
-    sex VARCHAR(20) NOT NULL,
-    age VARCHAR(20),
-);
-
-SELECT * FROM accident_cases;
-
--- create table structure for passengers_details
-CREATE TABLE passengers_details (
-	id INT NOT NULL AUTO_INCREMENT FOREIGN KEY(passengers_id)
-    passenger_id INT,
-    passenger_class INT NOT NULL,
-    passenger_ticket VARCHAR(20) NOT NULL,
-    trip_fare DECIMAL(10,2) NOT NULL,
-    assigned_cabin VARCHAR(20) NOT NULL,
-    parents_children INT NOT NULL,
-    siblings_spouses INT NOT NULL,
-    embark_point VARCHAR(20) NOT NULL,
+    passenger_class ENUM('1', '2', '3') NOT NULL,
+    passenger_ticket_number VARCHAR(250) NOT NULL,
+    passenger_assigned_cabin VARCHAR(10),
+    trip_fare DECIMAL(10, 2) NOT NULL,
+    parents_number_aboard INT NOT NULL,
+    siblings_number_aboard INT NOT NULL,
+    embarkation_point VARCHAR(250) NOT NULL,
     PRIMARY KEY (id),
-    
+    FOREIGN KEY (passengers_id) REFERENCES passengers (id)
 );
 
--- create table structure for accident_cases
+-- CREATE TABLE FOR ACCIDENT SURVIVORS(ONLY USED IF AN ACCIDENT OCCURS
 CREATE TABLE accident_cases (
-	id INT NOT NULL AUTO_INCREMENT FOREIGN KEY (passengers_id),
-    passenger_id INT,
-    survived BOOLEAN NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT,
+    passengers_id INT NOT NULL,
+	survived BOOLEAN NOT NULL,
     PRIMARY KEY (id),
-    
+    FOREIGN KEY (passengers_id) REFERENCES passengers (id)
 );
-
-
-
-
-
