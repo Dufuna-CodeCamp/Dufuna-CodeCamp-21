1183ee69a35ef21a8477c875e929bcef018f79bc
ChijiokeOrabueze <95033997+ChijiokeOrabueze@users.noreply.github.com>
2022-05-12 07:32:17 -0700

    ran test and made corrections

--- submissions/chijiokeOrabueze/php/weather.php
+++ submissions/chijiokeOrabueze/php/weather.php
@@ -3,12 +3,12 @@
 $temperature = 34;
 
 
-if ($temperature <= 20 ){
+if ($temperature <= 20){
     echo "It is freezing today";
-}elseif($temperature > 20 && $temperature < 30){
+}else if ($temperature > 20 && $temperature < 30){
     echo "The weather is just perfect";
-}elseif($temperature >= 30 && $temperature < 40){
+}else if ($temperature >= 30 && $temperature < 40){
     echo "It's so hot today";
-}elseif($temperature >= 40){
-    echo "Am I in the Sahara Desert?";
+}else if ($temperature >= 40){
+    echo "Am i in the Sahara today?";
 };
