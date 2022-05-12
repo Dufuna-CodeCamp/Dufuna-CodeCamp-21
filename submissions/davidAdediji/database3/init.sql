f006a9289ac4c66e6a33632856445656413c6eed
Emma Paris <davidadediji@gmail.com>
2022-05-12 12:55:04 +0100

    update changes

--- submissions/davidAdediji/database3/init.sql
+++
@@ -1,9 +0,0 @@
--- Total number of passengers that survived 
-SELECT COUNT(passenger_survived) 
-    FROM accident_cases 
-    WHERE passenger_survived = 1;
-
--- Total number of passengers that didn't survive
-SELECT COUNT(passenger_survived) 
-    FROM accident_cases 
-    WHERE passenger_survived = 0;
