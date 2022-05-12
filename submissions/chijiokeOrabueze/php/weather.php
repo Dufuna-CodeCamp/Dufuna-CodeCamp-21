c702186338c077d54a21ed8c7ebf579eba9cbac0
ChijiokeOrabueze <95033997+ChijiokeOrabueze@users.noreply.github.com>
2022-05-12 07:08:55 -0700

    added codes for php task 2

---
+++ submissions/chijiokeOrabueze/php/weather.php
@@ -0,0 +1,14 @@
+<?php
+
+$temperature = 34;
+
+
+if ($temperature <= 20 ){
+    echo "It is freezing today";
+}elseif($temperature > 20 && $temperature < 30){
+    echo "The weather is just perfect";
+}elseif($temperature >= 30 && $temperature < 40){
+    echo "It's so hot today";
+}elseif($temperature >= 40){
+    echo "Am I in the Sahara Desert?";
+};
