cb613950b73df839af0acd90c8cfa3ebdcff1f1a
OgunronbiMayowa <ogunronbimayowa13@gmail.com>
2022-05-14 13:38:46 -0500

    Conditionals/Loop

---
+++ submissions/mayowaOgunronbi/JavaScript/conditionals/script.js
@@ -0,0 +1,15 @@
+var numbers = 123 - 7;
+
+for (let i = 1; i <= numbers; i++) {
+    if (i % 3 === 0 && i % 5 == 0) {
+        document.write('<p>Software Deveoper</p>');
+    } else if (i % 5 === 0) {
+        document.write('<p>Developer</p>');
+    } else if (i % 3 === 0) {
+        document.write('<p>Software</p>');
+    } else {
+        document.write(`<p>${i}</p>`);
+    }
+}
+
+// document.write("<p>" + i + "</p>");
