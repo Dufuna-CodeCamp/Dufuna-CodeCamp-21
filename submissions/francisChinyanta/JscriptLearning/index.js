6f8c8217acffd255b2d561822467dd0569775cc5
fchinyanta <fc.chinyanta@gmail.com>
2022-05-15 14:05:51 +0200

    Printing the Product Price wit JavaScript

---
+++ submissions/francisChinyanta/JscriptLearning/index.js
@@ -0,0 +1,19 @@
+let hour = new Date().getHours();
+//console.log(hour);
+/*
+if (hour > 18) 
+    {console.log("Good evening")}
+    else{console.log("Good night")}
+*/
+let fruit = "Papayasi";
+
+switch (fruit){
+    case 'Orange':
+        console.log('I love oranges');
+        break;
+    case 'Papayas':
+        console.log('Papayas are amazing');
+        break;
+    default:
+        console.log("I don't have a fovourite")
+}
