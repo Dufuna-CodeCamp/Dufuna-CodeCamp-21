e623fbbd1ecccdb2dcb30cd20eb0f6e19136553b
John Promise Chilufya <51807673+Promise316@users.noreply.github.com>
2022-05-16 14:00:11 +0200

    I have added these files

---
+++ submissions/promiseChilufya/SlimTask/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/../app/app.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+
+$app->get("/", function(Request $request, Response $response) {
+
+    $response->getBody()->write("Hello World");
+
+    return $response;
+
+});
+
+$app->run();
