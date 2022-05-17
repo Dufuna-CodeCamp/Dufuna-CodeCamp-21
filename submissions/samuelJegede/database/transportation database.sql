93f844e6701505ced5848447e864ac0251c9d45b
JegedeSB <102415456+JegedeSB@users.noreply.github.com>
2022-05-17 14:01:42 +0100

    Transport database

--- submissions/samuelJegede/database/transportation database.sql
+++ submissions/samuelJegede/database/transportation database.sql
@@ -3,17 +3,17 @@ USE transport;
 CREATE TABLE passengers (
 	id INT NOT NULL AUTO_INCREMENT,
     full_name VARCHAR(250) NOT NULL,
-    sex VARCHAR(6) NOT NULL,
-    age VARCHAR(3),
+    sex VARCHAR(250) NOT NULL,
+    age INT,
     PRIMARY KEY (id)
 );
 CREATE TABLE trip_details (
 	id INT NOT NULL AUTO_INCREMENT,
     passenger_id INT NOT NULL,
     passenger_class ENUM('1', '2', '3') NOT NULL,
-    passenger_ticket_number VARCHAR(10) NOT NULL,
-    trip_fare VARCHAR(20) NOT NULL,
-    assigned_cabin VARCHAR(4),
+    passenger_ticket_number VARCHAR(250) NOT NULL,
+    trip_fare FLOAT(15,2) NOT NULL,
+    assigned_cabin VARCHAR(250),
     parents_children INT NOT NULL,
     siblings_spouuses INT NOT NULL,
     embarkation_point VARCHAR(250) NOT NULL,
@@ -23,6 +23,7 @@ CREATE TABLE trip_details (
 
 CREATE TABLE accidents (
 	id INT NOT NULL AUTO_INCREMENT,
-    survived VARCHAR(3) NOT NULL,
-    PRIMARY KEY (id)
+    survived ENUM('0','1') NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES passengers (id) 
 );
