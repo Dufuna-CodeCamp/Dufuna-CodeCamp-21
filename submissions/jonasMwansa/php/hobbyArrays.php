db79cf6f1844c6a236ce469377fb90ce890513db
jomlink <jonasmwansa7@gmail.com>
2022-05-17 17:22:47 +0200

    commit on the changes in the looping and hobbies content

--- submissions/jonasMwansa/php/hobbyArrays.php
+++ submissions/jonasMwansa/php/hobbyArrays.php
@@ -2,11 +2,42 @@
 
     //define and initialise the arrays
     $firstnames = ['Jonas', 'Francis', 'James', 'Richard', 'Joseph', 'Vincent', 'Edwin', 'Aaron'];
-    $hobbies = ['programming', 'playing football', 'betting', 'laboratory experiments', 'playing guitar', 'programming', 'photography', 'exploration'];
+    $hobbies = [
+            [' programming ','watching documentaries', 'wild exprolation','jogging'],
+            ['playing football', 'watching soccer'], 
+            ['betting', 'playing cards'], 
+            ['laboratory experiments', 'candy crush'], 
+            ['playing guitar', 'dancing', 'composing music'], 
+            ['programming'], 
+            ['photography', 'canoeing', 'swimming'], 
+            ['wild exprolation','swimming', 'singing']
+        ];
+
+    // foreach( $firstnames as $hobby => $name ) {
+    //     echo("My name is {$name} . I love {$hobbies[$hobby]} <br/>");
+    // }
 
     //we loop through
-    foreach( $firstnames as $hobby => $name ) {
-        echo("My name is {$name} . I love {$hobbies[$hobby]} <br/>");
+    for($counter = 0; $counter<count($firstnames); $counter++){
+
+        for($hobby =0; $hobby<count($hobbies); $hobby++){
+           
+            echo("My name is " . $firstnames[$counter] ." I love ");
+            $counter++;
+            
+            for($hobbyItem=0; $hobbyItem<count($hobbies[$hobby]); $hobbyItem++){
+                
+                //we check if the hobby item is no the last one we add a comma otherwise  we append append a full stop
+                if($hobbyItem!= count($hobbies[$hobby]) - 1){
+                 echo ($hobbies[$hobby][$hobbyItem] .", ");
+                }
+                else{
+                    echo ($hobbies[$hobby][$hobbyItem] .".");
+                }
+            }
+            echo("<br/><br/>");
+        }
+
     }
 
 ?>
