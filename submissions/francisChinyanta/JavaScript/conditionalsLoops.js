abbcfc7711009d7d223990eb7fec5159f9991289
FabunmiTolulopeEniola <70230223+ToluEnny24@users.noreply.github.com>
2022-05-18 10:47:26 +0100

    Codefactor fixed

--- submissions/francisChinyanta/JavaScript/conditionalsLoops.js
+++ submissions/francisChinyanta/JavaScript/conditionalsLoops.js
@@ -1,11 +1,11 @@
  /* JavaScript Second Task */
  var positiveDifference = 123 - 7;
- for(var x = 1;x < positiveDifference;x++){
+ for(var x = 1; x < positiveDifference; x++){
      if(x % 3 === 0){
         console.log("Software");
     }else if(x % 5 === 0){
         console.log("Developer"); 
-    }else if (x % 3 === 0 && x % 5 ===0){
+    }else if(x % 3 === 0 && x % 5 === 0){
          console.log("Software Developer");
      }else 
  console.log(x);
