71975f2dd42a255319d7e35f52f3b64f05338ddd
JegedeSB <102415456+JegedeSB@users.noreply.github.com>
2022-05-19 09:12:01 +0100

    Add files via upload

---
+++ submissions/samuelJegede/database/querying transport database.sql
@@ -0,0 +1,32 @@
+-- Number of passengers that survived
+SELECT COUNT(survived) FROM accidents WHERE survived = 1;
+424
+
+-- Number of passengers that did not survive
+SELECT COUNT(survived) FROM accidents WHERE survived = 0;
+467
+
+-- Name and sex of passengers under the age of 27 that embarked at Queenstown and Cherbourg
+SELECT passengers.full_name, passengers.sex FROM passengers
+INNER JOIN trip_details ON passengers.passenger_id = trip_details.passenger_id
+WHERE passengers.age < 27 AND (trip_details.embarkation_point = 'C'
+or trip_details.embarkation_point = 'Q');
+
+-- Passengers that embarked at Southampton and survived
+SELECT COUNT(survived) FROM accidents 
+INNER JOIN trip_details ON accidents.passenger_id = trip_details.passenger_id
+WHERE trip_details.embarkation_point = 'S' AND accidents.survived = 1;
+353
+
+-- Id and name of passengers who paid a fare greater than $100 AND above the age of 35 had siblings or spouses on board
+SELECT passengers.passenger_id, passengers.full_name FROM passengers
+INNER JOIN trip_details ON passengers.passenger_id = trip_details.passenger_id
+WHERE trip_details.trip_fare > 100.00 AND passengers.age > 35
+AND trip_details.siblings_spouses > 0;
+
+-- Total number of passengers who paid a fare greater than $100 AND above the age of 35 had siblings or spouses on board
+SELECT COUNT(full_name) FROM passengers
+INNER JOIN trip_details ON passengers.passenger_id = trip_details.passenger_id
+WHERE trip_details.trip_fare > 100.00 AND passengers.age > 35
+AND trip_details.siblings_spouses > 0;
+9
