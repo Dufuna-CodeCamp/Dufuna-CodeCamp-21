c7d0cc07227854b654f2178dd290f77743c8f7e9
mirriam6 <mirriammlaliki640@gmail.com>
2022-05-20 10:12:06 +0200

    I have created a modification page with react.

--- submissions/mirriamSikalume/REACT/react-app/src/App.js
+++ submissions/mirriamSikalume/REACT/react-app/src/App.js
@@ -2,8 +2,9 @@
 import './App.css';
 import { Header } from './components/header';
 import { Body } from './components/CartTitle';
-
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import {CartContent} from './components/CartContent'
+import { VerificationHolder } from './components/VerificationHolder';
 
 
 function App() {
@@ -12,9 +13,15 @@ function App() {
   return (
     <div>
     
-     <Header />
-     <Body />
-     <CartContent  />
+     <Router>
+       <Routes>
+          <Route path="/" element={<CartContent  />} />
+
+          <Route path="/Verification" element={<VerificationHolder />} />
+       </Routes>
+       
+     </Router>
+     
     </div>
     
     
