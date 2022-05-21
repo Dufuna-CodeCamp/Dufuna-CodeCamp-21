940481cad59eb2e4b116cd45d32222c76169aa8f
dejimuba@gmail.com <mac@Macs-MacBook-Pro.local>
2022-05-21 02:55:49 +0100

    I made the corrections to all comments as stated.

--- submissions/mubarakOlaoluwa/JavaScript/PrintingProductPrice/printingproductprice.js
+++ submissions/mubarakOlaoluwa/JavaScript/PrintingProductPrice/printingproductprice.js
@@ -1,27 +1,31 @@
-var productlist = ['object1', 'object2', 'object3'];
+function print (x) {
+
+    var productlist = ['object1', 'object2', 'object3'];
     
-    productlist[0] = {
-            productname1: 'Powerbank',
-            imageurl1: 'imageurl1',
-            price1: '#10,000'
-    };
+        productlist[0] = {
+            productname: 'Powerbank',
+            imageurl: 'imageurl1',
+            price: '#10,000'
+        };
 
-    productlist[1] = {
-        productname2: 'Hp laptop',
-        imageurl2: 'imageurl2',
-        price2: '#200,000'
-    };
+        productlist[1] = {
+            productname: 'Hp laptop',
+            imageurl: 'imageurl2',
+            price: '#200,000'
+        };
 
-    productlist[2] = {
-        productname3: 'Mobile Phone',
-        imageurl3: 'imageurl3',
-        price3: '#120,000'
-    };
+        productlist[2] = {
+            productname: 'Mobile Phone',
+            imageurl: 'imageurl3',
+            price: '#120,000'
+        };
 
-    document.write (productlist[0].productname1 + ' with ' + productlist[0].imageurl1 +
-                ' is sold for ' + productlist[0].price1 ); 
-    document.write ('<br>'+ productlist[1].productname2 + ' with ' + productlist[1].imageurl2 +
-                ' is sold for ' + productlist[1].price2 );
-    document.write ('<br>'+ productlist[2].productname3 + ' with ' + productlist[2].imageurl3 +
-                ' is sold for ' + productlist[2].price3 );
+return productlist[x].productname + ' with ' + productlist[x].imageurl +
+    ' is sold for ' + productlist[x].price ;  
 
+}
+
+for (i = 0; i < 3; i++)  {
+    document.write (print(i) + '<br>');
+
+}
