ad6638cdd0a9ebbad75a0b50d56078660409c2ac
Irene Nakiyingi <iryntracy@gmail.com>
2022-05-30 22:17:01 +0300

    create a database for a trasnportation company

---
+++ submissions/ireneNakiyingi/database/transportationdb.sql
@@ -0,0 +1,42 @@
+-- create and select database
+CREATE database transport_company;
+USE transport_company;
+
+-- create table for the information of the passengers
+CREATE TABLE registered_passengers(
+id INT NOT NULL auto_increment,
+passenger_id INT NOT NULL,
+passenger_name VARCHAR(255) NOT NULL,
+sex VARCHAR(10) NOT NULL ,
+age varchar(5) NULL,
+PRIMARY KEY(id)
+);
+select * from registered_passengers;
+
+-- create a table for the travel information of the passengers
+CREATE TABLE travel_details (
+id INT NOT NULL AUTO_INCREMENT,
+travel_id INT NOT NULL,
+passenger_class ENUM('1','2','3') NOT NULL,
+ticket_number VARCHAR(100) NOT NULL,
+trip_fare DECIMAL(10,4) NOT NULL,
+assigned_cabin VARCHAR(10) NULL,
+number_of_parents_children INT NOT NULL,
+number_of_siblings_spouses INT NOT NULL,
+point_of_embarkation VARCHAR(100) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id) REFERENCES registered_passengers(id)
+);
+select * from travel_details;
+
+-- table in the event of an accident, status = alive/dead
+CREATE TABLE flight_status(
+id INT NOT NULL AUTO_INCREMENT,
+traveller_id INT NOT NULL,
+status VARCHAR(10) NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id) REFERENCES registered_passengers(id)
+);
+select * from flight_status;
+
+
