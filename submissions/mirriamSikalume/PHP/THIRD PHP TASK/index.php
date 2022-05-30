4e26d2a58ed80da56db9d6f3c34ddaa086c08b17
Mirriam Sikalume <mirriammlaliki640@gmail.com>
2022-05-30 14:35:21 +0200

    Hobby Arrays

---
+++ submissions/mirriamSikalume/PHP/THIRD PHP TASK/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$names = array("Mirriam", "Jessy", "Henry", "Lemmy", "Sydney", "Kelvin", "Serah", "Royd");
+$hobbies = array("Playing Netball", "Jogging", "Watching Soccer", "Travelling", "Visiting neew places", "Gardening", "Singing", "Writing");
+
+for ($n=0; $n < 8; $n++) { 
+    echo "My name is $names[$i], I love $hobbies[$n].<br />";
+}
