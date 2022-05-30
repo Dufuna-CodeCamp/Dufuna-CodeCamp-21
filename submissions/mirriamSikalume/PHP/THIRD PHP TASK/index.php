955bf5e9e8c61b022dcc383624c92f33e85acac0
Mirriam Sikalume <mirriammlaliki640@gmail.com>
2022-05-30 15:30:15 +0200

    I have made changes to my Hobby Array

--- submissions/mirriamSikalume/PHP/THIRD PHP TASK/index.php
+++ submissions/mirriamSikalume/PHP/THIRD PHP TASK/index.php
@@ -4,5 +4,5 @@ $names = array("Mirriam", "Jessy", "Henry", "Lemmy", "Sydney", "Kelvin", "Serah"
 $hobbies = array("Playing Netball", "Jogging", "Watching Soccer", "Travelling", "Visiting neew places", "Gardening", "Singing", "Writing");
 
 for ($n=0; $n < 8; $n++) { 
-    echo "My name is $names[$i], I love $hobbies[$n].<br />";
+    echo "My name is $names[$n], I love $hobbies[$n].<br />";
 }
