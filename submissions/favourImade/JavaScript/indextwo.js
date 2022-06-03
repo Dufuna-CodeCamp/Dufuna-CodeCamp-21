606b83a8e098abd3d8f420ba5fc911130d7624cb
Favour Imade <imadefavour23@gmail.com>
2022-06-03 17:23:39 +0100

    i did the conditionals task

--- submissions/favourImade/JavaScript/indextwo.js
+++ submissions/favourImade/JavaScript/indextwo.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-var diff = 123 - 7;
-
-for(var i = 1; i <= diff; i++) {
-    if (i % 3 === 0 && i % 5 === 0){
-        document.write("Software Developer" + "<br><br>");
-    }
-    else if (i % 3 === 0){
-        document.write("Software" + "<br><br>");
-    }
-    else if (i % 5 ===0){
-        document.write("Developer" + "<br><br>");
-    }
-    else {
-        document.write(i+ "<br><br>");
-    }
-=======
 var diff = 123-7;
-
 for(i = 1; i <= diff; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         document.write("Software Developer" + "<br><br>");
@@ -33,6 +15,5 @@ for(i = 1; i <= diff; i++) {
     else {
         document.write(i + "<br><br>");
     }
-    
->>>>>>> afe4860eb280c00f100719b70ea149f80ca8b7ee
+
 }
