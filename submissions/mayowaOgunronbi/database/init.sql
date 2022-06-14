92f1d5551da525b828d79f0ec2ce53e6564b4374
OgunronbiMayowa <ogunronbimayowa13@gmail.com>
2022-06-14 13:46:17 -0500

    init

---
+++ submissions/mayowaOgunronbi/database/init.sql
@@ -0,0 +1,65 @@
+-- TO CREATE A DATABASE CALLED FODBAG
+CREATE DATABASE foodbag;
+SHOW DATABASES;
+USE foodbag;
+SHOW TABLES;
+
+-- TO CREATE THE FOOD TABLE
+CREATE TABLE food (
+	id INT AUTO_INCREMENT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+SELECT * FROM food;
+
+
+-- TO CREATE THE VENDOR FOOD TABLE
+CREATE TABLE vendors (
+	id INT AUTO_INCREMENT NOT NULL,
+    admin_id INT NOT NULL,
+    food_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    amount DOUBLE NOT NULL,
+    PRIMARY KEY (id),
+--     LINKING IT TO THE FOOD TABLE
+    FOREIGN KEY (food_id) REFERENCES food (id)
+);
+SELECT * FROM vendors;
+
+
+-- TO CREATE THE ADMINS TABLE
+CREATE TABLE admins (
+	id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR (255) NOT NULL,
+    type INT NOT NULL,
+    email_address VARCHAR (255) NOT NULL,
+    phone_number INT NOT NULL,
+    password VARCHAR (255) NOT NULL,
+    PRIMARY KEY (id)
+);
+SELECT * FROM admins;
+
+
+-- TO CREATE THE CUSTOMERS TABLE
+CREATE TABLE customers (
+	id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR (255) NOT NULL,
+    phone_number INT NOT NULL,
+    address VARCHAR (255) NOT NULL,
+    payment_method VARCHAR (255),
+    PRIMARY KEY (id)
+);
+SELECT * FROM customers;
+
+
+-- TO CREATE THE ORDERS TABLE
+CREATE TABLE orders (
+	id INT AUTO_INCREMENT NOT NULL,
+    vendors_id INT NOT NULL,
+    customers_id INT NOT NULL,
+    PRIMARY KEY (id),
+    --     LINKING IT TO THE VENDORS AND CUSTOMERS TABLES
+    FOREIGN KEY (vendors_id) REFERENCES vendors (id),
+    FOREIGN KEY (customers_id) REFERENCES customers (id)
+);
+SELECT * FROM orders;
