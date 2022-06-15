236e0865b50a2622bada2d694102d57feb39af33
Stanley Mubanga <estabannabum@gmail.com>
2022-06-15 07:02:22 -0700

    created cart page

---
+++ submissions/stanleyMubanga/REACT/react-app/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+
+  return (
+    <div className="top-header">
+      <div className='logo'>
+        <h4 className='logoA'>food</h4>
+        <h4 className='logoB'>Bag</h4>
+      </div>
+      <ul className='topNav'>
+        <li><a href="#" className='page-links'>Cart</a></li>
+        <li><a href="#" className='page-links'>Log in</a></li>
+        <li><a href="#" className='border-btn'>Sign up</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header;
