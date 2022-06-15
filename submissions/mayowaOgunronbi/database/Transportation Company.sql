fc9adaa998b1ec1a96f7c44edb057fdb8c1cf110
OgunronbiMayowa <ogunronbimayowa13@gmail.com>
2022-06-14 20:50:32 -0500

    Transportation Company

--- submissions/mayowaOgunronbi/database/Transportation Company.sql
+++ submissions/mayowaOgunronbi/database/Transportation Company.sql
@@ -1,32 +1,33 @@
-create database transportation_company;
-show databases;
-use transportation_company;
-create table passengers (
-	id int auto_increment not null,
-    full_name varchar(100) not null,
-    sex varchar(10) not null,
-    age int,
-    primary key (id)
-);
-select * from passengers;
-use transportation_company;
-create table travel_details (
-	id int auto_increment not null,
-    class int not null,
-    ticket varchar(10) not null,
-    fare decimal(10,2) not null,
-    cabin varchar(10),
-    parents_children int not null,
-    siblings_spouses int not null,
-    embarked varchar(50) not null,
-    primary key (id)
-);
-select * from travel_details;
+CREATE DATABASE transportation_company;
+USE transportation_company;
 
-create table survivals (
-	id int auto_increment not null,
-    survived int not null,
-    primary key (id)
+
+CREATE TABLE passengers (
+	id INT AUTO_INCREMENT NOT NULL,
+    full_name VARCHAR(250) NOT NULL,
+    sex VARCHAR(50) NOT NULL,
+    age INT,
+    PRIMARY KEY (id)
 );
-select * from survivals;
-show tables in transportation_company;
+
+
+CREATE TABLE travel_details (
+	id INT AUTO_INCREMENT NOT NULL,
+    class INT NOT NULL,
+    ticket VARCHAR(50) NOT NULL,
+    fare DECIMAL(10,2) NOT NULL,
+    cabin VARCHAR(10),
+    parents_children INT NOT NULL,
+    siblings_spouses INT NOT NULL,
+    embarked VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE survivals (
+	id INT AUTO_INCREMENT NOT NULL,
+    survived INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
