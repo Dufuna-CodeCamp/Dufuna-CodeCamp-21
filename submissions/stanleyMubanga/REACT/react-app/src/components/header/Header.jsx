71657f8753531c3c056d5d5b817b1d3bb3f2318d
Stanley Mubanga <estabannabum@gmail.com>
2022-06-15 09:41:40 -0700

    edited cart page

--- submissions/stanleyMubanga/REACT/react-app/src/components/header/Header.jsx
+++ submissions/stanleyMubanga/REACT/react-app/src/components/header/Header.jsx
@@ -10,9 +10,9 @@ function Header() {
         <h4 className='logoB'>Bag</h4>
       </div>
       <ul className='topNav'>
-        <li><a href="#" className='page-links'>Cart</a></li>
-        <li><a href="#" className='page-links'>Log in</a></li>
-        <li><a href="#" className='border-btn'>Sign up</a></li>
+        <li><a href="/" className='page-links'>Cart</a></li>
+        <li><a href="/" className='page-links'>Log in</a></li>
+        <li><a href="/" className='border-btn'>Sign up</a></li>
       </ul>
     </div>
   )
