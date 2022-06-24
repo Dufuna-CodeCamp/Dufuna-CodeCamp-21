a393bfbe0cc60fe5f8967b1429ab0c0db8b6345d
JegedeSB <102415456+JegedeSB@users.noreply.github.com>
2022-06-24 08:30:25 +0100

    code factor fixed

--- submissions/samuelJegede/php/unitconversion.php
+++ submissions/samuelJegede/php/unitconversion.php
@@ -2,17 +2,16 @@
 
 function convertUnits($number){
   $pound = $number * 2.205;
-  echo($number. " kilos". " to pounds is ".$number * $pound." lb");
-  echo"<br/>";
+  echo("$number  kilos to pounds is $pound lb");
+  echo"<br><br>";
 
   $yard = $number * 1.094;
-  echo($number." meters". " to yards is ".$yard * $number." yards");
-  echo"<br/>";
+  echo("$number  meters to yards is  $yard yards");
+  echo"<br><br>";
 
   $fahrenheit = ($number * 1.8) + 32;
-  echo($number." degrees Celsius to Fahrenheit is ");
-  echo(($number * 9/5) + 32 ." F");
-  echo"<br/>";
+  echo("$number degrees Celsius to Fahrenheit is $fahrenheit  F");
+  echo"<br><br>";
 
 }
 convertUnits(20);
