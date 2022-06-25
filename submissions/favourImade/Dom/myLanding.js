bf851eeb0a9a9e38278fcba11c44f1d35618847c
Favour Imade <imadefavour23@gmail.com>
2022-06-25 01:43:16 +0100

    I added validation to my Landing Page

---
+++ submissions/favourImade/Dom/myLanding.js
@@ -0,0 +1,30 @@
+let addressList = document.getElementById("address"),
+    fastFood = document.getElementById("fastfood"),
+    vegetables = document.getElementById("vegetables"),
+    resturant = document.getElementById("resturant"),
+    drinks = document.getElementById("drinks"),
+    interests = document.querySelectorAll("#inter"),
+    container = document.querySelectorAll(".interest");
+
+addressList.addEventListener('input', function(e) {
+    if(!(e.target.value == " ")){
+        fastFood.src = "../css/images/fast-food.svg";
+        vegetables.src = "../css/images/Vegetables & Drinks.svg";
+        drinks.src = "../css/images/Drinks & Cocktails.svg";
+        resturant.src = "../css/images/resturant.svg";
+
+        interests.forEach((inter) => {
+            inter.style.color = "brown";
+        });
+
+        for (let i = 0; i < container.length; i++){
+            container[i].addEventListener('click', loadVendor);
+            container[i].style.cursor = "pointer";
+        }
+
+        //function that loads the vendors page when an interest list item is loaded
+        function loadVendor() {
+            window.location = "vendor.html";
+        }
+    }
+})
