70390d593bb5ddc3e1c6e600840ea6e6cde862c8
Joey <josepholasimbo1@gmail.com>
2022-07-07 15:28:21 +0200

    Developer Conditionals

---
+++ submissions/josephOlasimbo/javascript/conditionals/index.js
@@ -0,0 +1,19 @@
+let value;
+
+for (let i = 1; i <= 116; i++) {
+    let string = "";
+    value = i;
+
+    if (i % 3 ===0) {
+        string = "Software"
+    }
+    if (i % 5 ===0) {
+        string = "Developer"
+    }
+    if (i % 3 ===0 && i % 5=== 0) {
+        string = "Software Developer"
+    }
+    value = (string) ? string : value;
+    document.write(`<p>${value}</p>`)
+}
+
