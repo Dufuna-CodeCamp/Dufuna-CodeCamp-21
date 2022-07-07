706b268f31d0cdffa74e70d2c07a620ab382e91a
joejoba <josepholasimbo1@gmail.com>
2022-07-07 14:47:45 +0200

    Conditionals

--- submissions/josephOlasimbo/javascript/conditionals/index.js
+++ submissions/josephOlasimbo/javascript/conditionals/index.js
@@ -1,16 +1,16 @@
 let value;
 
-for (let i = 1; i <= 116; i++) {
-    let string = "";
+for (let i =1; i<=116; i++) {
+    let string ="";
     value = i;
 
-    if (i % 3 === 0) {
+    if (i % 3 === 0){
         string = "Software"
     }
     if (i % 5 === 0) {
         string = "Developer"
     }
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 ===0 && i % 5 ===0){
         string = "Software Developer"
     }
     value = (string) ? string : value;
