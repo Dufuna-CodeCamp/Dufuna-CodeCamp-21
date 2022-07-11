5a848331666cc9599f944168aa1f0f339f185688
JegedeSB <102415456+JegedeSB@users.noreply.github.com>
2022-07-11 20:25:55 +0100

    Add files via upload

---
+++ submissions/samuelJegede/slim_php/routes/bootstrap/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Container\Container;
+
+$container = new Container();
+
+$container['settings'] = [
+    'determineRouteBeforeAppMiddleware' => false,
+    'displayErrorDetails' => true,
+    'db' => [
+        'driver' => 'mysql',
+        'host' => $_ENV['DB_HOSTNAME'],
+        'database' => $_ENV['DB_NAME'],
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+    ]
+];
