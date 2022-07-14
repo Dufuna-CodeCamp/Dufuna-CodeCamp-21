6e76eaab998fdf5b30a0eac725d2670df9602467
Stanley Mubanga <estabannabum@gmail.com>
2022-07-14 15:33:57 +0200

    created order success page

--- submissions/stanleyMubanga/REACT/react-app/src/App.js
+++ submissions/stanleyMubanga/REACT/react-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/header/Header';
 import Cart from './components/pages/cart/Cart';
 import Verification from "./components/pages/verificationPage/Verification";
+import OrderSuccess from './components/pages/orderPage/OrderSuccess';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Cart />} />
           <Route path="/Verification" element={<Verification />} />
+          <Route path="/OrderSuccess" element={<OrderSuccess />} />
         </Routes>
       </BrowserRouter>
     </div>
--- submissions/stanleyMubanga/REACT/react-app/src/components/pay/Pay.jsx
+++ submissions/stanleyMubanga/REACT/react-app/src/components/pay/Pay.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '../checkout/checkout.css';
 
 function Pay() {
   return (
     <button className='checkout'>
-    <button className='checkout-link'>Pay</button>
-</button>
+      <Link className='checkout-link' to="/OrderSuccess">Pay</Link>
+    </button>
   )
 }
 
