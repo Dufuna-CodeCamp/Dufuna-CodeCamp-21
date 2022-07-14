cb5f02d69d5fb51f87f5c1053729e2c4c88195a2
chidinma Odo <blessingodo25@gmail.com>
2022-07-14 14:39:17 -0700

    The Software Developer Conditionals

--- submissions/chidinmaOdo/javascript/conditionals.js
+++ submissions/chidinmaOdo/javascript/conditionals.js
@@ -5,16 +5,16 @@ let diff = 123 - 7;
  for (i = 1; i <= diff; i++){
     //conditions to be fulfilled using the if and else if statement
     //Numbers that are multiples of three(3)
-    if(i % 3 === 0){
-        document.write(`Software <br>`);
+    if(i % 3 === 0 && i % 5 === 0){
+        document.write(`Software Developer <br>`);
 
         //Numbers that are multiples of five (5)
-    } else if(i % 5 === 0){
-        document.write(`Developer  <br>`);
+    } else if(i % 3 === 0){
+        document.write(`Software  <br>`);
 
         //Numbers that are multiples of three(3) and five(5)
-    } else if (i % 3 === 0 && i % 5 === 0){
-        document.write(`Software Developers <br>`);
+    } else if (i % 5 === 0){
+        document.write(`Developer <br>`);
 
         //Numbers that are not multiple of three and five
     } else {
