59fde4e7952401de261dec3176c77b762da3ea0d
dejimuba@gmail.com <mac@Macs-MacBook-Pro.local>
2022-07-15 12:46:53 +0100

    All Changes have been updated

--- submissions/mubarakOlaoluwa/Database/init.sql
+++ submissions/mubarakOlaoluwa/Database/init.sql
@@ -8,30 +8,24 @@ CREATE TABLE food_table (
     PRIMARY KEY (id)
 );
 
-/*This inserts the various food types into the food_table database*/
-INSERT INTO food_table (type)
-	VALUES ('Fast food'),
-		   ('Vegetable & Fruits'),
-           ('Drinks & Cocktails'),
-           ('Resturants');
-
 /* This is the table for the second success criteria (vendors food) */
 CREATE TABLE vendor_food (
 	id INT NOT NULL AUTO_INCREMENT,
     admin_id INT NOT NULL,
     food_id INT NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(250) NOT NULL,
     amount DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (food_id) REFERENCES food_table(id)
+    FOREIGN KEY (food_id) REFERENCES food_table(id),
+    FOREIGN KEY (admin_id) REFERENCES food_table(id)
 );
 
 /*This is the table for the third success criteria (admins table)*/
 CREATE TABLE admins(
 	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(250) NOT NULL,
     type INT NOT NULL,
-    email_address VARCHAR(50) NOT NULL,
+    email_address VARCHAR(150) NOT NULL,
     phone_number INT NOT NULL,
     password VARCHAR(20) NOT NULL,
     PRIMARY KEY (id)
@@ -40,14 +34,10 @@ CREATE TABLE admins(
 /*This is the table for the fourth success criteria (customer table)*/
 CREATE TABLE customer(
 	id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    phone_number INT NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    payment_method VARCHAR(20) NOT NULL,
-    card_number INT NOT NULL,
-    card_name VARCHAR (50) NOT NULL,
-    expiry INT NOT NULL,
-    CVV INT NOT NULL,
+    name VARCHAR(250) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    address VARCHAR(250) NOT NULL,
+    payment_method VARCHAR(20),
     PRIMARY KEY (id)
 	);
     
@@ -55,9 +45,6 @@ CREATE TABLE customer(
 CREATE TABLE orders(
 	id INT NOT NULL AUTO_INCREMENT,
     vendor_id INT NOT NULL,
-    customer_id INT NOT NULL,
+    customer_id INT,
     PRIMARY KEY (id)
 	);
-
-
-
