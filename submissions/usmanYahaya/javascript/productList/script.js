d5f794fbc43388b3059f53ba7336db7f1c21b04b
Usman Yahaya <48054855+Usmaniyya@users.noreply.github.com>
2022-07-15 20:18:53 +0000

    made some changes

--- submissions/usmanYahaya/javascript/productList/script.js
+++ submissions/usmanYahaya/javascript/productList/script.js
@@ -3,7 +3,7 @@ var product_image = document.querySelector('.product-image');
 var product_price = document.querySelector('.product-price');
 
 var productList = [{
-        productName: "Groceries",
+        productName: "Grocery",
         imageUrl: "https://images.unsplash.com/photo-1584473457406-6240486418e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvY2VyaWVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         productPrice: "$4.00"
     },
@@ -13,7 +13,7 @@ var productList = [{
         productPrice: "$5.00"
     },
     {
-        productName: "Clothing",
+        productName: "Clothes",
         imageUrl: "https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2xvdGhpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
         productPrice: "$7.00"
     }
