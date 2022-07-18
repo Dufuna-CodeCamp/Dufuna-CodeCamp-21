59fc2ceabcc9b533dea9ad61484e35a4ed6b000f
FabunmiTolulopeEniola <70230223+ToluEnny24@users.noreply.github.com>
2022-07-18 12:54:24 +0100

    Removed ticket table

--- submissions/edwinNgoma/Database/transportation_database.sql
+++ submissions/edwinNgoma/Database/transportation_database.sql
@@ -12,24 +12,12 @@ age INT NULL,
 primary key(id)
 );
 
-/* tickets_table comes here*/
-
-CREATE TABLE tickets_table(
-id INT NOT NULL auto_increment,
-passengers_id INT NOT NULL,
-ticket_number VARCHAR(255) UNIQUE NOT NULL,
-date_purchased VARCHAR(50) NOT NULL,
-primary key(id),
-foreign key(passengers_id) references passengers_table(id)
-);
-
-
 /*create a trips table comes here*/
 
 CREATE TABLE trips_table(
 id INT NOT NULL auto_increment,
 passenger_id INT NOT NULL,
-ticket_id INT NOT NULL,
+ticket_number VARCHAR(250) UNIQUE NOT NULL,
 class_type INT NOT NULL,
 trip_fare FLOAT NOT NULL,
 cabin VARCHAR(255) NULL,
@@ -37,8 +25,7 @@ num_of_parents_or_children INT NULL,
 num_of_siblings_or_spouses INT NULL,
 embarkation_point VARCHAR(255) NOT NULL,
 primary key(id),
-foreign key(passenger_id) references passengers_table(id),
-foreign key(ticket_id) references tickets_table(id)
+foreign key(passenger_id) references passengers_table(id)
 );
 
 /* accidents_table comes here*/
