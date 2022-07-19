4432721fb19afade692b99680ed7466dadeff8c9
Usman Yahaya <48054855+Usmaniyya@users.noreply.github.com>
2022-07-19 11:20:58 +0000

    The Software Developer Conditionals

---
+++ submissions/usmanYahaya/javascript/conditionals/script.js
@@ -0,0 +1,13 @@
+var difference = Math.abs(7 - 123);
+console.log(difference)
+
+for (let i = 1; i <= difference; i++) {
+    if (i % 3 == 0) {
+        document.write(' <br> Software <br>')
+    } else if (i % 5 == 0) {
+        document.write('<br> Developer <br>')
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        document.write('Software Developer')
+    }
+
+}
