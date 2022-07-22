eaa1191141667f8f1b8f9cc255ba710db3b1efd3
FabunmiTolulopeEniola <70230223+ToluEnny24@users.noreply.github.com>
2022-07-22 12:43:25 +0100

    Codefactor error fixed

--- submissions/usmanYahaya/javascript/conditionals/script.js
+++ submissions/usmanYahaya/javascript/conditionals/script.js
@@ -1,12 +1,12 @@
 var difference = Math.abs(7 - 123);
 
 for (let i = 1; i <= difference; i++) {
-    if (i % 3 === 0) {
-        document.write(' <br> Software <br>')
+    if (i % 3 === 0 && i % 5 === 0) {
+        document.write(' <br> Software Developer<br>')
     } else if (i % 5 === 0) {
         document.write('Developer <br>')
-    } else if (i % 3 === 0 && i % 5 === 0) {
-        document.write('Software Developer')
+    } else if (i % 3 === 0) {
+        document.write('Software')
     } else {
         document.write(i + '<br>')
     }
