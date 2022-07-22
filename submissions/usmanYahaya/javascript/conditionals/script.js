90c24a916630db6c39535bd94cba7116d11fbdd8
Usman Yahaya <48054855+Usmaniyya@users.noreply.github.com>
2022-07-22 06:43:57 +0000

    add strict equality operator

--- submissions/usmanYahaya/javascript/conditionals/script.js
+++ submissions/usmanYahaya/javascript/conditionals/script.js
@@ -2,11 +2,11 @@ var difference = Math.abs(7 - 123);
 console.log(difference)
 
 for (let i = 1; i <= difference; i++) {
-    if (i % 3 == 0) {
+    if (i % 3 === 0) {
         document.write(' <br> Software <br>')
-    } else if (i % 5 == 0) {
+    } else if (i % 5 === 0) {
         document.write('<br> Developer <br>')
-    } else if (i % 3 == 0 && i % 5 == 0) {
+    } else if (i % 3 === 0 && i % 5 === 0) {
         document.write('Software Developer')
     } else {
         document.write(i + '<br>')
