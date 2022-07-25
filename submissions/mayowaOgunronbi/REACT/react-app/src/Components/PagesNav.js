cde2be456fd662f8200e0b969996cba19e4c236b
OgunronbiMayowa <ogunronbimayowa13@gmail.com>
2022-07-25 01:35:58 -0500

    React Cart Page

---
+++ submissions/mayowaOgunronbi/REACT/react-app/src/Components/PagesNav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PagesNav = () => {
+
+    return (
+
+        <div className="PagesNav">
+            <p> <a href="#"><span>Home {'>'}</span></a> Cart</p>
+            <p id="Cart">Cart</p>
+        </div>
+    )
+}
+
+export default PagesNav;
---
+++ submissions/mayowaOgunronbi/REACT/react-app/src/Components/Verification.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Verification = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Verification;
