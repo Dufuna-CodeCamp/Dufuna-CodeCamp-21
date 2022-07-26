4d898e74389e869df466bef571f87c40de1eba4f
Irene Nakiyingi <iryntracy@gmail.com>
2022-07-26 18:07:48 +0300

    React cart page

---
+++ submissions/ireneNakiyingi/REACT/food-bag/src/components/Checkout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Checkout.css'
+
+
+function checkOut(){
+    
+    return(
+        <div>
+            <button  className='checkout'>Checkout</button>
+       
+        </div>
+        
+    )
+}
+
+export default checkOut;
