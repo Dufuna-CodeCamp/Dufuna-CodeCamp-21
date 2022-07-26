170844f376e7609845f20f8e8e666cada76fff34
chidinma Odo <blessingodo25@gmail.com>
2022-07-25 17:44:17 -0700

    Validation of landing page

---
+++ submissions/chidinmaOdo/DOM/landingPage.js
@@ -0,0 +1,38 @@
+let fastFood = "../css/image/fast-food.svg";
+let drink = "../css/image/Drinks & Cocktails.svg";
+let restaurant = "../css/image/resturant.svg";
+let vegetable = "../css/image/Vegetables & Drinks.svg";
+let address = document.getElementById("locate");
+let images = document.querySelectorAll(".img-box");
+let imageTitle = document.querySelectorAll(".img-title");
+let cards = document.querySelectorAll(".box1");
+let vendorUrl = "./vendorpage.html";
+
+let imageArr = [fastFood, drink, restaurant, vegetable];
+
+const changeFunct = () => {
+    let selectedValue = address.options[address.selectedIndex].value;
+
+    //document.getElementsByClassName(".img-title"). style.color = "red";
+    
+    imageTitle.forEach((item) => {
+        item.classList.remove("img-title");
+        item.classList.add("img-title--red");
+    });
+
+    images.forEach((item, index) => {
+        item.src = imageArr[index];
+    });
+    console.log(selectedValue);
+    cards.forEach((item) => {
+        item.addEventListener("click", () => {
+            window.location.href = vendorUrl;
+        });
+    });
+};
+
+address.addEventListener("change", changeFunct);
+
+
+
+console.log("Hello");
