8c7519c2969fdc7ba7ba4312c5ccab48bf00ddf7
Stanley Mubanga <estabannabum@gmail.com>
2022-07-30 08:58:31 +0200

    crested weather.php

--- submissions/stanleyMubanga/PHP/unit_convertor.php
+++ submissions/stanleyMubanga/PHP/unit_convertor.php
@@ -6,11 +6,11 @@
 
     function unitConvertor($sum){
         $pounds = $sum * 2.20462;
-        $yards = $sum * 1.09361;
+        $yard = $sum * 1.09361;
         $fahrenheit = $sum * 1.8 + 32;
 
         echo "$sum Kilos converts to: $pounds Pounds <br/>";
-        echo "$sum meters converts to: $yards Yards <br/>";
+        echo "$sum meters converts to: $yard Yards <br/>";
         echo "$sum Degrees celcius converts to: $fahrenheit fahrenheit <br/>";
     }
 
