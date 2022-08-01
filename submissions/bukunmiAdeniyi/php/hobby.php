8635e6c1bce854fee008089bd38afefd7f870ee6
bukiz-cloud <adeniyibukunmiadetunji@gmail.com>
2022-08-01 08:18:20 -0700

    Creating hobby php file

---
+++ submissions/bukunmiAdeniyi/php/hobby.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$names = ["Tola", "Tolu", "Tayo", "Tade", "Toyo", "Titi", "Taye", "Toke"];
+$hobbies = ["Running", "Writing", "Reading", "Researching", "Travelling", "skating","Browsing", "Playing"];
+
+for ($i=0; $i < 8; $i++){
+    echo "My name is $names[$i]. i love $hobbies[$i] <br>";
+}
+?>
