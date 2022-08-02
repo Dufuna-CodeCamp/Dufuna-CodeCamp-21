0490da62b4509221abf679bcf1337869d575dec5
Irene Nakiyingi <iryntracy@gmail.com>
2022-08-03 00:07:42 +0300

    Verification page with react

---
+++ submissions/ireneNakiyingi/REACT/food-bag/src/components/Vbutton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Vbutton.css'
+
+function VButton(){
+    return(
+        <div >
+            <button className='paybtn' value='Pay'>
+            <span>Pay</span>
+           </button>
+        </div>
+        
+    )
+
+};
+
+export default VButton;
