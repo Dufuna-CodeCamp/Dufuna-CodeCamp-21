f617135cda3a1c3d405941e7c500e7fd94692893
Bukola Oyetimehin <73591664+Mayowa2020@users.noreply.github.com>
2022-08-08 18:13:52 +0100

    Update conditionals.js
    Updated the code on line 3 by including "=" sign.
    NB: Numbers from 1 and the positive difference between 7&123 (ie find the positive difference and print the numbers between 1 and that number while fulfilling other criteria)

--- submissions/blessingOluwapelumi/javascript/conditionals/conditionals.js
+++ submissions/blessingOluwapelumi/javascript/conditionals/conditionals.js
@@ -1,6 +1,6 @@
 var difference = 123-7;
 
-for (let i = 0; i < difference; i++) {
+for (let i = 0; i <=difference; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     document.write(` <p> Software Developer </p>`);
   } else if (i % 3 == 0) {
