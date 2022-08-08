c9765c8570472d809b3e97075730d284e17d381b
davidadediji <davidadediji@gmail.com>
2022-08-08 07:25:45 +0100

    new changes

--- submissions/davidAdediji/REACT/react-app/src/App.js
+++ submissions/davidAdediji/REACT/react-app/src/App.js
@@ -4,12 +4,13 @@ import OrderSuccess from './components/OrderSuccess';
 import {
   BrowserRouter,
   Routes,
-  Route,
+  Route
 } from "react-router-dom";
 import products from './data';
 import { useState } from 'react';
 
 function App() {
+
   const [cartItems, setCartItems] = useState([]);
 
   const onAdd = (product)=>{
@@ -22,11 +23,23 @@ function App() {
       setCartItems([...cartItems, {...product, quantity:1}]);
     }
   }
+
+
+  const onRemove = (product)=>{
+    const exist = cartItems.find((item)=>item.id===product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((item)=> item.id === product.id?{...exist, quantity:exist.quantity - (exist.quantity <= 1 ? 0:1)}:item)
+      )
+    }else{
+      setCartItems([...cartItems, {...product, quantity:1}]);
+    }
+  }
   return (
     <BrowserRouter>
     <Routes>
       <Route>
-        <Route path="/" element={<Cart products={products} onAdd={onAdd}/>} />
+        <Route path="/" element={<Cart products={products} onAdd={onAdd} onRemove={onRemove}/>} />
         <Route path="/success" element={<OrderSuccess/>} />
         <Route path="*" element={
           <main style={{ padding: "1rem"}}>
--- submissions/davidAdediji/REACT/react-app/src/components/Cart.js
+++ submissions/davidAdediji/REACT/react-app/src/components/Cart.js
@@ -1,8 +1,15 @@
 import Cancel from '../asserts/icons/cancel.png'
 import NavBar from "./Navbar";
 import Button from "./Button";
+import Plus from '../asserts/icons/plus.svg'
+import Minus from '../asserts/icons/Minus.svg'
+import Scooter from '../asserts/images/scooter 1.svg'
+import { useNavigate } from 'react-router-dom';
 
-const Cart = ({products, onAdd})=>{
+const Cart = ({products, onAdd, onRemove})=>{
+    const total = 0;
+    const deliveryFee = 1200;
+    const navigate = useNavigate();
     return(
         <main>
             <NavBar/>
@@ -20,30 +27,41 @@ const Cart = ({products, onAdd})=>{
                         </div>
                         <div className='cart-item border'>
                             <div>
-                                <img src={product.img} alt="" />
+                                <img className="cart-item-img"src={product.img} alt="" />
                             </div>
                             <div>
-                                Text
+                                <p className='item-name'>{product.name}</p>
+                                <div className='item-change-qty'>
+                                    <div>
+                                        <button className='btn-small'><img className='img-plus' src={Plus} alt="" /></button>
+                                    </div>
+                                    
+                                    <span className='qty'>{product.quantity}</span>
+                                    <div>
+                                        <button className='btn-small'><img className='img-minus' src={Minus} alt="" /></button>
+                                    </div>
+                                    
+                                </div>
                             </div>
                             <div className='item-price'>
-                                <p>{product.price}</p>
+                                <p className='item-price'>&#8358;{product.price}</p>
                             </div>
                         </div>
                     </section>
             )})}
                 <section className='cart'>
-                    <div className='border'>
-                        <img className='small' src={Cancel} alt="" />
+                    <div className='indent'>
+                        
                     </div>
                     <div className='cart-item border'>
                         <div>
-                            <img src="" alt="" />
+                            <img src={Scooter} alt="" />
                         </div>
                         <div>
-                            Text
+                            <p className='item-name center-delivery-fee'>Delivery fee</p>
                         </div>
                         <div className='item-price'>
-                            <p></p>
+                            <p>&#8358;{deliveryFee}</p>
                         </div>
                     </div>
                 </section>
