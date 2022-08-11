cb318a133fcb7de1ad3c8f9f1621e6cba2b028e1
Temitope Johnson <temitopejohnson4ts@gmail.com>
2022-08-11 13:50:08 +0100

    fixed codeFactor error

--- submissions/temitopeJohnson/productArrayAlgo/product.js
+++ submissions/temitopeJohnson/productArrayAlgo/product.js
@@ -1,19 +1,3 @@
-function priceChange() {
-    var productsLength = products.length;
-    for (let i = 0; i < productsLength; i++) {
-        if (products[i].length % 2 === 1) {
-            productPrice[i] = productPrice[i] / 2;
-        }
-        else {
-            if (products.length % 2 === 0) {
-                productPrice[i] = productPrice[i] * 2;
-            }
-        }
-        console.log('The price of ' + products[i] + ' is ' + productPrice[i]);
-    }
-    return priceChange;
-}
-
 const products = ['Canon Kit Lens 15-55mm', 'Nikon 3100', 'Samsung NX300',
     'Canon EOS 80D', 'Sony Alpha A7s', 'Sony NX 300M', 'Panasonic Lumix GH4',
     'Panasonic Lumix DC-GH5', 'Xiaomi Y1-M1', 'Fujifilm X-A10',
@@ -24,4 +8,15 @@ const productPrice = ['500.00', '200.00', '750.00', '2000.00', '1600.00',
     '300.00', '800.00', '5000.00', '500.00', '600.00', '1200.00', '1200.00', '2000.00',
     '2000.00', '5000.00', '750.00', '200.00', '800.00', '900.00', '750.00']
 
-var productName = priceChange();
+var productsLength = products.length;
+for (let i = 0; i < productsLength; i++) {
+    if (products[i].length % 2 === 1) {
+        productPrice[i] = productPrice[i] / 2;
+    }
+    else {
+        if (products.length % 2 === 0) {
+            productPrice[i] = productPrice[i] * 2;
+        }
+    }
+    console.log('The price of ' + products[i] + ' is ' + productPrice[i]);
+}
