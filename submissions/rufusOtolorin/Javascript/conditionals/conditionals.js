11a443df67ebb48f16533c88ae882b762561ce8b
Otruke <otruke1@gmail.com>
2022-08-16 11:35:21 +0100

    conditionals

---
+++ submissions/rufusOtolorin/Javascript/conditionals/conditionals.js
@@ -0,0 +1,17 @@
+var numb = 123 - 7
+
+for (var i = 1; i <= numb; i++){
+  if (((i % 3) === 0 ) && ((i % 5) === 0)){
+    document.write("Software Developer" + "<br>");
+  }
+  else if ((i % 5) === 0 ){
+    
+    document.write("Developer" + "<br>");
+  }
+  else if ((i % 3) ===0){
+    document.write("Software" + "<br>");
+  }
+  else {
+    document.write(i + "<br>");
+  }
+}
