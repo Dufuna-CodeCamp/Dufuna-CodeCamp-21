1ee3b37d2f1d95209079b5daa5b6f90f776d4e55
Irene Nakiyingi <iryntracy@gmail.com>
2022-08-16 12:59:30 +0300

    Php unit converter

---
+++ submissions/ireneNakiyingi/php/converter.php
@@ -0,0 +1,13 @@
+<?php
+function kgtolbs($parameter){
+    echo " $parameter Kilo to pound is: " . $parameter * 2.205 . " pounds. <br>";
+}
+function mtoyards($parameter){
+    echo "$parameter Meter to yard is: " . $parameter * 1.094 . " yards. <br>";
+}
+function CtoF($parameter){
+    echo "$parameter Celsuis to fahrenheit is: " . (1.8* $parameter+32)= . "F.<br>";
+}
+kgtolbs(46);
+mtoyards(97);
+CtoF(37);
