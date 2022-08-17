a64c728fed418aa5547be7584cb92e988950f483
Irene Nakiyingi <iryntracy@gmail.com>
2022-08-17 15:21:59 +0300

    Fixing error

--- submissions/ireneNakiyingi/php/converter.php
+++ submissions/ireneNakiyingi/php/converter.php
@@ -1,12 +1,13 @@
 <?php
 function kgtolbs($parameter){
-    echo " $parameter Kilo to pound is: " . $parameter * 2.205 . " pounds. <br>";
+    echo " $parameter Kilo to pound is = " . $parameter * 2.205 . " pounds. <br>";
 }
 function mtoyards($parameter){
-    echo "$parameter Meter to yard is: " . $parameter * 1.094 . " yards. <br>";
+    echo "$parameter Meter to yard is = " . $parameter * 1.094 . " yards. <br>";
 }
 function CtoF($parameter){
-    echo "$parameter Celsuis to fahrenheit is: " . (1.8* $parameter+32)= . "F.<br>";
+    $fah = (1.8* $parameter+32);
+    echo "$parameter Celsuis to fahrenheit is  = $fah F.<br>";
 }
 kgtolbs(46);
 mtoyards(97);
