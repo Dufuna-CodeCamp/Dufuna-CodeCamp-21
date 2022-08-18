133760254d305dad8fe10b2a05697fd86a909808
isaiah usiholo <evansaizik@gmail.com>
2022-08-18 16:55:44 +0100

    correction to transportation DB

--- submissions/isaiahUsiholo/database/transportation_company.sql
+++ submissions/isaiahUsiholo/database/transportation_company.sql
@@ -5,39 +5,32 @@ CREATE TABLE registered_passengers(
 	id INT NOT NULL AUTO_INCREMENT,
     full_name VARCHAR(255) NOT NULL,
     sex ENUM ('male', 'female') NOT NULL,
-    age INT NOT NULL,
+    age INT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE trips(
 	id INT NOT NULL AUTO_INCREMENT,
-    passenger_class ENUM ('1', '2', '3'),
-    passenger_id INT NOT NULL,
+    passenger_class ENUM ('1', '2', '3') NOT NULL,
+    registered_passengers_id INT NOT NULL,
     ticket_number VARCHAR(255) NOT NULL,
     trip_fare FLOAT (10, 2) NOT NULL,
-    assigned_cabin VARCHAR(255) NOT NULL,
-    number_of_parents_or_children INT NOT NULL,
-    number_of_siblings_or_spouse INT NOT NULL,
+    assigned_cabin VARCHAR(50) NULL,
+    number_of_parents_or_children INT NULL,
+    number_of_siblings_or_spouse INT NULL,
     point_of_embarkment VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (passenger_id) REFERENCES registered_passengers (id)
+    FOREIGN KEY (registered_passengers_id) REFERENCES registered_passengers (id)
 );
-ALTER TABLE trips MODIFY passenger_class ENUM ('1', '2', '3') NOT NULL;
+
 
 CREATE TABLE accidents(
 	id INT NOT NULL AUTO_INCREMENT,
-    affected_passengers INT NOT NULL,
-    passenger_status ENUM ('survived', 'deceased'),
+    registered_passengers_id INT NOT NULL,
+    passenger_status ENUM ('survived', 'deceased') NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (affected_passengers) REFERENCES registered_passengers (id)
+    FOREIGN KEY (registered_passengers_id) REFERENCES registered_passengers (id)
 );
-ALTER TABLE accidents MODIFY passenger_status ENUM ('survived', 'deceased') NOT NULL;
-
-SHOW COLUMNS FROM accidents;
 
 
-
-
-
-
-
+SHOW COLUMNS FROM registered_passengers;
