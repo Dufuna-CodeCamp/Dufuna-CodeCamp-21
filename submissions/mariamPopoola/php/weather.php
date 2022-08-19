75733fd669a8efe08047cd457cbc46b431bc8419
Popoola Mariam <popoolamariam05@gmail.com>
2022-08-18 22:36:12 -1000

    weather corrected

--- submissions/mariamPopoola/php/weather.php
+++ submissions/mariamPopoola/php/weather.php
@@ -1,5 +1,4 @@
-
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,25 +7,11 @@
     <title>Document</title>
 </head>
 <body>
-    <form>
-        <input>
+    <form action="weather1.php" method="post">
+      <label for="Temperature">what is the Temperature of the day?</label>
+      <input type="text" name="weather">
+
+      <input type="submit">
     </form>
 </body>
-</html> -->
-
-<?php
-# to check the temperature of the day
-$temperature = 29;
-
-if($temperature <= 20){
-    echo"It is freezing today";
-
-}elseif($temperature > 20 && $temperature < 30){
-    echo"The weather is just perfect";
-
-}elseif($temperature >= 30 && $temperature < 40){
-    echo"It is hot today";
-
-}elseif ($temperature <= 40){
-    echo"Am I in sahar Desert?";
-}
+</html>
---
+++ submissions/mariamPopoola/php/weather1.php
@@ -0,0 +1,17 @@
+<?php
+$temperature = $_POST["weather"];
+
+if($temperature <= 20){
+    echo"It is freezing today";
+  
+  }elseif($temperature > 20 && $temperature < 30){
+    echo"The weather is just perfect";
+  
+  }elseif($temperature >= 30 && $temperature < 40){
+    echo"It is hot today";
+  
+  }elseif ($temperature <= 40){
+    echo"Am I in sahar Desert?";
+  }
+  
+  
