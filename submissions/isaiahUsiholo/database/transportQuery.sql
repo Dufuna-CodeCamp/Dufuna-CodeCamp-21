bc77c56009ddd0c303c2881ac2dd0d58e08b7c13
isaiah usiholo <evansaizik@gmail.com>
2022-08-24 11:06:48 +0100

    corrections on transportation query

--- submissions/isaiahUsiholo/database/transportQuery.sql
+++ submissions/isaiahUsiholo/database/transportQuery.sql
@@ -1,18 +1,16 @@
-select * from accidents;
-delete from accidents;
-alter table registered_passengers modify age varchar(50);
+ALTER TABLE accidents MODIFY passenger_status BOOLEAN;
 SET SQL_SAFE_UPDATES = 0;
 
 SHOW TABLES;
-SELECT * FROM accidents;
+SELECT * FROM registered_passengers;
 
 -- the total number of people who survived = 342;
 SELECT passenger_status, COUNT(passenger_status) FROM accidents
-WHERE passenger_status = 'survived';
+WHERE passenger_status = '1';
 
 -- the total number of people who did not survive = 549;
 SELECT passenger_status, COUNT(passenger_status) FROM accidents
-WHERE passenger_status = '';
+WHERE passenger_status = '0';
 
 
 -- the name and sex of passengers under age 27 that embarked at Queenstown and Cherbourg;
@@ -24,14 +22,14 @@ AND point_of_embarkment IN ('Q', 'C');
 -- how many passengers that embarked at southhampton survived (total = 218);
 SELECT point_of_embarkment, COUNT(point_of_embarkment) FROM trips 
 INNER JOIN accidents ON trips.registered_passengers_id = accidents.registered_passengers_id
-WHERE point_of_embarkment = 'S' AND accidents.passenger_status = 'survived';
+WHERE point_of_embarkment = 'S' AND accidents.passenger_status = '1';
 
 -- get the id, name and the total number of passengers
 -- who paid a fare greater than $100
 -- above age 35, had siblings or spouse aboard;
--- total number = 7;
-SELECT registered_passengers.id, full_name, COUNT(registered_passengers.id) FROM registered_passengers
+-- total number = 13;
+SELECT registered_passengers.id, registered_passengers.age, registered_passengers.full_name, COUNT(registered_passengers.id) FROM registered_passengers
 JOIN trips ON registered_passengers.id = trips.registered_passengers_id 
-WHERE trip_fare > 100 AND registered_passengers.age > 35 
-AND number_of_parents_or_children > 0 AND number_of_siblings_or_spouse > 0
+WHERE (trip_fare > '100' AND age > '35')
+AND number_of_siblings_or_spouse != '0'
 GROUP BY registered_passengers.id;
