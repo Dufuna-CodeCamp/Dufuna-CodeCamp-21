7ce2f398374c6da292c603d946fd4a14f28d0986
Taiwo Tomiwa <taiwotomiwa18@gmail.com>
2022-08-24 17:20:26 +0100

    tested js file

--- submissions/tomiwaTaiwo/JavaScript/productprice.js
+++ submissions/tomiwaTaiwo/JavaScript/productprice.js
@@ -3,25 +3,25 @@
         {
             productName: "Iphone 11",
             imageUrl: "https://picsum.photos/200",
-            productPrice: "£200"
+            productPrice: "200",
         },
     
         {
-            productName: "Iphone 12",
+            productName: "Samsung A30",
             imageUrl: "https://picsum.photos/300",
-            productPrice: "£300"
+            productPrice: "300",
         },
     
         {
-            productName: "Iphone 13",
+            productName: "Tecno Spark 4",
             imageUrl: "https://picsum.photos/400",
-            productPrice: "£400"
+            productPrice: "400",
         }
-    ];
+    ]
     //i looped the array here
     for (var i = 0; i < productList.length; i++) {
         var product = productList[i]; //the product is a variable name is used for the array product list and . was used to target specific items
-        document.write( product.productName + " with " + product.imageUrl + " is sold for " + product.productPrice + "</br>");
+        document.write( product.productName + " with " + product.imageUrl + " is sold for " + "N" + product.productPrice + "</br>");
         //i added the /br so each item will be displayed on a differnt line
      }
     
