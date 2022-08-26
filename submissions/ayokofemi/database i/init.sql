da35caa1fdd10875167949d94aaf3983dd8c5c7c
Ayoko Oluwafemi <ayokooluwafemifriday@gmail.com>
2022-08-26 15:20:28 +0100

    i have made changes

--- submissions/ayokofemi/database i/init.sql
+++ submissions/ayokofemi/database i/init.sql
@@ -1,7 +1,7 @@
 --for the foodTable--
 CREATE TABLE food (
 id INT NOT NULL AUTO_INCREMENT,
-type VARCHAR(20) NOT NULL,
+type ENUM ('fast_food','vegetable_fruits','drinks_cocktails','resturants') NOT NULL DEFAULT 'fast_food',
 PRIMARY KEY (id)
 );
 
@@ -21,7 +21,7 @@ FOREIGN KEY (admin_id) REFERENCES admin (id)
 CREATE TABLE admins (
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(250) NOT NULL,
-type  ENUM("bank_transfer","Card","payment_on_arrival","dollar") NOT NULL,
+type  ENUM('0','1') NOT NULL DEFAULT '0',
 email_address VARCHAR(120) NOT NULL,
 phone_number INT(18) NOT NULL,
 password VARCHAR(16) NOT NULL,
@@ -34,7 +34,7 @@ id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(100) NOT NULL,
 phone_number VARCHAR(18) NOT NULL,
 address VARCHAR(220) NOT NULL,
-payment_method VARCHAR(25) NULL,
+payment_method  ENUM('card', 'cash', 'transfer') NULL DEFAULT 'card',
 PRIMARY KEY (id)
 );
 
@@ -42,7 +42,7 @@ PRIMARY KEY (id)
 CREATE TABLE orders (
 id INT NOT NULL AUTO_INCREMENT,
 vendor_id INT NOT NULL,
-customer_id INT NOT NULL,
+customer_id INT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (vendor_id) REFERENCES vendor_food (id),
 FOREIGN KEY (customer_id) REFERENCES customer (id)
