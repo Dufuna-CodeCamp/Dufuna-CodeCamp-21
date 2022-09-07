a2f5c4d08c3f89edbb46d056157635e89fe580ab
blessingOluwapelumi <blessingoluwapelumi28@gmail.com>
2022-09-07 23:15:32 +0100

    i added landing page.js and also updated the landing page html and css files. i also updated the images in the assets folder so it can display on the landing page

---
+++ submissions/blessingOluwapelumi/DOM/landingPage.js
@@ -0,0 +1,33 @@
+const currentDeliveryLocation = document.querySelector(".location");
+const foodImages = document.querySelectorAll(".interest-images");
+const interest = document.querySelectorAll(".interest-container");
+
+// console.log(currentDeliveryLocation, foodImages, interest);
+// span = document.querySelector(".span");
+
+const domChanges = function () {
+  for (let i = 0; i < foodImages.length, i < interest.length; i++) {
+    let imageSource = foodImages[i].attributes.src;
+    let sourceValue = imageSource.value;
+    let mainImgParent = interest[i];
+
+    if (currentDeliveryLocation.value) {
+      imageSource.textContent = sourceValue.replace("_disabled", "");
+      mainImgParent.children[1].style.color = "#AD4C4C";
+      mainImgParent.children[2].style.color = "#696969";
+      mainImgParent.style.cursor = "pointer";
+      // mainImgParent.onclick = () => (location.href = "vendor.html");
+      // span.textDetails = "";
+    } else {
+      imageSource.textContent = sourceValue
+        .replace(".svg", "")
+        .concat(`_disabled.svg`);
+      mainImgParent.children[1].style.color = "#A5A5A5";
+      mainImgParent.children[2].style.color = "#A5A5A5";
+      mainImgParent.style.cursor = "auto";
+      mainImgParent.onclick = () => (location.href = "");
+    }
+  }
+};
+
+currentDeliveryLocation.addEventListener("change", domChanges);
