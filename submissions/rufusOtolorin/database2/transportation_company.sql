65eac5eafd720e81b622ce29c41910c09d7fce9d
Otruke <otruke1@gmail.com>
2022-09-12 22:29:01 +0100

    database2 updated

--- submissions/rufusOtolorin/database2/transportation_company.sql
+++ submissions/rufusOtolorin/database2/transportation_company.sql
@@ -3,25 +3,25 @@ CREATE DATABASE transportation_company;
 
  -- TO CONFIRM THAT THE TRANSPORTATION COMPANY DATABASE DONE
  SHOW DATABASES;
- 
+
  -- TO START CREATING TABLES
  USE transportation_company;
  
   -- TO CREATE PASSENGERS TABLE
   CREATE TABLE passengers (
   id INT NOT NULL AUTO_INCREMENT,
-  full_name VARCHAR(150) NOT NULL,
+  full_name VARCHAR(250) NOT NULL,
   sex VARCHAR(10) NOT NULL,
-  age VARCHAR(100),
+  age VARCHAR(10) NULL,
   PRIMARY KEY (id)
   );
   
    -- TO CREATE TRAVEL DETAILS TABLE
-   CREATE TABLE passengers_details (
+   CREATE TABLE travel_details (
    id INT NOT NULL AUTO_INCREMENT,
    passengers_id INT NOT NULL,
-   class INT NOT NULL,
-   ticket_number VARCHAR(50) NOT NULL,
+   class ENUM('1', '2', '3') NULL DEFAULT '1',
+   ticket_number VARCHAR(250) NOT NULL,
    trip_fare DECIMAL(10, 2) NOT NULL,
    assigned_cabin VARCHAR(255),
    parent_children_aboard INT NOT NULL,
@@ -35,10 +35,11 @@ CREATE DATABASE transportation_company;
     CREATE TABLE accident_cases (
      id INT NOT NULL AUTO_INCREMENT,
      passengers_id INT NOT NULL,
-     survivals INT NOT NULL,
+     survivals BOOLEAN NOT NULL,
      PRIMARY KEY (id),
      FOREIGN KEY (passengers_id) REFERENCES passengers (id)
     );
-    
+
      -- TO SHOW ALL TABLE INI THE TRANSPORTATION COMPANY DATABASE
     SHOW tables
+    
