98dd30fea8962988a909ad5e15a2e76e4fc5a8ea
Irene Nakiyingi <iryntracy@gmail.com>
2022-09-19 20:23:48 +0300

    Customer list view

--- submissions/ireneNakiyingi/php/connection.php
+++ submissions/ireneNakiyingi/php/connection.php
@@ -2,13 +2,15 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "mysql";
+$password = "Treasure12";
 $db = "food_bag";
 try{
     $connect = new PDO ( "mysql:$servername ;$db" ,
      $username,$password);
     $connect->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $connect->exec("use food_bag");
+    echo "Success.";
 } catch(PDOException $e){
     die ("ERROR: Could not connect. " . $e->getMessage());
 };
+?>
--- submissions/ireneNakiyingi/php/customer_list.php
+++ submissions/ireneNakiyingi/php/customer_list.php
@@ -2,22 +2,46 @@
 require_once("connection.php");
 
 try{
-
-
-    $customers = " ";
-    $customer_data = array();
-    $sql = "use food_bag; 
-    SELECT SN, Full_Name, Email_Address, Created_At, Actions FROM customer ";
+    $sql = "SELECT SN, Full_Name, Email_Address, Created_At FROM customer ";
     $pdoresult = $connect->query($sql);
+
+    
+    $customer_data = array();
+    
     if (isset($_COOKIE["CUSTOMERS"])){
-        $customer_data= $_COOKIE["CUSTOMERS"];
+        $customer_data= json_decode ($_COOKIE["CUSTOMERS"], true);
 
     } else {
+       
         if ($pdoresult -> rowCount()>0){
             $customer_data= $pdoresult->fetchAll();
-            setcookie("CUSTOMERS", time()+ 8600, "/", "", false);
+            setcookie("CUSTOMERS", json_encode($customer_data), time()+ 8600, "/", "", false);
+            
         }
     };
+    /*
+    if (is_array($customer_data) && count ($customer_data)>0){
+        echo " <h2> Customer Details </h2>";
+            echo '<table>';
+                echo '<tr>';
+                    echo '<th>S/N</th>';
+                    echo '<th>Full Name</th>';
+                    echo '<th>Email Address</th>';
+                    echo '<th>Created At</th>';
+                    echo '<th>Action</th>';
+                echo '</tr>';
+        foreach($customer_data as $person ){
+            echo "<tr>";
+            echo "<td>" . $person['SN'] . "</td>";
+            echo "<td>" . $person['Full_Name'] . "</td>";
+            echo  "<td>" . $person['Email_Address'] . "</td>";
+            echo"<td>" . $person['Created_At'] . "</td>";
+            echo "<td> <button>View </button> </td>";
+            echo "</tr>";
+        }
+    } else{
+        echo "No content found";
+    }*/
     if (!empty($pdoresult)){
         echo " <h2> Customer Details </h2>";
             echo '<table>';
@@ -34,8 +58,7 @@ try{
                     echo "<td>" . $rows['Full_Name'] . "</td>";
                     echo "<td>" . $rows['Email_Address'] . "</td>";
                     echo "<td>" . $rows['Created_At'] . "</td>";
-                    echo "<td> <button><a href='orders.php'>View </a></button></td>";
-                    echo "<td>" . $rows['Actions'] . "<td/>";
+                    echo "<td> <button>View </button></td>";
                 echo '</tr>';
             }
             echo '</table>';
