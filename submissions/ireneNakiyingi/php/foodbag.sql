92a80b12ea876dbcef1e647a403efacd526f5dae
Irene Nakiyingi <iryntracy@gmail.com>
2022-09-22 12:35:04 +0300

    sql edit

--- submissions/ireneNakiyingi/php/foodbag.sql
+++ submissions/ireneNakiyingi/php/foodbag.sql
@@ -1,20 +1,23 @@
 USE food_bag;
 -- customers table
-CREATE TABLE customer (
+CREATE TABLE customers (
   SN INT NOT NULL AUTO_INCREMENT,
   Full_Name Varchar(45), 
   Email_Address Varchar(40),
   Created_At datetime ,
   PRIMARY KEY (SN)
 ); 
-INSERT INTO customer 
+
+
+INSERT INTO customers 
 ( Full_Name, Email_Address, Created_At) VALUES 
 ('Alex crew', 'a@gmail.com', now()),
 ('parker tee', 'p@gmail,com',now())
 ;
-INSERT INTO customer 
+select * from customers;
+INSERT INTO customers 
 ( Full_Name, Email_Address, Created_At) VALUES 
 ('Betty White', 'bw@gmail.com', now()),
 ('Wanda hunt', 'huntw@gmail,com',now())
 ;
-select * from customer;
+select * from customers;
