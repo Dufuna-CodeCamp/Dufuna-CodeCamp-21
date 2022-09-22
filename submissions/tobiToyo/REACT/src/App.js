2f397b949c292f4fc113883008c5955fcbd551fb
Tobi Toyo <toyocodes@gmail.com>
2022-09-22 02:37:22 -0700

    Successful Order Page done

--- submissions/tobiToyo/REACT/src/App.js
+++ submissions/tobiToyo/REACT/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CartPage from './components/CartPage';
-import Verification from './components/VerificationPage';
+// import Verification from './components/VerificationPage';
+import Verification from './components/VerificationPage(FunctionalComponent)';
+import SuccessfulOrder from './components/SuccessfulOrder';
 
 
 
@@ -12,9 +14,10 @@ function App() {
     <div className="App">
       
      <BrowserRouter>
-      <Routes>
+      <Routes >
         <Route path="/" element={<CartPage />} />
         <Route path='/Verification' element={<Verification />}/>
+        <Route path='/OrderSuccess' element={<SuccessfulOrder />}  />
       </Routes>
      </BrowserRouter>
      
