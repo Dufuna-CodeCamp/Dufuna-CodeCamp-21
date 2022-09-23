1e438714766e4d63963902cf4813b2cfa67f0f77
bunmi <bunmidavids194@gmail.com>
2022-09-23 17:42:27 +0100

    Fixed issues with E-commerce Database

--- submissions/bunmiOladipupo/database3/init.sql
+++ submissions/bunmiOladipupo/database3/init.sql
@@ -4,15 +4,15 @@ USE foodbag;
 -- table for foods is created below
 CREATE TABLE foods(
 	food_id INT NOT NULL AUTO_INCREMENT,
-    food_type ENUM('fast food','vegetables','fruits','drinks and cocktails','restaurants') NOT NULL,
+    food_type ENUM('fast_food','vegetable_fruits','drinks_cocktails','restaurants') NOT NULL,
     PRIMARY KEY(food_id)
 );
 -- admin table below
 CREATE TABLE admins(
 	admin_id INT NOT NULL AUTO_INCREMENT,
-    admin_name VARCHAR(100) NOT NULL,
+    admin_name VARCHAR(250) NOT NULL,
     admin_type ENUM('0','1') NOT NULL,
-    email_address VARCHAR(100) NOT NULL,
+    email_address VARCHAR(250) NOT NULL,
     phone_number VARCHAR(50) NOT NULL,
     admin_password VARCHAR(255) NOT NULL,
     PRIMARY KEY(admin_id)
@@ -22,7 +22,7 @@ CREATE TABLE vendor_foods(
 	vendor_id INT NOT NULL AUTO_INCREMENT,
     admin_id INT NOT NULL,
     food_id INT NOT NULL,
-    vendor_name VARCHAR(100) NOT NULL,
+    vendor_name VARCHAR(250) NOT NULL,
     amount DECIMAL(10,2) NOT NULL,
     PRIMARY KEY(vendor_id),
     FOREIGN KEY(food_id) REFERENCES foods(food_id),
@@ -31,12 +31,12 @@ CREATE TABLE vendor_foods(
 -- customer table below
 CREATE TABLE customers(
 	customer_id INT NOT NULL AUTO_INCREMENT,
-    customer_name VARCHAR(100) NOT NULL,
+    customer_name VARCHAR(250) NOT NULL,
     customer_phone VARCHAR(50) NOT NULL,
-    customer_address VARCHAR(100) NOT NULL,
+    customer_address VARCHAR(250) NOT NULL,
     payment_method ENUM('CARD','USSD') NULL,
     card_number VARCHAR(50) NULL,
-    card_name VARCHAR(100) NULL,
+    card_name VARCHAR(250) NULL,
     expiry_date DATE NULL,
     cvv TINYINT NULL,
     PRIMARY KEY(customer_id)
