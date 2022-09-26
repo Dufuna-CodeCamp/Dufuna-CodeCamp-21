4bcf75106bed2d2a390c9aff11cd24626b62cbf0
Tobi Toyo <toyocodes@gmail.com>
2022-09-26 07:14:58 -0700

    running tests

--- submissions/tobiToyo/PHP/index.php
+++ submissions/tobiToyo/PHP/index.php
@@ -1,21 +1,21 @@
 <?php 
-echo "Unit Converter <br>";
+echo "Unit Converter <br><br>";
 
-echo "Kilos to Pounds function <br>";
+echo "Kilos to Pounds function <br><br>";
 
 function kiloToPounds ($kilo) {
   $pound = $kilo * 2.20462262185;
-  echo "$kilo kg = $pound lbs <br>";
+  echo "$kilo kg = $pound lbs <br><br><br>";
 }
 
 kiloToPounds(30);
 
 
-echo "Meters to Yards <br>";
+echo "Meters to Yards <br><br>";
 
 function metersToYards ($meter) {
-  $yard = $meter * 1.093613;
-  echo "$meter m = $yard yards <br>"  ;
+  $yard = $meter * 1.0936132983;
+  echo "$meter m = $yard yards <br><br><br>"  ;
 }
 
 metersToYards(5);
@@ -29,3 +29,17 @@ function celciusToF ($celc) {
 celciusToF(100);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Module</title>
+</head>
+<body>
+  <br>
+   <a href="weather.php">Check The Weather Here </a>
+</body>
+</html>
