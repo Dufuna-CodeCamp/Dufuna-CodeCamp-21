a18d8e9516d31160f76fc908821ec2bea275cb13
Precious Aienobe <aienobeprecious.gmail.com>
2022-09-28 20:14:33 +0100

    landing page validation update

--- submissions/preciousAienobe/DOM/landing_page.js
+++ submissions/preciousAienobe/DOM/landing_page.js
@@ -1,13 +1,26 @@
-let fastFood = document.getElementById("fast-food");
-let vegetable = document.getElementById("vegetables");
-let cocktails = document.getElementById("cocktails");
-let resturant = document.getElementById("resturant");
+let interests = document.querySelectorAll(".interests")
+let images = document.querySelectorAll(".img");
+
+let imageURL = [
+  "../images/fast-food.svg",
+  "../images//Vegetables & Drinks.svg",
+  "../images/Drinks & Cocktails.svg",
+  "../images/resturant.svg",
+];
 
 let dropdown = document.getElementById("dropdown");
 
 dropdown.addEventListener("change", function () {
-  fastFood.src = "../images/fast-food.svg";
-  vegetable.src = "../images//Vegetables & Drinks.svg";
-  cocktails.src = "../images/Drinks & Cocktails.svg";
-  resturant.src = "../images/resturant.svg";
+  interests.forEach(interests => {
+    interests.style.cursor = "pointer";
+    interests.addEventListener("click", () => {
+      window.location.href = "../html/vendor_page.html";
+    })
+  });
+
+  for (i = 0; i < imageURL.length; i++) {
+    images[i].src = imageURL[i];
+  }
 });
+
+
