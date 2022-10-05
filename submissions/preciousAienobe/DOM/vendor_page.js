7b8946a82561cec8841a5cc7283050abbd95e8e6
Precious Aienobe <aienobeprecious.gmail.com>
2022-10-05 13:48:35 +0100

    vendor page validation update

--- submissions/preciousAienobe/DOM/vendor_page.js
+++ submissions/preciousAienobe/DOM/vendor_page.js
@@ -1,5 +1,4 @@
 let addBtn = document.querySelectorAll(".add-btn");
-let removeBtn = document.querySelectorAll(".remove-btn");
 let dot = document.querySelector(".dot");
 
 let arr = [];
@@ -8,16 +7,15 @@ addBtn.forEach((item) => {
     item.addEventListener("click", () => {
         if (item.classList.contains("add-btn")) {
             item.classList.replace("add-btn", "remove-btn");
-            item.innerText = "Remove";
+            item.textContent = "Remove";
             item.style.color = "rgb(255, 255, 255)";
             arr.push("item-added");
         }
         else {
             item.classList.replace("remove-btn", "add-btn");
-            item.innerText = "Add to Cart";
+            item.textContent = "Add to Cart";
             item.style.color = "#C17979"
             arr.pop();
-            console.log(arr);
         }
 
         if (arr.length === 0) {
