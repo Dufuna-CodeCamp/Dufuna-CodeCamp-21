d4623c0df10d65f9f9829660cc11324cde15566a
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-10-30 08:22:59 +0100

    verification page

--- submissions/mauriceOkeke/REACT/react-app/src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "../App";
-
-const thisComponent = shallow(<App />);
-
-it("renders without crashing", () => {
-  thisComponent;
-});
