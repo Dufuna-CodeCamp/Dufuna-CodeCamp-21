ec7e55677a00e4af964dfaa44fd168b99ed238e4
bunmi <bunmidavids194@gmail.com>
2022-11-11 05:08:48 +0100

    My REACT APP

---
+++ submissions/bunmiOladipupo/react/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Header from './components/CartItem.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// const headerValues = ReactDOM.createRoot(document.getElementById('root'));
+// headerValues.render(<Header />);
+
