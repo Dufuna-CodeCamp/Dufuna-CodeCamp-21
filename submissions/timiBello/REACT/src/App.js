846888b023f146209b058ccd414bfa1c74149df7
Timilehin08 <timilehindebra@gmail.com>
2022-11-17 18:30:06 +0100

    created verification  page

--- submissions/timiBello/REACT/src/App.js
+++ submissions/timiBello/REACT/src/App.js
@@ -1,6 +1,7 @@
 import CartPage from './components/cartPage';
 import '../src/App.css';
 import Nav from './components/Nav';
+import Verification from './components/Verification';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
 
         <Routes>
           <Route path='/' element={<CartPage />} />
+          <Route path='/verification' element={<Verification />} />
         </Routes>
       </div>
     </BrowserRouter>
