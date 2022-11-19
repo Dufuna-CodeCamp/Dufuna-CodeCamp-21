3aa21184c997844665e2b5b1921b8fd0c82fc404
bunmi <bunmidavids194@gmail.com>
2022-11-19 10:55:09 +0100

    success page

--- submissions/bunmiOladipupo/react-app/src/App.js
+++ submissions/bunmiOladipupo/react-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import Verification from './components/Verification.js';
 import CartPage from './components/CartPage.js';
+import Success from './components/Success.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/Verification" exact element={<Verification />}></Route>
         <Route path="/" exact element={<CartPage /> }></Route>
+        <Route path="/Success" exact element={<Success />}></Route>
       </Routes>
     </div>
   );
---
+++ submissions/bunmiOladipupo/react-app/src/components/Success.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Component } from "react";
+import Header from "./Header.js";
+import Illustration_success from "../Illustration_success.svg";
+
+class Success extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <>
+                <Header />
+                <div className="row">
+                    <div className="col-12" id="ordersuccess">                              
+                        <img src={Illustration_success} alt="delivery success"/>
+                        <h2>Order Successful</h2>
+                        <span id="span">We have started processing your order.</span>
+                    </div>
+                    <div className="col-12 style">
+
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Success;
