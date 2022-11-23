55d6269a2a61c24e09bb5901746e551c8cb7a455
Mwaba Chabo <mchabo616@gmail.com>
2022-11-23 17:27:28 +0200

    made required changes

--- submissions/mwabaChabo/Databases/init2.sql
+++ submissions/mwabaChabo/Databases/init2.sql
@@ -3,20 +3,20 @@ CREATE DATABASE food_bag;
 /*Highlighting the database to be used*/
 USE food_bag;
 /*creating a food table*/
-CREATE TABLE food (
+CREATE TABLE foods (
 id INT NOT NULL AUTO_INCREMENT,
-type ENUM('FastFoods','Vegetables&Fruits','Drinks&Cocktails','Restaurants'),
+type ENUM('FastFoods','Vegetables_Fruits','Drinks_Cocktails','Restaurants'),
 PRIMARY KEY(id)
 );
 /*creating a Vendor_food table*/
-CREATE TABLE vendor_food (
+CREATE TABLE vendor_foods (
 id INT NOT NULL AUTO_INCREMENT,
 admin_id INT NOT NULL,
 food_id INT NOT NULL,
 name VARCHAR(250) NOT NULL,
-amount DECIMAL(6,2) NOT NULL,
+amount DECIMAL(10,2) NOT NULL,
 PRIMARY KEY(id),
-FOREIGN KEY(admin_id) REFERENCES admin(id)
+FOREIGN KEY(food_id) REFERENCES food(id)
 );
 /*creating an admin table*/
 CREATE TABLE admin(
@@ -33,6 +33,17 @@ CREATE TABLE orders(
 id INT NOT NULL AUTO_INCREMENT,
 vendor_id INT NOT NULL,
 customer_id INT NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(vendor_id) REFERENCES vendors(id),
+FOREIGN KEY(customer_id) REFERENCES customers(id)
+);
+/*creating a customers table*/
+CREATE TABLE customers(
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(250) NOT NULL,
+phone_number VARCHAR(250) NOT NULL,
+address VARCHAR(250) NOT NULL,
+payment_method VARCHAR(250) NOT NULL,
 PRIMARY KEY(id)
 );
 
