8fe2a678389e00480f234ea6eff3aaf32e260d2a
Mwaba Chabo <mchabo616@gmail.com>
2022-11-23 09:22:33 +0200

    made a few changes to my database

--- submissions/mwabaChabo/Databases/init.sql
+++ submissions/mwabaChabo/Databases/init.sql
@@ -4,17 +4,17 @@ CREATE TABLE registered_passengers (
 PassengerId INT NOT NULL AUTO_INCREMENT,
 FullName VARCHAR(100) NOT NULL,
 Sex VARCHAR(10) NOT NULL,
-Age VARCHAR(40),
+Age VARCHAR(10) NULL,
 PRIMARY KEY(PassengerId)
 );
 
 CREATE TABLE travel_details (
 Id INT NOT NULL AUTO_INCREMENT,
 PassengerId INT NOT NULL,
-PassengerClasses INT  NOT NULL,
-PassengerTicketNumbers VARCHAR(100) NOT NULL,
+PassengerClasses ENUM('1','2','3') NULL DEFAULT'1',
+PassengerTicketNumbers VARCHAR(250) NOT NULL,
 TripFares DECIMAL(8.2) NOT NULL,
-AssignedCabins VARCHAR(100),
+AssignedCabins VARCHAR(255),
 ParentChildrenAboard INT NOT NULL,
 SiblingsSpousesAboard INT NOT NULL,
 Embarkations VARCHAR(100) NOT NULL,
@@ -25,10 +25,7 @@ FOREIGN KEY(PassengerId) REFERENCES registered_passengers(PassengerId)
 CREATE TABLE survivors(
 Id INT NOT NULL AUTO_INCREMENT,
 PassengerId INT NOT NULL,
-SurvivedPassengers BOOLEAN  NOT NULL,
+SurvivedPassengers BOOLEAN NOT NULL,
 PRIMARY KEY(Id),
 FOREIGN KEY(PassengerId) REFERENCES registered_passengers(PassengerId)
 );
-
-
-
