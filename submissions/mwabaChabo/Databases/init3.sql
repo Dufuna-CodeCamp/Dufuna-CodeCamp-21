e9faeb4c5f060fcd2298bf6364b1eb01be792492
Mwaba Chabo <mchabo616@gmail.com>
2022-11-24 15:52:58 +0200

    made changes

--- submissions/mwabaChabo/Databases/init3.sql
+++ submissions/mwabaChabo/Databases/init3.sql
@@ -1,4 +1,4 @@
-SELECT * FROM survivors WHERE survivedPassengers ='1';
+SELECT * FROM survivors WHERE SurvivedPassengers ='1';
 /*total number ofpassengers who survived are
  342*/
 SELECT * FROM survivors WHERE survivedPassengers ='0';
@@ -10,5 +10,6 @@ SELECT registered_passengers.FullName, registered_passengers.Sex FROM registered
 SELECT * FROM survivors LEFT JOIN travel_details ON survivors.id=travel_details.PassengerId WHERE Embarkation='S' AND SurvivedPassengers > 0;
 /*registered passengers that survived who embarked at Southampton
 218*/
-/*total number of passengers who paid a fare greater than $100 and are above the age of 35 with their siblings and spouses on board*/
-SELECT COUNT(*) FROM registered_passengers LEFT JOIN travel_details ON registered_passengers.PassengerId=travel_details.PassengerId WHERE TripFares>100 AND SiblingsSpousesAboard > 0 AND registered_passengers.age > 35;
+/*total number of passengers who paid a fare greater than $100 and are above the age of 35 with their siblings and spouses on board
+are 9 */
+SELECT FullName, Sex, Age  FROM registered_passengers LEFT JOIN travel_details ON registered_passengers.PassengerId=travel_details.PassengerId WHERE TripFares>100 AND SiblingsSpousesAboard > 0 AND registered_passengers.age > 35;
