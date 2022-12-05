81f4fdd849d36fb267c2385eb6d9822c96bc79f6
RehkMansa <godspowernathaniel25@gmail.com>
2022-12-05 22:55:36 +0100

    finished dom tasks 1

---
+++ submissions/nathanielGodspower/DOM/vendors_page_validation.js
@@ -0,0 +1,44 @@
+const locations = document.getElementById('location');
+const interestsItem = document.querySelectorAll('.interests__item');
+const addToCartButtons = document.querySelectorAll('.kfc-single button');
+const cart = document.getElementById('cart');
+
+let cartItems = 0;
+
+addToCartButtons.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		const innerText = btn.innerText;
+
+		if (innerText === 'Add to Cart') {
+			btn.innerHTML = "<img src='./img/remove.svg'/> Remove";
+			btn.classList.add('is-selected');
+			cartItems++;
+		}
+
+		if (innerText === 'Remove') {
+			btn.innerHTML = 'Add to Cart';
+			btn.classList.remove('is-selected');
+			cartItems--;
+		}
+
+		if (cartItems > 0) {
+			cart.classList.add('has-cart');
+		} else {
+			cart.classList.remove('has-cart');
+		}
+	});
+});
+
+locations.onchange = () => {
+	const location = locations.value;
+
+	if (location) {
+		interestsItem.forEach((interest) => {
+			interest.href = '../css/vendors.html';
+		});
+	} else {
+		interestsItem.forEach((interest) => {
+			interest.removeAttribute('href');
+		});
+	}
+};
