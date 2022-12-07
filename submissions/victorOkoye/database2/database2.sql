ec242c5e8d98c2266acca7559d3d1bb4731d704a
chivicoko <vicharmonic@gmail.com>
2022-12-07 16:07:30 +0000

    made corrections in lines 23,24,25,28 and 38

--- submissions/victorOkoye/database2/database2.sql
+++ submissions/victorOkoye/database2/database2.sql
@@ -20,12 +20,12 @@ CREATE TABLE passengers_details (
 	id INT NOT NULL AUTO_INCREMENT,
     passengers_id INT NOT NULL,
     passenger_class ENUM('1','2','3') NOT NULL,
-    ticket_number VARCHAR(50) NOT NULL,
-    trip_fare DECIMAL(10,5) NOT NULL,
-    cabin VARCHAR(100),
+    ticket_number VARCHAR(250) NOT NULL,
+    trip_fare DECIMAL(10,2) NOT NULL,
+    cabin VARCHAR(250),
     num_children_parents INT NOT NULL,
     num_sibilings_spouses INT NOT NULL,
-    embark_point VARCHAR(10) NOT NULL,
+    embark_point VARCHAR(100) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (passengers_id) REFERENCES passengers (id)
 );
@@ -35,7 +35,7 @@ CREATE TABLE passengers_details (
 CREATE TABLE accident_cases (
 	id INT NOT NULL AUTO_INCREMENT,
     passengers_id INT NOT NULL,
-    status INT NOT NULL,
+    status BOOLEAN NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (passengers_id) REFERENCES passengers_details (id)
 );
