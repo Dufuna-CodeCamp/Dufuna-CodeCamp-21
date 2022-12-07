9b11d8b08fdf2ba741e5e4e0faf8d32fa461cf81
bunmi <bunmidavids194@gmail.com>
2022-12-07 09:04:21 +0100

    improved converter function

--- submissions/bunmiOladipupo/php/converter.php
+++ submissions/bunmiOladipupo/php/converter.php
@@ -1,17 +1,27 @@
 <?php
-    function converter($param){
+    function converter($pound=null,$yard=null,$celsiu=null){
         //kilos to pounds- 1kg = 2.20462pounds
         //metres to yards- 1m = 1.09361yards
         //celsius to fahrenheit (^C * 9/5) + 32
-        $pounds = $param * 2.20462;
-        $yards = $param * 1.09361;
-        $celsius = ($param * 9/5) + 32;
-
-        echo "$param kilos equals $pounds pounds<br>";
-        echo "$param metres equals $yards yards<br>";
-        echo "$param celsius equals $celsius Fahrenheit<br>";
+        $pounds = $pound * 2.20462;
+        $yards = $yard * 1.09361;
+        $celsius = ($celsiu * 9/5) + 32;
+		if($pound != null){
+        	
+        echo "$pound kilos equals $pounds pounds<br>";
+        }
+        if($yard != null){
+        	
+        echo "$yard metres equals $yards yards<br>";
+        }
+        if($celsiu != null){
+        	
+        echo "$celsiu celsius equals $celsius Fahrenheit<br>";
+        }
 
     }
 
     converter(1)
+    converter(1,1)
+    converter(1,1,1)
 ?>
