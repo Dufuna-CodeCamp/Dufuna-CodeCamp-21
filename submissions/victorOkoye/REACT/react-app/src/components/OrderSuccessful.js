39fba781a5314232f274d0d828f378896c44262a
chivicoko <vicharmonic@gmail.com>
2022-12-16 15:00:57 +0000

    separate the OrderSuccessful styles in another page

--- submissions/victorOkoye/REACT/react-app/src/components/OrderSuccessful.js
+++ submissions/victorOkoye/REACT/react-app/src/components/OrderSuccessful.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import "../css/OrderSuccessful.css";
 
 const OrderSuccessful = () => {
-    const containerDiv = {height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center"};
-    const wrapperDiv =  {textAlign: "center"};
-    const firstP = {fontSize: "28px", color: "green", fontWeight: "bold"};
+    // const containerDiv = {height: "100vh", width: "100vw", display: "flex", alignItems: "center", justifyContent: "center"};
+    // const wrapperDiv =  {textAlign: "center"};
+    // const firstP = {fontSize: "28px", color: "green", fontWeight: "bold"};
   return (
-    <div style={containerDiv}>
-        <div style={wrapperDiv}>
+    <div className='container'>
+        <div className='wrapper'>
             <img src= "images/Illustration_success.svg" alt = "order-success"/>
-            <p style={firstP}>Order successful</p>
+            <p className='firstParagraph'>Order successful</p>
             <p>We have started processing your order.</p>
         </div>
     </div>
   )
 }
 
-export default OrderSuccessful
+export default OrderSuccessful;
