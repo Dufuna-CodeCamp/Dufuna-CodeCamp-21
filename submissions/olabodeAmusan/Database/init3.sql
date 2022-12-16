a37e12882ea4c36b49150017b5b99bbf4afa0484
Olabode Amusan <amusanolabode360@gmail.com>
2022-12-16 21:09:18 +0100

    Querying the Transportation Database

--- submissions/olabodeAmusan/Database/init3.sql
+++ submissions/olabodeAmusan/Database/init3.sql
@@ -19,6 +19,7 @@ INNER JOIN trips INNER JOIN accidents ON passengers.id = trips.passenger_id AND 
 /*218*/
 
 /*Get the id, name and the total number of passengers who paid a fare greater than $100 and above the age of 35 had siblings or spouses on board?*/
-SELECT passengers.id, passengers.name FROM passengers
-INNER JOIN trips ON passengers.id = trips.passenger_id WHERE trip_fare > 100 AND age > 35 AND (parch OR sibsp > 0); 
+SELECT passengers.id, passengers.name, COUNT(*) FROM passengers
+INNER JOIN trips ON passengers.id = trips.passenger_id WHERE trip_fare > 100 AND age > 35 AND (parch OR sibsp > 0) 
+GROUP BY passengers.id, passengers.name;
 /*15*/
