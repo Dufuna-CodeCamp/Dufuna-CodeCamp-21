813d870502673471876dfe8ab5291d7260aec238
MARWUMI <MARWUMI@GMAIL.COM>
2022-12-21 14:04:22 +0100

    [NDFFET-169] Create the Verification page using REACT

--- submissions/omowumiOlatinwo/React/src/Components/Checkout.js
+++ submissions/omowumiOlatinwo/React/src/Components/Checkout.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom"
+
 
 function Checkout(props) {
     return(
-        <>
-                <button type="button" className="btnCheckout" id={props.id}>{props.name}</button>
+        <div>
+              <Link to={props.link}>
+                <button type="button" className="btnCheckout" id={props.id} onclick={props.checkout}>{props.name} </button>
+              </Link>
             
-        </>
+        </div>
     )
 }
 
