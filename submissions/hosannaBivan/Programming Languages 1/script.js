0b9726c46a2399a0b5eba8d7e13c600e62730b99
Hosanna Bivan <hosabivan@gmail.com>
2023-01-05 20:08:14 +0100

    js

--- submissions/hosannaBivan/Programming Languages 1/script.js
+++ submissions/hosannaBivan/Programming Languages 1/script.js
@@ -4,25 +4,24 @@ document.open();
 for(i = 1; i <= 116; i++){
     
     if(i % 15 == 0){
-        document.write("Software Developer");
+        document.write("Software Developer \n"); 
     }
 
     // multiples of 3 prints Software
     else if(i % 3 == 0){
-        document.write("Software");
+        document.write("Software \n");
     }
 
      // multiples of 5 prints Developer
     else if(i % 5 == 0){
-        document.write("Developer");
+        document.write("Developer \n");
     }  
 
     else {
-        document.write(i);
+        document.write(i + "\n");
     }
 
 }
 
+
 document.close();
-
-
