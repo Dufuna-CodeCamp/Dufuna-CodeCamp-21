2e73936290b3f8d6561849450fbb62aef4401c4a
gokivumbi <okivumbi@sailglobalcorp.com>
2023-01-06 16:11:31 +0300

    cart

--- submissions/kivumbiGeorge/REACT/src/App.js
+++ submissions/kivumbiGeorge/REACT/src/App.js
@@ -1,28 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-// import CartPage from "./pages/cart";
+
 import CartPage from "./components/CartPage";
 
 function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>˝
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <CartPage />
-  );
+  return <CartPage />;
 }
 
 export default App;
