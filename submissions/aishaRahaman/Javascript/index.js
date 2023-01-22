f37f899012db9ca4c2bb30ea10b4c271d1ab3df5
Aisha <aishareal8@gmail.com>
2023-01-22 11:57:15 +0100

    corrections

--- submissions/aishaRahaman/Javascript/index.js
+++ submissions/aishaRahaman/Javascript/index.js
@@ -2,17 +2,19 @@ let x = 123;
 x -= 7;
 //document.write(x); 
 
-var str = "";
 for(i = 1; i <= x; i++) {
-    //Multiples of 3
-    if (i % 3 == 0 ) {
-        document.write("Software" + "<br>");
+    //Multiples of 3 and 5
+    if (i % 3 == 0 && i % 5 == 0) {
+        document.write("Software Developer" + "<br>");
+        
         //Multiples of 5
     }else if (i % 5 == 0) {
         document.write("Developer" + "<br>");
-        //Multiples of 3 and 5
-    }else if (i % 3 == 0 && i % 5 == 0) {
-        document.write("Software Developer");
+
+        //Multiples of 3
+    }else if (i % 3 == 0) {
+        document.write("Software" + "<br>");
+        
         //Multiples of none
     }else{
         document.write(i + "<br>");
