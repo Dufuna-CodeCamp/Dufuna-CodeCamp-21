7f5d2fc27bf116ba9ee42e02ff403e4e6f796625
Hosanna Bivan <hosabivan@gmail.com>
2023-02-04 02:11:19 +0100

    dom

--- submissions/hosannaBivan/Dom/script2.js
+++ submissions/hosannaBivan/Dom/script2.js
@@ -1,9 +1,35 @@
 var boxes = document.getElementsByClassName("f");
+var newBoxes = document.getElementsByClassName("g");
+
 
 for(var box of boxes){
-    box.addEventListener('click', function makeSelection(){
-        
-        this.classList.toggle("display-none")
-        
-    });
+    box.addEventListener('click', this.makeSelection);
+    
+}
+
+for(var newBox of newBoxes){
+    newBox.addEventListener('click', this.undoSelection);
+    
+}
+
+function makeSelection(){
+    for(var box of boxes){
+        box.style.display = "none";
+
+    }
+    for(var newBox of newBoxes){
+        newBox.style.display = "block"
+
+    }
+}
+
+function undoSelection(){
+    for(var newBox of newBoxes){
+        newBox.style.display = "none"
+    }
+
+    for(var box of boxes){
+        box.style.display = "block";
+    }
+    
 }
