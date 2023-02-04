e2395faf3c3f362a9ee5adfad10ba90857fffe34
gokivumbi <okivumbi@sailglobalcorp.com>
2023-02-04 18:25:39 +0300

    initial verification page commit

--- submissions/kivumbiGeorge/react/react-app/src/App.js
+++ submissions/kivumbiGeorge/react/react-app/src/App.js
@@ -1,9 +1,63 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 import "./App.css";
 import CartPage from "./components/CartPage";
+import VerificationPage from "./components/VerificationPage";
 
 function App() {
-  return <CartPage />;
+
+    // const router = createBrowserRouter([
+    //     {
+    //         path: "/",
+    //         element: <div>
+    //             <p> please navigate to <Link to="/cart">Cart</Link> for the Cart page </p>
+    //             <br/>
+    //             <p> please navigate to <Link to="/verification">Verification</Link> for the Verification page </p>
+    //         </div>,
+    //     },
+    //     {
+    //         path: "/cart",
+    //         element: <CartPage/>,
+    //     },
+    //     {
+    //         path: "/verification",
+    //         element: <VerificationPage/>
+    //     }
+    // ]);
+    return (
+        // <CartPage />
+        // <VerificationPage/>
+        // <RouterProvider router={router}/>
+        <Router>
+            <Switch>
+
+                {/*<Route path="/">*/}
+
+                    <Route path="/cart">
+                        <CartPage/>
+                    </Route>
+                    <Route path="/verification">
+                        <VerificationPage/>
+                    </Route>
+                {/*</Route>*/}
+                <Route path="/">
+                    <div>
+                        <p> please navigate to <Link to="/cart">Cart</Link> for the Cart page </p>
+                        <br/>
+                        <p> please navigate to <Link to="/verification">Verification</Link> for the Verification page
+                        </p>
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
+    );
+
 }
 
 export default App;
