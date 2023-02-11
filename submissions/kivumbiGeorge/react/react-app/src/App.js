2a5233bc6bfad2915dbe909fb916d11483655e50
gokivumbi <okivumbi@sailglobalcorp.com>
2023-02-11 15:43:55 +0300

    initial commit

--- submissions/kivumbiGeorge/react/react-app/src/App.js
+++ submissions/kivumbiGeorge/react/react-app/src/App.js
@@ -1,63 +1,52 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./App.css";
 import CartPage from "./components/CartPage";
+import OrderSuccessful from "./components/OrderSuccessful";
 import VerificationPage from "./components/VerificationPage";
 
 function App() {
+  return (
+    <Router>
+      <Switch>
+        {/*<Route path="/">*/}
 
-    // const router = createBrowserRouter([
-    //     {
-    //         path: "/",
-    //         element: <div>
-    //             <p> please navigate to <Link to="/cart">Cart</Link> for the Cart page </p>
-    //             <br/>
-    //             <p> please navigate to <Link to="/verification">Verification</Link> for the Verification page </p>
-    //         </div>,
-    //     },
-    //     {
-    //         path: "/cart",
-    //         element: <CartPage/>,
-    //     },
-    //     {
-    //         path: "/verification",
-    //         element: <VerificationPage/>
-    //     }
-    // ]);
-    return (
-        // <CartPage />
-        // <VerificationPage/>
-        // <RouterProvider router={router}/>
-        <Router>
-            <Switch>
-
-                {/*<Route path="/">*/}
-
-                    <Route path="/cart">
-                        <CartPage/>
-                    </Route>
-                    <Route path="/verification">
-                        <VerificationPage/>
-                    </Route>
-                {/*</Route>*/}
-                <Route path="/">
-                    <div>
-                        <p> please navigate to <Link to="/cart">Cart</Link> for the Cart page </p>
-                        <br/>
-                        <p> please navigate to <Link to="/verification">Verification</Link> for the Verification page
-                        </p>
-                    </div>
-                </Route>
-            </Switch>
-        </Router>
-    );
-
+        <Route path="/cart">
+          <CartPage />
+        </Route>
+        <Route path="/verification">
+          <VerificationPage />
+        </Route>
+        <Route path="/success">
+          <OrderSuccessful />
+        </Route>
+        {/*</Route>*/}
+        <Route path="/">
+          <div>
+            <p>
+              {" "}
+              please navigate to <Link to="/cart">Cart</Link> for the Cart page{" "}
+            </p>
+            <br />
+            <p>
+              {" "}
+              please navigate to <Link to="/verification">
+                Verification
+              </Link>{" "}
+              for the Verification page
+            </p>
+            <br />
+            <p>
+              {" "}
+              please navigate to <Link to="/success">Success</Link> for the
+              Success page
+            </p>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
