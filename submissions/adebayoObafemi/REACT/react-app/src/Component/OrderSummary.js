229681b83d9fc9f904dbbd43a3511830e95bdd6c
Bayo Obafemi <ebenezerbayo@gmail.com>
2023-02-21 03:50:25 +0100

    added cart page with react

---
+++ submissions/adebayoObafemi/REACT/react-app/src/Component/OrderSummary.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Navigation from "./Navigation";
+import "./ordersummary.css";
+import orderSuccessImage from "../FoodBag-Task-Assets/Illustration_success.svg";
+
+export default function OrderSummary() {}
---
+++ submissions/adebayoObafemi/REACT/react-app/src/Component/Verification.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState } from "react";
+import "./verification.css";
+import Navigation from "./Navigation";
+import radioButton from "../FoodBag-Task-Assets/radio_button_checked.svg";
+import { Link } from "react-router-dom";
+
+export default function Verification() {}
