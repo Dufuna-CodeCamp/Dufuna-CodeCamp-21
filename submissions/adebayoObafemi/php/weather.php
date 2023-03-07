c4ab223eb8cc64a41605c66bec40922274895c20
Bayo Obafemi <ebenezerbayo@gmail.com>
2023-03-07 17:46:12 +0100

    added the third task in the php module

---
+++ submissions/adebayoObafemi/php/weather.php
@@ -0,0 +1,13 @@
+<?php
+    $temperature = 27;
+
+    if($temperature <= 20) {
+        echo "It is freezing today <br>";
+    } elseif($temperature > 20 && $temperature < 30) {
+        echo "The weather is just perfect. <br>";
+    } elseif($temperature >= 30 && $temperature < 40) {
+        echo "It's so hot today. <br>";
+    } elseif($temperature >= 40) {
+        echo "Am I in the Sahara Desert?!";
+    }
+?>
