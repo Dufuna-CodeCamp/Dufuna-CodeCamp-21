6064508e4716c94c226b2c93145bb0deafe09ee1
Bayo Obafemi <ebenezerbayo@gmail.com>
2023-03-09 00:43:17 +0100

    run test for the weather.php file

--- submissions/adebayoObafemi/php/weather.php
+++ submissions/adebayoObafemi/php/weather.php
@@ -1,13 +1,13 @@
 <?php
-    $i = 27;
+    $temperature = 27;
 
-    if($i <= 20) {
+    if($temperature <= 20) {
         echo "It is freezing today <br>";
-    } elseif($i > 20 && $i < 30) {
+    } elseif($temperature > 20 && $temperature < 30) {
         echo "The weather is just perfect. <br>";
-    } elseif($i >= 30 && $i < 40) {
+    } elseif($temperature >= 30 && $temperature < 40) {
         echo "It's so hot today. <br>";
-    } elseif($i >= 40) {
-        echo "Am I in the Sahara Desert?!";
+    } elseif($temperature >= 40) {
+        echo "Am i in the Sahara Desert?!";
     }
 ?>
