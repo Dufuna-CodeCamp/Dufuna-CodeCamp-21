f42765b2c4543635ef6abc1ab6f148788463b373
Paul Jibola <harjibbolar26@gmail.com>
2023-03-12 00:06:56 +0100

    make changes to js file

--- submissions/paulElugbaju/javascript/loops.js
+++ submissions/paulElugbaju/javascript/loops.js
@@ -4,13 +4,13 @@ let i = 1;
 
 while (i <= x) {
     if (i%3==0 && i%5==0) {
-        document.write('Software Developer ' + '<br>');
+        document.write('Software Developer' + '<br>');
     } 
     else if (i%5==0) {
-        document.write('Developer ' + '<br>');
+        document.write('Developer' + '<br>');
     } 
     else if (i%3==0) {
-        document.write ('Software ' + '<br>');
+        document.write ('Software' + '<br>');
     } 
     else {
         document.write(i + '<br>')
