25a44214e4a53b426d3058cc37b98ee87078cea0
Paul Jibola <harjibbolar26@gmail.com>
2023-03-12 00:20:08 +0100

    new changes

--- submissions/paulElugbaju/javascript/loops.js
+++ submissions/paulElugbaju/javascript/loops.js
@@ -6,11 +6,11 @@ while (i <= x) {
     if (i%3==0 && i%5==0) {
         document.write('Software Developer' + '<br>');
     } 
-    else if (i%5==0) {
-        document.write('Developer' + '<br>');
-    } 
     else if (i%3==0) {
-        document.write ('Software' + '<br>');
+        document.write('Software' + '<br>');
+    } 
+    else if (i%5==0) {
+        document.write ('Developer' + '<br>');
     } 
     else {
         document.write(i + '<br>')
