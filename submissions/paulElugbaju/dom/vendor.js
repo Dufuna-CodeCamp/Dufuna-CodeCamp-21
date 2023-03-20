b9c49147809143449cddb2181912f9be399baf48
Paul Jibola <harjibbolar26@gmail.com>
2023-03-20 03:54:20 +0100

    edit js file for vendor

--- submissions/paulElugbaju/dom/vendor.js
+++ submissions/paulElugbaju/dom/vendor.js
@@ -6,17 +6,10 @@ button.forEach((rem) => {
     rem.addEventListener('click', function (e) {
         if (rem.innerHTML == 'Add to Cart') {
             rem.innerHTML = '<img src="./images/remove.svg" alt="" style = "width: 30%; margin: -5px 5px -5px -5px; color: #fff" class = "remove">Remove';
-            rem.style.display = 'flex';
-            rem.style.justifyContent = 'space-around';
-            rem.style.alignItems = 'center';
-            rem.style.backgroundColor = '#a5a5a5';
-            rem.style.color = 'white';
-            rem.style.border = 'none'
+            rem.style = 'display: flex; justify-content: space-around; align-items: center; background-color: #a5a5a5; color: white; border: none'
         } else if (rem.innerText == 'Remove') {
             rem.innerHTML = 'Add to Cart';
-            rem.style.backgroundColor = '#fff';
-            rem.style.color = '#ad4c4c';
-            rem.style.border = '1.5px solid #ad4c4c';
+            rem.style = 'background-color: white; color: #ad4c4c; border: 1.5px solid #ad4c4c';
         };
 
         const cartList = document.querySelectorAll('.remove').length;
