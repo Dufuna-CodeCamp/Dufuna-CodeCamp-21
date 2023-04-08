5429c64a874b58f8aa3032bb1b025c200d5aaa9e
Hosanna Bivan <hosabivan@gmail.com>
2023-04-08 13:05:30 +0100

    correction on food2.html

--- submissions/hosannaBivan/Dom/script2.js
+++ submissions/hosannaBivan/Dom/script2.js
@@ -3,33 +3,50 @@ var newBoxes = document.getElementsByClassName("g");
 
 
 for(var box of boxes){
-    box.addEventListener('click', this.makeSelection);
+    box.addEventListener('mouseover', makeSelection);
     
 }
 
 for(var newBox of newBoxes){
-    newBox.addEventListener('click', this.undoSelection);
+    newBox.addEventListener('mouseover', undoSelection);
     
 }
 
+
+
 function makeSelection(){
-    for(var box of boxes){
-        box.style.display = "none";
-
+    for(let i=0; i<boxes.length; i++){
+        boxes[i].addEventListener('click', function(e){
+            e.preventDefault();
+            
+            const displayOld = boxes[i]
+            const displayNew = newBoxes[i];
+            
+            displayOld.style.display = "none";
+            displayNew.style.display = "block";
+            
+        })
     }
-    for(var newBox of newBoxes){
-        newBox.style.display = "block"
-
-    }
+ 
+        
 }
 
-function undoSelection(){
-    for(var newBox of newBoxes){
-        newBox.style.display = "none"
-    }
 
-    for(var box of boxes){
-        box.style.display = "block";
-    }
+
+function undoSelection(){
+
+for(let i=0; i<newBoxes.length; i++){
+    newBoxes[i].addEventListener('click', function(e){
+        e.preventDefault();
+        
+        
+        const displayOld = boxes[i]
+        const displayNew = newBoxes[i];
+        
+        displayOld.style.display = "block";
+        displayNew.style.display = "none";
+    })
+}
+
     
 }
