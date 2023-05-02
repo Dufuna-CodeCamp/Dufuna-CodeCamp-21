8dbc2cd709b8be5c22212f3545dd5d77ca85e881
Azeezatodun <azeezatodunoye@gmail.com>
2023-05-02 20:45:57 +1300

    ran test on my transport database

--- submissions/azeezatOyedeji/database2/init.sql
+++ submissions/azeezatOyedeji/database2/init.sql
@@ -9,14 +9,14 @@ CREATE TABLE passengers(
 id INT NOT NULL AUTO_INCREMENT,
 full_name VARCHAR(50) NOT NULL,
 sex VARCHAR(7) NOT NULL,
-age INT,
+age VARCHAR(3),
 PRIMARY KEY (id)
 );
 
 -- travel details data table;
 CREATE TABLE passengers_details(
 id INT NOT NULL AUTO_INCREMENT,
-passenger_id INT NOT NULL,
+passengers_id INT NOT NULL,
 p_class INT NOT NULL,
 ticket_num VARCHAR(20) NOT NULL,
 trip_fare FLOAT(10,4) NOT NULL,
@@ -25,14 +25,14 @@ num_parent_children INT NOT NULL,
 num_siblings_spouses INT NOT NULL,
 embarked CHAR(1) NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (passenger_id) REFERENCES passenger(id)
+FOREIGN KEY (passengers_id) REFERENCES passenger(id)
 );
 
 -- to create survival table;
 CREATE TABLE accident_cases(
 id INT NOT NULL AUTO_INCREMENT,
- passenger_id INT NOT NULL,
+ passengers_id INT NOT NULL,
  survived INT NOT NULL,
  PRIMARY KEY (id),
- FOREIGN KEY (passenger_id) REFERENCES passenger_details(id)
+ FOREIGN KEY (passengers_id) REFERENCES passenger_details(id)
 );
