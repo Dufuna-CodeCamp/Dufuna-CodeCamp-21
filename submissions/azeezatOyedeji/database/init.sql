99366e7d56af46b1d728d6893449f105fd327368
Azeezatodun <azeezatodunoye@gmail.com>
2023-05-03 00:49:55 +1300

    updated changes to foodbag database

--- submissions/azeezatOyedeji/database/init.sql
+++ submissions/azeezatOyedeji/database/init.sql
@@ -7,10 +7,8 @@ USE foodbag;
 -- create table for food
 CREATE TABLE food(
 id INT NOT NULL AUTO_INCREMENT,
-fast_food VARCHAR(100) NOT NULL,
-vegetables_fruits VARCHAR(100) NOT NULL,
-drinks VARCHAR(100) NOT NULL,
-resturant VARCHAR(100) NOT NULL
+type ENUM('Fast Food','Vegetables & Fruits','Drinks & Cocktails','Resturants'),
+PRIMARY KEY (id)
 );
 
 -- create vendors table
@@ -19,28 +17,31 @@ id INT NOT NULL AUTO_INCREMENT,
 admin_id INT NOT NULL,
 food_id INT NOT NULL,
 name VARCHAR(255) NOT NULL,
-amount FLOAT(6,2) NOT NULL,
+amount DECIMAL(10,2) NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (admin_id) REFERENCES food(id)
+FOREIGN KEY (admin_id) REFERENCES admin(id),
+FOREIGN KEY (food_id) REFERENCES food(id)
 );
 
 -- creating admins table
 CREATE TABLE admins(
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(255) NOT NULL,
-type INT NOT NULL,
+type ENUM('0','1') NOT NULL,
 email_address VARCHAR(255) NOT NULL,
-phone_number INT NOT NULL,
-password VARCHAR(15) NOT NULL
+phone_number VARCHAR(100) NOT NULL,
+password VARCHAR(15) NOT NULL,
+PRIMARY KEY(id)
 );
 
 -- creating customers account table
 CREATE TABLE customer_acc(
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(255) NOT NULL,
-phone_number INT NOT NULL,
+phone_number VARCHAR(100) NOT NULL,
 address VARCHAR(255) NOT NULL,
-payment_method VARCHAR(20)
+payment_method ENUM('POS','Cash','Transfer'),
+PRIMARY KEY (id)
 );
  
 -- creating orders table
@@ -48,5 +49,7 @@ CREATE TABLE orders(
 id INT NOT NULL AUTO_INCREMENT, 
 vendor_id INT NOT NULL, 
 customer_id INT,
-FOREIGN KEY (vendor_id) REFERENCES vendor_food(id)
+PRIMARY KEY(id),
+FOREIGN KEY (vendor_id) REFERENCES vendor_food(id),
+FOREIGN KEY (customer_id) REFERENCES customer_acc(id)
 ); 
