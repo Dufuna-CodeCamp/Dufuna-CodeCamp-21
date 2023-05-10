0be80d88c38dc0cbcccc91e9baac6d381504921b
Oluwapelumi <pelumiiadetoye@gmail.com>
2023-05-11 00:54:00 +0100

    made new changes

--- submissions/bukunmiAdeniyi/React/src/components/CartList.js
+++ submissions/bukunmiAdeniyi/React/src/components/CartList.js
@@ -1,35 +1,34 @@
-import React from "react"
+import React from "react";
 
-import './css/CartList.css'
+import "./css/CartList.css";
 
-import { FaTimes, FaMinus, FaPlus } from 'react-icons/fa'
+import { FaTimes, FaMinus, FaPlus } from "react-icons/fa";
 
-export const CartList = ({cartItem, addCart, onRemove, removeMenu}) => {
+export const CartList = ({ cartItem, addCart, onRemove, removeMenu }) => {
+  return (
+    <div>
+      <div className="cart-item">
+        <FaTimes onClick={() => removeMenu(cartItem)} className="fa-icons-x" />
+        <img className="cart-img" src={cartItem.image} alt={cartItem.title} />
 
-    return (
-        <div>
-            <div className='cart-item' >
-            
-                <FaTimes onClick={() => removeMenu(cartItem)} className='fa-icons-x' />
-                <img className='cart-img' src={cartItem.image} alt={cartItem.title} />   
-                        
-                <span className='cart-name'>{cartItem.title}</span>
+        <span className="cart-name">{cartItem.title}</span>
 
-                <span className='price'>#{cartItem.cost * cartItem.qty} </span>
+        <span className="price">#{cartItem.cost * cartItem.qty} </span>
 
-                <div className='qty-btn'>
-                
-                    <button onClick={() => addCart(cartItem)} ><FaPlus /></button>
-                    <span className='qty'>{cartItem.qty}</span>
-                    <button onClick={() => onRemove(cartItem)} ><FaMinus  /></button>
-                    
-                </div>
-            </div>
-
-            <hr />            
-            
+        <div className="qty-btn">
+          <button onClick={() => addCart(cartItem)}>
+            <FaPlus />
+          </button>
+          <span className="qty">{cartItem.qty}</span>
+          <button onClick={() => onRemove(cartItem)}>
+            <FaMinus />
+          </button>
         </div>
-    )
-}
+      </div>
 
-export default CartList
+      <hr />
+    </div>
+  );
+};
+
+export default CartList;
