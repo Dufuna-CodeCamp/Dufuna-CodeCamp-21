24fffe1dd4e62a75e606747100e85da974a83869
Elijah.Zgambo <ejzgambo@gmail.com>
2023-06-06 17:05:01 +0200

    dom1

---
+++ submissions/elijahZgambo/Dom/vendor.js
@@ -0,0 +1,33 @@
+
+let adField = document.querySelectorAll('.adBtn'),
+    cartAdd = document.getElementById('cartSpan');
+
+
+adField.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        var txt = btn.innerHTML;
+        if (txt == 'Add to Cart') {
+            btn.innerHTML = '<span class="draw"></span> Remove';
+            btn.style.background = 'gray';
+            btn.style.border = 'none';
+            btn.style.color = 'rgba(255, 255, 255, 1)'
+            cartAdd.style.background = 'brown';
+        } else {
+            btn.innerHTML = 'Add to Cart';
+            btn.style.background = 'transparent';
+            btn.style.border = '1px solid brown';
+            btn.style.color = 'brown';
+            var evaluator = function () {
+                for (var i = 0; i < adField.length; i++) {
+                    if (adField[i].innerHTML == '<span class="draw"></span> Remove') {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            if (!evaluator()) {
+                cartAdd.style.background = 'transparent';
+            }
+        }
+    });
+});
