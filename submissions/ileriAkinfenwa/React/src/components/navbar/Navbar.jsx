5862fe380271d36968c02ae48a39bdd7f9deee76
Akinfenwa Taiwo Christopher <taiwoakinfenwa2019@gmail.com>
2023-06-20 05:10:01 +0100

    add React-Cart

--- submissions/ileriAkinfenwa/React/src/components/navbar/Navbar.jsx
+++ submissions/ileriAkinfenwa/React/src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar=(props) => {
                 <h3 style={{cursor:'pointer'}} onClick={()=>props.handleShowCart(false)}>Food<span>Bag</span></h3>
             </div>
             <div class="navlinks">
-                <div href="cart" style={{cursor:'pointer'}} onClick={()=>props.handleShowCart(true)}>Cart <sup style={{marginLeft:'-2px'}}> {props.count}</sup></div>
+                <div href="cart" style={{cursor:'pointer'}} onClick={()=>props.handleShowCart(true)}>Cart <sup style={{visibility: props.count ? "visible": "hidden" ,backgroundColor: props.count ? " #AD4C4C": "hidden",color: props.count ? " #AD4C4C": "hidden",borderRadius:"50%", fontSize:'8px'}}>{props.count}</sup></div>
                 <a href="login">Login</a>
                 <a href="signup">Sign up</a>
             </div>
