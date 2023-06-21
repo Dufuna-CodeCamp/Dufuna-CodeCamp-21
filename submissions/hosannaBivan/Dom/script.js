8ff25800692d7f5e7ce23d2a9c702bdb4e67a4b6
Hosanna Bivan <hosabivan@gmail.com>
2023-06-21 11:09:33 +0100

    remove comments

--- submissions/hosannaBivan/Dom/script.js
+++ submissions/hosannaBivan/Dom/script.js
@@ -1,30 +1,32 @@
 var buttons = document.getElementsByClassName("cart")
 var cart = document.getElementById('cart')
+var removeIcon = document.getElementsByClassName("span-none")
+
 
 function addToCart(){
     for(let i=0; i<buttons.length; i++){
         buttons[i].addEventListener("click", () => {
 
-            buttons[i].classList.toggle("remove");
 
             if (buttons[i].innerHTML == "Add to Cart"){
-                buttons[i].innerHTML = "Remove"
+
+                buttons[i].innerHTML = "Remove <span><img class='remove-icon' src='./images/Vector.png' alt=''></span>"
+
+                buttons[i].classList.add("remove");
+
+                cart.classList.add("cart-dot");   
+
             }
             else{
                 buttons[i].innerHTML = "Add to Cart"
+
+                buttons[i].classList.remove("remove");
+
+                cart.classList.remove("cart-dot");   
+
             }
-
-                if(buttons[i].innerHTML == "Remove"){
-                    cart.classList.add("cart-dot");   
-                }
-                else{
-                    cart.classList.remove("cart-dot");   
-                }
-
             
         })
-
-        
         
     }
 
--- submissions/hosannaBivan/Dom/script2.js
+++ submissions/hosannaBivan/Dom/script2.js
@@ -26,50 +26,8 @@ for(var newBox of newBoxes){
 function makeSelection(){
     for(let i=0; i<newBoxes.length; i++){
         newBoxes[i].addEventListener('click', function(){
-            // e.preventDefault();
             
             window.location.href="food.html";
         })
     }       
 }
-
-
-
-
-
-// var buttons = document.getElementsByClassName("cart")
-// var cart = document.getElementById('cart')
-
-// function addToCart(){
-//     for(let i=0; i<buttons.length; i++){
-//         buttons[i].addEventListener("click", () => {
-
-//             buttons[i].classList.toggle("remove");
-
-//             if (buttons[i].innerHTML == "Add to Cart"){
-//                 buttons[i].innerHTML = "Remove"
-//             }
-//             else{
-//                 buttons[i].innerHTML = "Add to Cart"
-//             }
-
-//                 if(buttons[i].innerHTML == "Remove"){
-//                     cart.classList.add("cart-dot");   
-//                 }
-//                 else{
-//                     cart.classList.remove("cart-dot");   
-//                 }
-
-            
-//         })
-
-        
-        
-//     }
-
-//     } 
-
-// addToCart()
-
-
-
