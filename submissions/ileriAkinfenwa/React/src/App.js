cc251d89aa2787a4196c95d2ade22393b8e6d233
Akinfenwa Taiwo Christopher <taiwoakinfenwa2019@gmail.com>
2023-06-26 10:48:11 +0100

    add React-sucessPage

--- submissions/ileriAkinfenwa/React/src/App.js
+++ submissions/ileriAkinfenwa/React/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Verification from './page/verification/Verification';
 import Data from './Data';
 import { Cart } from './page/cartPage/Cart';
+import Sucess from './page/sucessPage/Sucess';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Data/>}/>
         <Route path='cart' element={<Cart/>}/>
-        <Route path='verification' element={<Verification/>}/>'
+        <Route path='verification' element={<Verification/>}/>
+        <Route path='sucess' element={<Sucess/>}/>
       </Routes>
       </div>
   )
