a71464666547e9333a9a7ae07a399c27aad8e63e
Dalitsofullstack <magabadalitso@gmail.com>
2023-07-21 11:32:57 +0200

    [BCAFUS-27] Develop an E-commerce Store Database

--- submissions/dalitsoMagaba/database/init.sql
+++ submissions/dalitsoMagaba/database/init.sql
@@ -1,55 +1,43 @@
--- foodbag database creation
-DROP DATABASE foodbag;
-CREATE DATABASE foodbag;
-USE foodbag;
-
--- TABLES
-
--- food table
+SHOW DATABASES;
+DROP DATABASE food_bag;
+CREATE DATABASE food_bag;
+USE food_bag;
 
 CREATE TABLE food (
 id INT NOT NULL AUTO_INCREMENT,
-type ENUM('1','2','3','4'),
+type ENUM ('fast_food','vegetable_fruits','drinks_cocktails','restaurants') NOT NULL DEFAULT 'fast_food',
 PRIMARY KEY (id)
 );
-
--- vendor table
 
 CREATE TABLE vendor_food (
 id INT NOT NULL AUTO_INCREMENT,
 admin_id INT NOT NULL,
 food_id INT NOT NULL,
-name VARCHAR(30) NOT NULL,
-amount DECIMAL (10,2) NOT NULL,
+name VARCHAR(50) NOT NULL,
+amount DECIMAL(10,2) NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (admin_id) REFERENCES admins(id),
 FOREIGN KEY (food_id) REFERENCES food(id)
 );
 
--- admins table
-
 CREATE TABLE admins (
 id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(30) NOT NULL,
+name VARCHAR(50) NOT NULL,
 type ENUM ('0','1') NOT NULL,
-email_address VARCHAR(50) NOT NULL,
-phone_number VARCHAR(20) NOT NULL,
-password VARCHAR(30) NOT NULL,
+email_address VARCHAR(100) NOT NULL,
+phone_number VARCHAR(50) NOT NULL,
+password VARCHAR(50) NOT NULL,
 PRIMARY KEY (id)
 );
 
--- customers table
-
-CREATE TABLE customer (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(30) NOT NULL,
-phone_number VARCHAR(20) NOT NULL,
-address VARCHAR(50) NOT NULL,
-payment_method VARCHAR(250),
+CREATE TABLE customers (
+id INT UNIQUE,
+name VARCHAR(50) NOT NULL,
+phone_number VARCHAR(50) NOT NULL,
+address VARCHAR(250) NOT NULL,
+payment_method ENUM ('card','cash','transfer','POS') DEFAULT 'card',
 PRIMARY KEY (id)
 );
-
--- orders table
 
 CREATE TABLE orders (
 id INT NOT NULL AUTO_INCREMENT,
@@ -57,5 +45,5 @@ vendor_id INT NOT NULL,
 customer_id INT NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (vendor_id) REFERENCES vendor_food(id),
-FOREIGN KEY (customer_id) REFERENCES customer(id)
+FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
