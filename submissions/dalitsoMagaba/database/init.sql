17fa43a1f729d8b14b6609b8b9e22f3fc64dce2e
Dalitsofullstack <magabadalitso@gmail.com>
2023-07-21 13:14:40 +0200

    [BCAFUS-27] Develop an E-commerce Store Database

--- submissions/dalitsoMagaba/database/init.sql
+++ submissions/dalitsoMagaba/database/init.sql
@@ -1,4 +1,3 @@
-SHOW DATABASES;
 DROP DATABASE food_bag;
 CREATE DATABASE food_bag;
 USE food_bag;
@@ -13,7 +12,7 @@ CREATE TABLE vendor_food (
 id INT NOT NULL AUTO_INCREMENT,
 admin_id INT NOT NULL,
 food_id INT NOT NULL,
-name VARCHAR(50) NOT NULL,
+name VARCHAR(250) NOT NULL,
 amount DECIMAL(10,2) NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY (admin_id) REFERENCES admins(id),
@@ -22,18 +21,18 @@ FOREIGN KEY (food_id) REFERENCES food(id)
 
 CREATE TABLE admins (
 id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(50) NOT NULL,
-type ENUM ('0','1') NOT NULL,
-email_address VARCHAR(100) NOT NULL,
-phone_number VARCHAR(50) NOT NULL,
-password VARCHAR(50) NOT NULL,
+name VARCHAR(250) NOT NULL,
+type ENUM('0','1') NOT NULL,
+email_address VARCHAR(250) NOT NULL,
+phone_number VARCHAR(250) NOT NULL,
+password VARCHAR(250) NOT NULL,
 PRIMARY KEY (id)
 );
 
 CREATE TABLE customers (
 id INT UNIQUE,
-name VARCHAR(50) NOT NULL,
-phone_number VARCHAR(50) NOT NULL,
+name VARCHAR(250) NOT NULL,
+phone_number VARCHAR(250),
 address VARCHAR(250) NOT NULL,
 payment_method ENUM ('card','cash','transfer','POS') DEFAULT 'card',
 PRIMARY KEY (id)
