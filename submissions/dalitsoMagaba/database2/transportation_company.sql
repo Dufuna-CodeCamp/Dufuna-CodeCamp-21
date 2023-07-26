5a3bb457766dbe99e60d01fc0406106b27cf8699
Dalitsofullstack <magabadalitso@gmail.com>
2023-07-26 19:01:24 +0200

    transportation company database amendments

--- submissions/dalitsoMagaba/database2/transportation_company.sql
+++ submissions/dalitsoMagaba/database2/transportation_company.sql
@@ -4,7 +4,7 @@ CREATE DATABASE transportation_company;
 USE transportation_company;
 
 
-CREATE TABLE passengers_details (
+CREATE TABLE passengers (
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(250) NOT NULL,
 age VARCHAR(10) DEFAULT NULL,
@@ -14,7 +14,7 @@ PRIMARY KEY (id)
 
 CREATE TABLE trips (
 id INT NOT NULL AUTO_INCREMENT,
-passengers_details_id INT,
+passengers_id INT,
 passenger_class ENUM ('1','2','3'),
 passenger_ticket_number VARCHAR(250),
 trip_fare DECIMAL (5,2) NOT NULL,
@@ -23,13 +23,13 @@ parent_children_aboard INT NOT NULL,
 siblings_spouses_aboard INT NOT NULL,
 embarkation VARCHAR(250) NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (passengers_details_id) REFERENCES passengers_details(id)
+FOREIGN KEY (passengers_id) REFERENCES passengers(id)
 );
 
 CREATE TABLE accident_cases (
 id INT NOT NULL AUTO_INCREMENT,
-passengers_details_id INT,
+passengers_id INT,
 passenger_survived BOOLEAN,
 PRIMARY KEY (id),
-FOREIGN KEY (passengers_details_id) REFERENCES passengers_details(id)
+FOREIGN KEY (passengers_id) REFERENCES passengers(id)
 );
