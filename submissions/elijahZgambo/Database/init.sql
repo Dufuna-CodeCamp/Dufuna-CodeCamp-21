153d821db1e66d7ea9935dadd9de87f8c7da1a82
Elijah.Zgambo <ejzgambo@gmail.com>
2023-08-16 11:46:04 +0200

    mysql update

--- submissions/elijahZgambo/Database/init.sql
+++ submissions/elijahZgambo/Database/init.sql
@@ -13,9 +13,9 @@ create table food (
 create table  admins (
 	id int not null auto_increment, 
 	name varchar(250) not null, 
-    type int not null, 
+    type boolean not null, 
 	email_address varchar(250) not null, 
-	phone_number int not null, 
+	phone_number varchar(250) not null, 
 	password varchar(250) not null, 
 	primary key(id) 
 	);
@@ -26,7 +26,7 @@ create table vendor_food (
 	admin_id int not null, 
 	food_id int not null, 
 	name varchar(250) not null, 
-	amount int not null, 
+	amount decimal(10,2) not null, 
 	primary key(id), 
 	foreign key (admin_id) references admins (id), 
 	foreign key (food_id) references food(id)
@@ -46,8 +46,22 @@ create table customer (
 create table orders ( 
 	id int not null auto_increment,
 	vendor_id int not null,
-    customer_id int not null,
+    customer_id int null,
     primary key(id),
     foreign key(vendor_id) references vendor_food(id),
     foreign key(customer_id) references customer(id)
     );
+	
+show columns from food;
+alter table food modify type ENUM('fast_food', 'vegetable_fruits', 'drinks_cocktails', 'restaurants') not null default 'fast_food';
+alter table admins modify name varchar(250) not null;
+alter table admins modify email_address varchar(250) not null;
+alter table admins modify name varchar(250) not null;
+alter table vendor_food modify name varchar(250) not null;
+alter table customer modify phone_number varchar(250) not null;
+alter table customer modify address varchar(250) not null;
+alter table customer modify payment_method ENUM('card', 'cash', 'transfer') null default 'card';
+alter table admins modify phone_number varchar(250) not null;
+alter table vendor_food modify amount decimal(10,2) not null;
+alter table orders modify customer_id int null;
+alter table admins modify type boolean default true;
